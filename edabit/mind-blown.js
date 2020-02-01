c8da6e64c4158b033470794ceac8e875c512b836
jcacct <jcacct@protonmail.ch>
2020-02-01 08:00:57 -0800

    hours to seconds solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -98,3 +98,43 @@ function addition(num) {
 }
 
 // -----------------------------------------------------
+// Write a function that converts hours into seconds.
+// Examples
+
+// howManySeconds(2) ➞ 7200
+
+// howManySeconds(10) ➞ 36000
+
+// howManySeconds(24) ➞ 86400
+
+// Notes
+
+//     60 seconds in a minute, 60 minutes in an hour
+//     Don't forget to return your answer.
+
+function howManySeconds(hours) {
+	return hours * 60 * 60;
+}
+
+// --------------------------------------------------------
+// You are given 2 out of 3 angles in a triangle, in degrees.
+
+// Write a function that classifies the missing angle as either "acute", "right", or "obtuse" based on its degrees.
+
+//     An acute angle is less than 90 degrees.
+//     A right angle is exactly 90 degrees.
+//     An obtuse angle is greater than 90 degrees (but less than 180 degrees).
+
+// For example: missingAngle(11, 20) should return "obtuse", since the missing angle would be 149 degrees, which makes it obtuse.
+// Examples
+
+// missingAngle(27, 59) ➞ "obtuse"
+
+// missingAngle(135, 11) ➞ "acute"
+
+// missingAngle(45, 45) ➞ "right"
+
+// Notes
+
+// The sum of angles of any triangle is always 180 degrees.
+
