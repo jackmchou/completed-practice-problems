f5c8da9b2020542ec5b621fc0b10c0ccbdc3e824
jcacct <jcacct@protonmail.ch>
2020-02-02 10:13:40 -0800

    getEqual solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -216,3 +216,60 @@ function isFourLetters(arr) {
 function findLargestNum(arr) {
 	return Math.max(...arr);
 }
+
+// In music, notes can be written out in multiple ways (especially for notes on the black keys). Although these notes are spelled out differently, they still are the same note physically.
+
+// C# = Db, D# = Eb, F# = Gb, G# = Ab, A# = Bb
+
+// Given a musical note, create a function that returns its enharmonic equivalent. The examples below should make this clear.
+// Examples
+
+// getEquivalent("D#") ➞ "Eb"
+
+// getEquivalent("Gb") ➞ "F#"
+
+// getEquivalent("Bb") ➞"A#"
+
+// Notes
+
+//     Note names will always be a capital letter followed by either # or b.
+//     Remember that the note after G is A and vice versa.
+
+function getEquivalent(note) {
+	switch (note) {
+		case 'C#':
+			return 'Db';
+			break;
+		case 'D#':
+			return 'Eb';
+			break;
+		case 'F#':
+			return 'Gb';
+			break;
+		case 'G#':
+			return 'Ab';
+			break;
+		case 'A#':
+			return 'Bb';
+			break;
+		case 'Db':
+			return 'C#';
+			break;
+		case 'Eb':
+			return 'D#';
+			break;
+		case 'Gb':
+			return 'F#';
+			break;
+		case 'Ab':
+			return 'G#';
+			break;
+		case 'Bb':
+			return 'A#';
+			break;
+		default:
+			return 0;
+	}
+}
+
+//--------------------------------------------------------
