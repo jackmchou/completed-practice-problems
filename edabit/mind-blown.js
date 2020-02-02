b5602d7f39255f2037c1de9d7723ca8bf7626499
jcacct <jcacct@protonmail.ch>
2020-02-02 10:54:44 -0800

    redundant complete

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -314,3 +314,20 @@ function canNest(arr1, arr2) {
 function acceptedWords(arr) {
 	return arr.filter(i => i.slice(0,1) !== "C");
 }
+
+// ----------------------------------------------------------
+// Write a function redundant that takes in a string str and returns a function that returns str.
+// Examples
+
+// const f1 = redundant("apple")
+// f1() ➞ "apple"
+
+// const f2 = redundant("pear")
+// f2() ➞ "pear"
+
+// const f3 = redundant("")
+// f3() ➞ ""
+
+// Notes
+
+// Your function should return a function, not a string.
