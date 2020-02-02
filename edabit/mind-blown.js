d8120558e8858471da4dec20db5d2ffbe3527719
jcacct <jcacct@protonmail.ch>
2020-02-02 09:12:40 -0800

    isTruthy solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -145,3 +145,51 @@ function missingAngle(angle1, angle2) {
 	: angles < 90 ? "obtuse"
 	: null;
 }
+
+//------------------------------------------------------
+// A value is said to be "truthy" if it evaluates to true in a Boolean context. All values are truthy in JavaScript unless they're one of the following:
+
+//     false
+//     null
+//     undefined
+//     0
+//     NaN
+//     ""
+
+// In JavaScript, an empty object and an empty array are both considered "truthy," but an empty string is considered false when evaluated as a Boolean (this behavior is what we call "falsey").
+
+// Create a function that takes an argument of any data type and returns 1 if it's truthy and 0 if it's falsy.
+// Examples
+
+// isTruthy(0) ➞ 0
+
+// isTruthy(false) ➞ 0
+
+// isTruthy("") ➞ 0
+
+// isTruthy("false") ➞ 1
+
+// Notes
+
+//     You may have to take into account NaN's unique behavior in JavaScript. While other falsey values are equal to themselves, NaN != NaN
+//     If you get stuck on a challenge, find help in the Resources tab.
+//     If you're really stuck, unlock solutions in the Solutions tab.
+
+function isTruthy(input) {
+	return !input ? 0 : 1;
+}
+
+//-----------------------------------------------------
+// Create a function that takes an array of strings and returns the words that are exactly four letters.
+// Examples
+
+// isFourLetters(["Tomato", "Potato", "Pair"]) ➞ ["Pair"]
+
+// isFourLetters(["Kangaroo", "Bear", "Fox"]) ➞ ["Bear"]
+
+// isFourLetters(["Ryan", "Kieran", "Jason", "Matt"]) ➞ ["Ryan", "Matt"]
+
+// Notes
+
+// You can expect valid strings for all test cases.
+
