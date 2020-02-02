5d5d77b34c777f5c9e2765a16b3733dc5f1584b1
jcacct <jcacct@protonmail.ch>
2020-02-02 10:45:54 -0800

    acceptedWords solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -297,3 +297,20 @@ function getEquivalent(note) {
 function canNest(arr1, arr2) {
 	return Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2) ? true : false; 
 }
+
+//----------------------------------------------------------
+// You are give an array with random words but your program doesn't accept words that begin with the capital letter "C". Remove the unaccepted words and return the new array.
+// Examples
+
+// accepted(["Ducks", "Bears",  "Cats"]) ➞ ["Ducks", "Bears"]
+
+// accepted(["cars", "trucks", "planes"] ➞ ["cars", trucks", "planes"]
+
+// accepted(["Cans", "Worms", "Bugs", "Cold", "Beans"]) ➞ ["Worms", "Bugs", "Beans"]
+
+// Notes
+
+// Use a RegEx boundary assertion in your function.
+function acceptedWords(arr) {
+	return arr.filter(i => i.slice(0,1) !== "C");
+}
