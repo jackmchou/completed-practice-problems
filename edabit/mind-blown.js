88afa3efd1af6ee178e6c57f091eadd063d1b993
jcacct <jcacct@protonmail.ch>
2020-02-03 12:25:39 -0800

    triangular number sequence solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -468,3 +468,21 @@ function Go(num) {
 function findSmallestNum(arr) {
 	return Math.min(...arr);
 }
+
+//----------------------------------------------------------
+// This Triangular Number Sequence is generated from a pattern of dots that form a triangle. The first 5 numbers of the sequence, or dots, are: 1, 3, 6, 10, 15. Write a function that converts n number of places with its corresponding number.
+// Examples
+
+// triangle(1) ➞ 1
+
+// triangle(6) ➞ 21
+
+// triangle(215) ➞ 23220
+
+// Notes
+
+// N/A
+
+function triangle(n) {
+	return (n * (n+1)) / 2;
+}
