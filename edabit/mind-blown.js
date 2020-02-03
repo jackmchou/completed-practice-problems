82b8d161da06b8e541ce035a6810c865bbcf532b
jcacct <jcacct@protonmail.ch>
2020-02-03 11:14:41 -0800

    Go solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -420,3 +420,27 @@ function getDistance(a, b) {
 	let d = parseFloat(Math.hypot(a['x'] - b['x'], a['y'] - b['y']).toFixed(3));
 	return d === 0 ? 0 : d;
 }
+
+//-------------------------------------------------------------------
+// Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
+// Examples
+
+// Go(1) ➞ "-"
+
+// Go(5) ➞ "-----"
+
+// Go(3) ➞ "---"
+
+// Notes
+
+//     You will be provided integers ranging from 1 to 60.
+//     Don't forget to return your result as a string.
+//     If you get stuck on a challenge, find help in the Resources tab.
+//     If you're really stuck, unlock solutions in the Solutions tab.
+
+function Go(num) {
+	var str = '';
+	var dash = '-';
+	str += dash.repeat(num);
+	return str;
+}
