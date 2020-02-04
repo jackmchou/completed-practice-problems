560be3f94f7c776ddd26aaf806c926df82c7b15f
jcacct <jcacct@protonmail.ch>
2020-02-04 09:33:54 -0800

    countVowels solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -1,3 +1,22 @@
+// Create a function that takes a string and returns the number (count) of vowels contained within it.
+// Examples
+
+// countVowels("Celebration") ➞ 5
+
+// countVowels("Palm") ➞ 1
+
+// countVowels("Prediction") ➞ 4
+
+// Notes
+
+//     a, e, i, o, u are considered vowels (not y).
+//     All test cases are one word and only contain letters.
+
+function countVowels(str) {
+	return str.match(/[aeiou]/gi).length;
+}
+
+// --------------------------------------------------------
 // Write a function that converts an object into an array, where each element represents a key-value pair.
 // Examples
 
