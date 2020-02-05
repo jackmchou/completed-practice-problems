a8f6251b0f2b915ebc28b63ce1179696c53c393c
jcacct <jcacct@protonmail.ch>
2020-02-05 11:19:34 -0800

    addEnding solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -1,3 +1,26 @@
+// Create a function that adds a string ending to each member in an array.
+// Examples
+
+// addEnding(["clever", "meek", "hurried", "nice"], "ly")
+// ➞ ["cleverly", "meekly", "hurriedly", "nicely"]
+
+// addEnding(["new", "pander", "scoop"], "er")
+// ➞ ["newer", "panderer", "scooper"]
+
+// addEnding(["bend", "sharpen", "mean"], "ing")
+// ➞ ["bending", "sharpening", "meaning"]
+
+// Notes
+
+//     Don't forget to return the result.
+//     If you get stuck on a challenge, find help in the Resources tab.
+//     If you're really stuck, unlock solutions in the Solutions tab.
+
+function addEnding(arr, ending) {
+	return arr.map(i => i + ending);
+}
+
+//----------------------------------------------------
 // A value is omnipresent if it exists in every subarray inside the main array.
 
 // To illustrate:
