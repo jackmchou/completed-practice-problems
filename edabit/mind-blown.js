3cc4d76e4aa302b1b6b688164d93046c0607731f
jcacct <jcacct@protonmail.ch>
2020-02-16 10:01:53 -0800

    century solved

--- edabit/mind-blown.js
+++ edabit/mind-blown.js
@@ -1,3 +1,39 @@
+// Create a function that takes in a year and returns the correct century.
+// Examples
+
+// century(1756) ➞ "18th century"
+
+// century(1555) ➞ "16th century"
+
+// century(1000) ➞ "10th century"
+
+// century(1001) ➞ "11th century"
+
+// century(2005) ➞ "21st century"
+
+// Notes
+
+//     All years will be between 1000 and 2010.
+//     The 11th century is between 1001 and 1100.
+//     The 18th century is between 1701-1800.
+
+function century(year) {
+	let cen = String(Math.ceil(year * 0.01)).slice(0,2);
+	return cen === '21' ? `${cen}st century` : `${cen}th century`;
+}
+
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  let result = '';
+  while (num > 0) {
+    result += str;
+    num--;
+  }
+  return result;
+}
+
 // Create a function that takes a number and returns an array with the digits of the number in reverse order.
 // Examples
 
