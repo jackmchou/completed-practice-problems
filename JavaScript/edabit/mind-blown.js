9b0d291cd41ed37b7084439085c97fc14ec9549c
jcacct <jcacct@protonmail.ch>
2020-03-15 09:27:26 -0700

    getAbsSum and formatPhoneNumber completed

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -40,6 +40,46 @@ function sortByAnswer(arr) {
 //     If you get a number with .0 on the end, return the integer value (e.g. return "4" rather than "4.0").
 //     If the number is 0, 0.0, 000, 00.00, etc... return "0".
 
+function removeLeadingTrailing(n) {
+	return n.replace(/^0+(\d)|(\d)0+$|(\d)\.0+$/g, '$1$2$3')
+}
+
+// Create a function that takes an array of 10 numbers (between 0 and 9) and returns a string of those numbers formatted as a phone number (e.g. (555) 555-5555).
+// Examples
+
+// formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ➞ "(123) 456-7890"
+
+// formatPhoneNumber([5, 1, 9, 5, 5, 5, 4, 4, 6, 8]) ➞ "(519) 555-4468"
+
+// formatPhoneNumber([3, 4, 5, 5, 0, 1, 2, 5, 2, 7]) ➞ "(345) 501-2527"
+
+// Notes
+
+// Don't forget the space after the closing parenthesis.
+
+function formatPhoneNumber(numbers) {
+	return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
+// Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element.
+// Examples
+
+// getAbsSum([2, -1, 4, 8, 10]) ➞ 25
+
+// getAbsSum([-3, -4, -10, -2, -3]) ➞ 22
+
+// getAbsSum([2, 4, 6, 8, 10]) ➞ 30
+
+// getAbsSum([-1]) ➞ 1
+
+// Notes
+
+// The term "absolute value" means to remove any negative sign in front of a number, and to think of all numbers as positive (or zero).
+
+function getAbsSum(arr) {
+	return arr.reduce((cur, acc) => Math.abs(cur) + Math.abs(acc), 0)
+}
+
 // Create a function that takes an array of non-negative numbers and strings and return a new array without the strings.
 // Examples
 
