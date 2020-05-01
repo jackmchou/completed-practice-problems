281596e3daf9d3ea3899ba1d94083a28e0d39889
Jack Chou <jcacct@protonmail.ch>
2020-05-01 06:40:18 -0700

    Completed getDays problem

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Create a function that takes two dates and returns the number of days between the first and second date.
+// Examples
+// getDays(
+//   new Date("June 14, 2019"),
+//   new Date("June 20, 2019")
+// ) ➞ 6
+// getDays(
+//   new Date("December 29, 2018"),
+//   new Date("January 1, 2019")
+// ) ➞ 3
+// getDays(
+//   new Date("July 20, 2019"),
+//   new Date("July 30, 2019")
+// ) ➞ 10
+// Notes
+// N/A
+
+function getDays(date1, date2) {
+	return Math.abs(date1.getTime() - date2.getTime()) / (1000 * 3600 * 24);
+}
+
 // Your task is to create a Circle constructor that creates a circle with a radius provided by an argument. 
 // The circles constructed must have two getters getArea() (PIr^2) and getPerimeter() (2PI*r) 
 //which give both respective areas and perimeter (circumference).
