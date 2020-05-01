ee09f34d29c6b06ecc2ef0edb96b3693e31cc619
Jack Chou <jcacct@protonmail.ch>
2020-05-01 06:32:44 -0700

    Completed Circle exercise

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,27 @@
+// Your task is to create a Circle constructor that creates a circle with a radius provided by an argument. 
+// The circles constructed must have two getters getArea() (PIr^2) and getPerimeter() (2PI*r) 
+//which give both respective areas and perimeter (circumference).
+// For help with this class, I have provided you with a Rectangle constructor which you can use as a base example.
+// Examples
+// let circy = new Circle(11);
+// circy.getArea();
+// // Should return 380.132711084365
+// let circy = new Circle(4.44);
+// circy.getPerimeter();
+// // Should return 27.897342763877365
+// Notes
+// Don't worry about floating point precision - I've factored in the chance that your answer may be 
+// more or less accurate than mine. This is more of a tutorial than a challenge so the topic covered may be 
+//considered advanced, yet the challenge is more simple - so if this challenge gets labelled as easy, don't worry too much.
+
+class Circle {
+	constructor(radius) {
+		this.radius = radius
+	}
+	getArea(){return Math.PI * Math.pow(this.radius, 2)}
+	getPerimeter(){return 2 * Math.PI * this.radius}
+}
+
 // Create a function that returns only strings with unique characters.
 // Examples
 // filterUnique(["abb", "abc", "abcdb", "aea", "bbb"]) ➞ ["abc"]
