745f21116039d850f0a2281131dd7a71354942ee
Jack Chou <jcacct@protonmail.ch>
2020-05-01 07:44:56 -0700

    Completed largestSwap problem

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,28 @@
+// Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+// To illustrate:
+// largestSwap(27) ➞ false
+// largestSwap(43) ➞ true
+// If 27 is our input, we should return false because swapping the digits gives us 72, and 72 > 27. 
+//On the other hand, swapping 43 gives us 34, and 43 > 34.
+// Examples
+// largestSwap(14) ➞ false
+// largestSwap(53) ➞ true
+// largestSwap(99) ➞ true
+// Notes
+// Numbers with two identical digits (third example) should yield true (you can't do better).
+
+function largestSwap(num) {
+	const numStr = '' + num
+  let swappedStr = []
+  let swappedNum
+  for (let index = numStr.length; index--;) {
+    swappedStr.push(numStr[index])
+    swappedNum = swappedStr.join('')
+  }
+  if (num < swappedNum) return false;
+  return true;
+}
+
 // Async operations don't always go as planned. When errors creep up we need to know how to handle them. We can pass the reject callback to our executor function to pass errors to our promise.
 // let promise = new Promise( (resolve, reject) => {
 //   setTimeout(( ) => {
