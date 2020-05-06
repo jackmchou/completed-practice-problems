9652f12f4fbebce050e79cacf18aaff782b55c51
Jack Chou <jcacct@protonmail.ch>
2020-05-06 06:46:34 -0700

    Solved charCount

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,21 @@
+// Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string.
+// Examples
+// charCount("a", "edabit") ➞ 1
+// charCount("c", "Chamber of secrets") ➞ 1
+// charCount("b", "big fat bubble") ➞ 4
+// Notes
+// Your output must be case-sensitive (see second example).
+
+function charCount(myChar, str) {
+	let count = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === myChar) {
+			count++
+		}
+	}
+	return count;
+}
+
 // Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa! Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
 // Examples
 // timeForMilkAndCookies(new Date(2013, 11, 24)) ➞ true
