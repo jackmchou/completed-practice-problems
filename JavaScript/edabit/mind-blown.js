70fc6da2095f1297eb2d4f7351860caa8be335cc
Jack Chou <jcacct@protonmail.ch>
2020-05-07 13:11:27 -0700

    solved AddIndex

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -29,6 +29,33 @@ function sortByAnswer (arr) {
 	return arr
 }
 
+// Add the Index
+// Given an array of numbers, create a function which returns the same array but with each index 
+// elements index in the array added to itself. This means you add 0 to the number at index 0,
+// add 1 to the number at index 1, etc...
+// input = [1, 2, 3, 4, 5]
+// output = [1, 3, 5, 7, 9]
+
+//Pseudocode
+//Use a for loop to loop through the index
+//With each index add the index number as a number to the index value
+//return the array
+
+function addIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] += i
+  }
+  return arr
+}
+
+//i = 2
+//arr[i] = 3
+//arr.length = 5
+//arr = [1, 3, 5]
+addIndex([1, 2, 3, 4, 5]);
+
+
+
 // This challenge will help you interpret mathematical relationships both algebraically and geometrically.
 // Matchstick Houses, Steps 1, 2 and 3
 // Create a function that takes a number(step) as an argument and returns the amount of matchsticks in that step.See step 1, 2 and 3 in the image above.
