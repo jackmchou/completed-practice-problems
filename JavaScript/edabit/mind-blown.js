9341102d574562f04b3aa79c6016c2a8a38915d2
Jack Chou <jcacct@protonmail.ch>
2020-05-13 09:54:37 -0700

    solved findLargestNums

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.
+// Examples
+// findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) ➞ [7, 90, 2]
+// findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) ➞ [-34, -2, 7]
+// findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) ➞ [0.7634, 9.32, 9]
+// Notes
+// Watch out for negative integers (numbers).
+
+function findLargestNums(arr) {
+	let resultArr = []
+  for (let i = 0; i < arr.length; i++) {
+    let arrMax = arr[i][0]
+    for (let ii = 0; ii < arr[i].length; ii++) {
+      if (arr[i][ii] > arrMax) {
+      	arrMax = arr[i][ii]
+      }
+    }
+    resultArr.push(arrMax)
+  }
+  return resultArr
+}
+
 // Given an object of how many more pages each ink color can print, output the maximum number of pages the printer can print before any of the colors run out.
 // Examples
 
