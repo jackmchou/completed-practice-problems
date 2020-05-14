34a5c04ef17546c27fb5023d1138e57759b28eab
Jack Chou <jcacct@protonmail.ch>
2020-05-14 08:48:16 -0700

    Solved isValid

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,19 @@
+// Zip codes consist of 5 consecutive digits. Given a string, write a function to determine whether the input is a valid zip code. A valid zip code is as follows:
+//     Must only contain numbers (no non-digits allowed).
+//     Must not contain any spaces.
+//     Must not be greater than 5 digits in length.
+// Examples
+// isValid("59001") ➞ true
+// isValid("853a7") ➞ false
+// isValid("732 32") ➞ false
+// isValid("393939") ➞ false
+// Notes
+// N/A
+
+function isValid(zip) {
+	return /\d{4,5}[^\s\Da-z]/gi.test(zip) && zip.length <= 5
+}
+
 // Create a function that returns the number of hashes and pluses in a string.
 // Examples
 // hashPlusCount("###+") ➞ [3, 1]
