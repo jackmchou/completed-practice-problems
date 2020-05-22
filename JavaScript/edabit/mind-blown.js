399591e326e96c14740cada70a03f67de5d501fc
Jack Chou <jcacct@protonmail.ch>
2020-05-22 08:47:22 -0700

    Solved countTrue

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,16 @@
+// Create a function which returns the number of true values there are in an array.
+// Examples
+// countTrue([true, false, false, true, false]) ➞ 2
+// countTrue([false, false, false, false]) ➞ 0
+// countTrue([]) ➞ 0
+// Notes
+//     Return 0 if given an empty array.
+//     All array items are of the type bool (true or false).
+
+function countTrue(arr) {
+	 return arr.filter(idx => idx === true).length
+}
+
 // The additive persistence of an integer, n, is the number of times you have to replace n with the sum of its digits until n becomes a single digit integer.
 // The multiplicative persistence of an integer, n, is the number of times you have to replace n with the product of its digits until n becomes a single digit integer.
 // Create two functions that take an integer as an argument and:
