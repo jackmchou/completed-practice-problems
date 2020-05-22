d67659ce7f4597946a25210dcdc67b3de12850c5
Jack Chou <jcacct@protonmail.ch>
2020-05-22 08:43:35 -0700

    Solved additive and multiplicativePersistence

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,72 @@
+// The additive persistence of an integer, n, is the number of times you have to replace n with the sum of its digits until n becomes a single digit integer.
+// The multiplicative persistence of an integer, n, is the number of times you have to replace n with the product of its digits until n becomes a single digit integer.
+// Create two functions that take an integer as an argument and:
+//     Return its additive persistence.
+//     Return its multiplicative persistence.
+// Examples: Additive Persistence
+// additivePersistence(1679583) ➞ 3
+// // 1 + 6 + 7 + 9 + 5 + 8 + 3 = 39
+// // 3 + 9 = 12
+// // 1 + 2 = 3
+// // It takes 3 iterations to reach a single-digit number.
+// additivePersistence(123456) ➞ 2
+// // 1 + 2 + 3 + 4 + 5 + 6 = 21
+// // 2 + 1 = 3
+// additivePersistence(6) ➞ 0
+// // Because 6 is already a single-digit integer.
+// Examples: Multiplicative Persistence
+// multiplicativePersistence(77) ➞ 4
+// // 7 x 7 = 49
+// // 4 x 9 = 36
+// // 3 x 6 = 18
+// // 1 x 8 = 8
+// // It takes 4 iterations to reach a single-digit number.
+// multiplicativePersistence(123456) ➞ 2
+// // 1 x 2 x 3 x 4 x 5 x 6 = 720
+// // 7 x 2 x 0 = 0
+// multiplicativePersistence(4) ➞ 0
+// // Because 4 is already a single-digit integer.
+// Notes
+// N/A
+
+function additivePersistence(n) {
+  if (('' + n).length === 1) return 0
+  let persistence = 1
+  function getSum(num) {
+    let sum = 0;
+    const numStr = '' + num
+    for (let i = 0; i < numStr.length;i++) {
+      sum += +numStr[i]
+    }
+    if (('' + sum).length !== 1) {
+      getSum(sum)
+      persistence++
+    }
+    return sum
+  }
+  getSum(n)
+  return persistence
+}
+
+function multiplicativePersistence(n) {
+  if (('' + n).length === 1) return 0
+  let persistence = 1
+  function getProduct(num) {
+    let product = 1;
+    const numStr = '' + num
+    for (let i = 0; i < numStr.length;i++) {
+      product *= +numStr[i]
+    }
+    if (('' + product).length !== 1) {
+      getProduct(product)
+      persistence++
+    }
+    return product
+  }
+  getProduct(n)
+  return persistence
+}
+
 // Given an array of scrabble tiles, create a function that outputs the maximum possible score a player can achieve by summing up the total number of points for all the tiles in their hand. Each hand contains 7 scrabble tiles.
 // Here's an example hand:
 // [
