026713873f36745fe115659b46c6085739f9c191
Jack Chou <jcacct@protonmail.ch>
2020-05-22 09:38:34 -0700

    Solved rgbtoHex

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,16 @@
+// Create a function that converts color in RGB format to Hex format.
+// Examples
+// rgbToHex("rgb(0, 128, 192)") ➞ "#0080c0"
+// rgbToHex("rgb(45, 255, 192)") ➞ "#2dffc0"
+// rgbToHex("rgb(0, 0, 0)") ➞ "#000000"
+// Notes
+// The Hex format should be displayed in lowercase.
+
+function rgbToHex(col) {
+  const rgbArr = col.slice(4, col.length - 1).split(', ')
+  return '#' + rgbArr.map(idx => idx === '0' ? '00' : Number(idx).toString(16)).join('')
+}
+
 // There are three towers. The objective of the game is to move all the disks over to tower #3, but you can't place a larger disk onto a smaller disk. To play the game or learn more about the Tower of Hanoi, check the Resources tab.
 // Tower of Hanoi
 // Create a function that takes a number discs as an argument and returns the minimum amount of steps needed to complete the game.
