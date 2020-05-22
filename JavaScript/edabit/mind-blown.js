a2ce31a79d1da8e51920ef8de348b8e013ed3768
Jack Chou <jcacct@protonmail.ch>
2020-05-22 09:12:23 -0700

    Solved towerHanio

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,18 @@
+// There are three towers. The objective of the game is to move all the disks over to tower #3, but you can't place a larger disk onto a smaller disk. To play the game or learn more about the Tower of Hanoi, check the Resources tab.
+// Tower of Hanoi
+// Create a function that takes a number discs as an argument and returns the minimum amount of steps needed to complete the game.
+// Examples
+// towerHanoi(3) ➞ 7
+// towerHanoi(5) ➞ 31
+// towerHanoi(0) ➞ 0
+// Notes
+// The amount of discs is always a positive integer.
+
+function towerHanoi(discs) {
+  if (discs === 1) return 1
+  return Math.pow(2, discs) - 1
+}
+
 // Given a string of numbers separated by a comma and space, return the product of the numbers.
 // Examples
 // multiplyNums("2, 3") ➞ 6
