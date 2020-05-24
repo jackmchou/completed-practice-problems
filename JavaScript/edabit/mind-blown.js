ad34a4ebcaf30b5db7b02cab9808ae338af66ba4
Jack Chou <jcacct@protonmail.ch>
2020-05-24 07:39:51 -0700

    Solved Rock paper scissor

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -67,6 +67,26 @@ class Pagination {
   }
 }
 
+// Create a function which takes two strings (p1 and p2 ⁠— which represent player 1 and 2) as arguments and returns a string stating the winner in a game of Rock, Paper, Scissors.
+// Each argument will contain a single string: "Rock", "Paper", or "Scissors". Return the winner according to the following rules:
+//     Rock beats Scissors
+//     Scissors beats Paper
+//     Paper beats Rock
+// If p1 wins, return the string "The winner is p1". If p2 wins, return the string "The winner is p2" and if p1 and p2 are the same, return "It's a draw".
+// Examples
+// rps("Rock", "Paper") ➞ "The winner is p2"
+// rps("Scissors", "Paper") ➞ "The winner is p1"
+// rps("Paper", "Paper") ➞ "It's a draw"
+// Notes
+// All inputs will be valid strings.
+
+function rps(p1, p2) {
+  if (p1 === p2) return "It's a draw"
+  else if (p1 === 'Rock' && p2 === 'Scissors' || p1 === 'Scissors' && p2 === 'Paper' || p1 === 'Paper' && p2 === 'Rock') return 'The winner is p1'
+  if () return 'The winner is p1'
+  return 'The winner is p2'
+}
+
 // Create a function that takes numbers as arguments, adds them together, and returns the product of digits until the answer is only 1 digit long.
 // Examples
 // sumDigProd(16, 28) ➞ 6
