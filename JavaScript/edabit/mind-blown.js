80748859c4596dc19e5788c974adce16c5f55bad
Jack Chou <jcacct@protonmail.ch>
2020-05-25 09:10:37 -0700

    Solved ERA

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -67,6 +67,21 @@ class Pagination {
   }
 }
 
+// Create a function that returns an Earned Run Average (ERA). An ERA is calculated by multiplying 9 by the quotient of Earned Runs Allowed er divided by ip Innings Pitched.
+// In baseball statistics, innings are represented with a fractional part of .1 (1/3) or .2 (2/3) to represent the number of outs in an inning. A whole number or a number with a fractional part of .0 represents a full inning with three outs. Check the Resources tab for a deeper explanation.
+// Examples
+// era(22, 99) ➞ 2.00
+// era(23, 99.1) ➞ 2.08
+// era(24, 99.2) ➞ 2.17
+// Notes
+//     ERA is represented with a scale of 2: 2.08
+//     For 1/3 and 2/3, use a scale of 2.
+
+function era(er, ip) {
+  const eraNum = (er / ip) * 9
+  return (eraNum) % 1 == 0 ? (eraNum).toFixed(2) : ('' + eraNum).slice(0, 4)
+}
+
 // You can think of character classes as characters with special meaning. They are recognized as special when you place the \ before the character.
 // Here are a list of the characters classes in JavaScript:
 // ., \cX, \d, \D, \f, \n, \r, \s, \S, \t, \v, \w, \W, \0, \xhh, \uhhhh, \uhhhhh,
