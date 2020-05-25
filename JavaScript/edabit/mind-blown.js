a89c1971669b7b8a0bd550f4bd817428925dcc4b
Jack Chou <jcacct@protonmail.ch>
2020-05-25 07:59:48 -0700

    Solved letterAtPosition

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -67,6 +67,25 @@ class Pagination {
   }
 }
 
+// Given a number between 1-26, return what letter is at that position in the alphabet. Return "invalid" if the number given is not within that range, or isn't an integer.
+// Examples
+// letterAtPosition(1) ➞ "a"
+// letterAtPosition(26.0) ➞ "z"
+// letterAtPosition(0) ➞ "invalid"
+// letterAtPosition(4.5) ➞ "invalid"
+// Notes
+//     Return a lowercase letter.
+//     Numbers that end with ".0" are valid.
+
+function letterAtPosition(n) {
+  if (n == 0 || n % 1 != 0) return 'invalid'
+  let alphabetArr = ''
+  for (let i = 9; ++i < 36;) {
+    alphabetArr += (i).toString(36)
+  }
+  return alphabetArr[n - 1]
+}
+
 // function shirtSize({size = "big"}) {
 //   return size
 // }
