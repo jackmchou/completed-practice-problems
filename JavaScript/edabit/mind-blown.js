03b8a23768cacdc1ee27c3c19fbcf529387f1b4f
Jack Chou <jcacct@protonmail.ch>
2020-05-25 08:41:24 -0700

    Solved RegEx: Character Classes XII⁠I - hexidecimal

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -67,6 +67,18 @@ class Pagination {
   }
 }
 
+// You can think of character classes as characters with special meaning. They are recognized as special when you place the \ before the character.
+// Here are a list of the characters classes in JavaScript:
+// ., \cX, \d, \D, \f, \n, \r, \s, \S, \t, \v, \w, \W, \0, \xhh, \uhhhh, \uhhhhh,
+// You might get text that looks like it's all English characters but it very well may not be:
+// pànts !== pants
+// To ensure that you only get the characters you want in a string you will need to use the character classes that accept hexadecimal digits.
+// Create a regex that matches the word "edabit". You cannot use character classes \w, \d, [], or . to solve.
+// Notes
+// Use the handy text to hex converter tool in the Resources tab.
+
+const REGEXP = /\x65\x64\x61\x62\x69\x74/g
+
 // Create a function which validates whether a number n is exclusively within the bounds of lower and upper. Return false if n is not an integer.
 // Examples
 // intWithinBounds(3, 1, 9) ➞ true
