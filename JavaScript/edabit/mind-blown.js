d0ef99888436f50f98326b7ea79b97b3f57a7533
Jack Chou <jcacct@protonmail.ch>
2020-05-25 08:13:26 -0700

    Solved intWithinBound

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -67,6 +67,20 @@ class Pagination {
   }
 }
 
+// Create a function which validates whether a number n is exclusively within the bounds of lower and upper. Return false if n is not an integer.
+// Examples
+// intWithinBounds(3, 1, 9) ➞ true
+// intWithinBounds(6, 1, 6) ➞ false
+// intWithinBounds(4.5, 3, 8) ➞ false
+// Notes
+//     Exclusively means that a number is considered not within the bounds if it is equal to the upper bound (see example #2).
+//     Bounds will be always given as integers.
+
+function intWithinBounds(n, lower, upper) {
+  if (n % 1 != 0) return false 
+  else return lower <= n && n < upper
+}
+
 // Given a number between 1-26, return what letter is at that position in the alphabet. Return "invalid" if the number given is not within that range, or isn't an integer.
 // Examples
 // letterAtPosition(1) ➞ "a"
