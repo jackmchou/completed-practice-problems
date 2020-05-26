d436fb47c71afb720b204532ec64eb31760f483b
Jack Chou <jcacct@protonmail.ch>
2020-05-26 07:59:30 -0700

    solved reversed binary integer

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -67,6 +67,28 @@ class Pagination {
   }
 }
 
+// Write a function that takes an integer n, reverses the binary representation of that integer, and returns the new integer from the reversed binary.
+// Examples
+// reversedBinaryInteger(10) ➞ 5
+// // 10 = 1010 -> 0101 = 5
+// reversedBinaryInteger(12) ➞ 3
+// // 12 = 1100 -> 0011 = 3
+// reversedBinaryInteger(25) ➞ 19
+// // 25 = 11001 -> 10011 = 19
+// reversedBinaryInteger(45) ➞ 45
+// // 45 = 101101 -> 101101 = 45
+// Notes
+// All values of n will be positive.
+
+function reversedBinaryInteger(num) {
+  const binary = (num).toString(2)
+  let reversedBinary = []
+  for (let i = binary.length; --i >= 0;) {
+    reversedBinary.push(binary[i])
+  }
+  return parseInt(reversedBinary.join(''), 2)
+}
+
 // Given an array of 10 numbers, return the maximum possible total made by summing just 5 of the 10 numbers.
 // Examples
 // maxTotal([1, 1, 0, 1, 3, 10, 10, 10, 10, 1]) ➞ 43
