d879250c0ca4f2e6ff47b64b9dd266de93e53ec7
Jack Chou <jcacct@protonmail.ch>
2020-05-29 12:53:30 -0700

    Solved boomIntensity

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,37 @@ class Pagination {
   }
 }
 
+// Given a number, return a string of the word "Boom", which varies in the following ways:
+//     The string should include n number of "o"s, unless n is below 2 (in that case, return "boom").
+//     If n is evenly divisible by 2, add an exclamation mark to the end.
+//     If n is evenly divisible by 5, return the string in ALL CAPS.
+//     If n is evenly divisible by both 2 and 5, return the string in ALL CAPS and add an exclamation mark to the end.
+// The example below should help clarify these instructions.
+// Examples
+// boomIntensity(4) ➞ "Boooom!"
+// // There are 4 "o"s and 4 is divisible by 2 (exclamation mark included)
+// boomIntensity(1) ➞ "boom"
+// // 1 is lower than 2, so we return "boom"
+// boomIntensity(5) ➞ "BOOOOOM"
+// // There are 5 "o"s and 5 is divisible by 5 (all caps)
+// boomIntensity(10) ➞ "BOOOOOOOOOOM!"
+// // There are 10 "o"s and 10 is divisible by 2 and 5 (all caps and exclamation mark included)
+// Notes
+//     A number which is evenly divisible by 2 and 5 will have both effects applied (see example #4).
+//     "Boom" will always start with a capital "B", except when n is less than 2, then return a minature explosion as "boom".
+
+function boomIntensity(n) {
+	if (2 > n) return 'boom'
+  let result = 'B'
+  for (let i = 0; i < n; i++) {
+    result += 'o'
+  }
+  if (n % 2 === 0 && n % 5 === 0) return `${result.toUpperCase()}M!`
+  else if (n % 2 === 0) return `${result}m!`
+  else if (n % 5 === 0) return `${result.toUpperCase()}M`
+  else return `${result}m`
+}
+
 // Create a function that squares every digit of a number.
 // Examples
 // squareDigits(9119) ➞ 811181
@@ -73,7 +104,14 @@ class Pagination {
 // Notes
 // The function receives an integer and must return an integer.
 
-
+function squareDigits(n) {
+	const nStr = '' + n
+  let result = ''
+  for (let i = 0; i < nStr.length; i++) {
+    result += nStr[i] * nStr[i]
+  }
+  return +result
+}
 
 // Create a function that takes an array of strings and return an array, sorted from shortest to longest.
 // Examples
