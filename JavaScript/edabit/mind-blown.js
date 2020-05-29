97b5fce2f719ee14d39a8a00db8e55edca9265e4
Jack Chou <jcacct@protonmail.ch>
2020-05-29 12:24:40 -0700

    solved sqaureDigits

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,16 @@ class Pagination {
   }
 }
 
+// Create a function that squares every digit of a number.
+// Examples
+// squareDigits(9119) ➞ 811181
+// squareDigits(2483) ➞ 416649
+// squareDigits(3212) ➞ 9414
+// Notes
+// The function receives an integer and must return an integer.
+
+
+
 // Create a function that takes an array of strings and return an array, sorted from shortest to longest.
 // Examples
 // sortByLength(["Google", "Apple", "Microsoft"])
