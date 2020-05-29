2b18a1c1994f1ad160182708d21000816d690b31
Jack Chou <jcacct@protonmail.ch>
2020-05-29 11:52:19 -0700

    Solved sortByLength

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,31 @@ class Pagination {
   }
 }
 
+// Create a function that takes an array of strings and return an array, sorted from shortest to longest.
+// Examples
+// sortByLength(["Google", "Apple", "Microsoft"])
+// ➞ ["Apple", "Google", "Microsoft"]
+// sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"])
+// ➞ ["Raphael", "Leonardo", "Donatello", "Michelangelo"]
+// sortByLength(["Turing", "Einstein", "Jung"])
+// ➞ ["Jung", "Turing", "Einstein"]
+// Notes
+// All test cases contain arrays with strings of different lengths, so you won't have to deal with multiple strings of the same length.
+
+function sortByLength(arr) {
+  let tmp
+  for (let i = 0; i < arr.length; i++) {
+    for(let j = i + 1; j < arr.length; j++) {
+      if (arr[i].length > arr[j].length) {
+        tmp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = tmp
+      }
+    }
+  }
+  return arr
+}
+
 // Create a function that accepts a string of space separated numbers and returns the highest and lowest number (as a string).
 // Examples
 // highLow("1 2 3 4 5") ➞ "5 1"
