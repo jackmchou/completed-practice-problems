d33eee7a12978dc94e3665ca1d3ca1ff82fd2a07
Jack Chou <jcacct@protonmail.ch>
2020-05-29 13:25:19 -0700

    solved arrayOps

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,23 @@ class Pagination {
   }
 }
 
+// Create a function that takes three parameters and returns an array with the first parameter x, the second parameter y, and every number in between the first and second parameter in ascending order. Then filter through the array and return the array with numbers that are only divisible by the third parameter n.
+// Examples
+// arrayOperation(1, 10, 3) ➞ [3, 6, 9]
+// arrayOperation(7, 9, 2) ➞ [8]
+// arrayOperation(15, 20, 7) ➞ []
+// Notes
+//     The final array should consist of all numbers between x and y inclusive that are divisible by n.
+//     Return an empty array if there are no numbers that are divisible by n.
+
+function arrayOperation(x, y, n) {
+	let arr = []
+  for (x; x <= y;x++) {
+    if (x % n === 0) arr.push(x)
+  }
+  return arr
+}
+
 // Given a number, return a string of the word "Boom", which varies in the following ways:
 //     The string should include n number of "o"s, unless n is below 2 (in that case, return "boom").
 //     If n is evenly divisible by 2, add an exclamation mark to the end.
