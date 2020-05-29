0bf0f1d38058b9a228e5ccd603737d3071e198bc
Jack Chou <jcacct@protonmail.ch>
2020-05-29 13:27:26 -0700

    solved Promises III

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,24 @@ class Pagination {
   }
 }
 
+// Promises are just objects that contain the outcome of asynchronous operations. So when do you use one? When you want to control the outcome of an asynchronous operation. All you have to do is wrap the asynchronous function with a promise constructor.
+// The promise constructor requires you to pass a function called the executor which takes two parameters, resolve and reject. Both are functions that you use to pass or reject a value that is usually the result of the async operation. Here's an example of a simple promise:
+// let promise = new Promise( (resolve, reject) => {
+//   setTimeout(( ) => {
+//      resolve("edabit")
+//   }, 1000)
+// })
+// setTimeout is a built-in JavaScript function that is very commonly used in tutorials to represent async operations. After 1000ms has passed, we call the callback function in setTimeout() and pass a string "edabit" to the resolve function.
+// Create a simple promise and pass the resolve function a string value of your choice. Use the setTimeout function as your asynchronous operation. Your setTimeout() function should not exceed 1000ms. Store the promise inside a variable named promise.
+// Notes
+// Check the Resources tab for more info on promises.
+
+let promise = new Promise( (resolve, reject) => {
+  setTimeout(( ) => {
+     resolve("edabit")
+  }, 1000)
+})
+
 // Create a function that takes three parameters and returns an array with the first parameter x, the second parameter y, and every number in between the first and second parameter in ascending order. Then filter through the array and return the array with numbers that are only divisible by the third parameter n.
 // Examples
 // arrayOperation(1, 10, 3) ➞ [3, 6, 9]
