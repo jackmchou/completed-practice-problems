10cf5dcae999b6b48b6247dce188350a85d054a7
Jack Chou <jcacct@protonmail.ch>
2020-05-31 08:48:44 -0700

    Solved calculator

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,21 @@ class Pagination {
   }
 }
 
+// Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
+// Examples
+// calculator(2, "+", 2) ➞ 4
+// calculator(2, "*", 2) ➞ 4
+// calculator(4, "/", 2) ➞ 2
+// Notes
+// If the input tries to divide by 0, return: "Can't divide by 0!"
+
+function calculator(num1, operator, num2) {
+  if (operator == '+') return num1 + num2
+  if (operator == '-') return num1 - num2
+  if (operator == '*') return num1 * num2
+  if (operator == '/') return num1 / num2
+}
+
 // Create a function that returns true if two arrays contain identical values, and false otherwise.
 
 // To solve this question, your friend writes the following code:
