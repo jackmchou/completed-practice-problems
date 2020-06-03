af95e197637d3064a7b89edfe92a8675f751689e
Jack Chou <jcacct@protonmail.ch>
2020-06-03 07:07:42 -0700

    solved SolveForExp

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,18 @@ class Pagination {
   }
 }
 
+// Create a function that takes a number a and finds the missing exponent x so that a when raised to the power of x is equal to b.
+// Examples
+// solveForExp(4, 1024) ➞ 5
+// solveForExp(2, 1024) ➞ 10
+// solveForExp(9, 3486784401) ➞ 10
+// Notes
+// a is raised to the power of what in order to equal b?
+
+function solveForExp(a, b) {
+	return Math.round(Math.log(b) / Math.log(a))
+}
+
 // Write a function that takes an integer i and returns an integer with the integer backwards followed by the original integer.
 // To illustrate:
 // 123
