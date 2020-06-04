64ac6d33ab3412bb7ca50179d31a6dff0a5270ea
Jack Chou <jcacct@protonmail.ch>
2020-06-04 07:43:25 -0700

    Solved progressDays

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,25 @@ class Pagination {
   }
 }
 
+// To train for an upcoming marathon, Johnny goes on one long-distance run each Saturday. He wants to track how often the number of miles he runs this Saturday exceeds the number of miles run the previous Saturday. This is called a progress day.
+// Create a function that takes in an array of miles run every Saturday and returns Johnny's total number of progress days.
+// Examples
+// progressDays([3, 4, 1, 2]) ➞ 2
+// // There are two progress days, (3->4) and (1->2)
+// progressDays([10, 11, 12, 9, 10]) ➞ 3
+// progressDays([6, 5, 4, 3, 2, 9]) ➞ 1
+// progressDays([9, 9])  ➞ 0
+// Notes
+// Running the same number of miles as last week does not count as a progress day.
+
+function progressDays(runs) {
+	let days = 0 
+  for (let i = 0; i < runs.length; i++) {
+    if (runs[i] < runs[i + 1]) days++
+  }
+  return days
+}
+
 // Return the sum of all items in an array, where each item is multiplied by its index (zero-based). For empty arrays, return 0.
 // Examples
 // indexMultiplier([1, 2, 3, 4, 5]) ➞ 40
