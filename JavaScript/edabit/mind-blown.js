105f7d0818952fcae135486c05c5e7c8802898a7
Jack Chou <jcacct@protonmail.ch>
2020-06-04 07:26:05 -0700

    Solved indexmultiplier

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,23 @@ class Pagination {
   }
 }
 
+// Return the sum of all items in an array, where each item is multiplied by its index (zero-based). For empty arrays, return 0.
+// Examples
+// indexMultiplier([1, 2, 3, 4, 5]) ➞ 40
+// // (1*0 + 2*1 + 3*2 + 4*3 + 5*4)
+// indexMultiplier([-3, 0, 8, -6]) ➞ -2
+// // (-3*0 + 0*1 + 8*2 + -6*3)
+// Notes
+// All items in the array will be integers.
+
+function indexMultiplier(arr) {
+	let result = 0
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i] * i
+  }
+  return result
+}
+
 // Create a function that returns "even" if a number has an even number of factors and "odd" if a number has an odd number of factors.
 // Examples
 // factorGroup(33) ➞ "even"
