a6b4627f568e393dc81c6805128ef74093e9e0c1
Jack Chou <jcacct@protonmail.ch>
2020-06-04 08:17:20 -0700

    Solved mean

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,24 @@ class Pagination {
   }
 }
 
+// Create a function that returns the mean of all digits.
+// Examples
+// mean(42) ➞ 3
+// mean(12345) ➞ 3
+// mean(666) ➞ 6
+// Notes
+//     The mean of all digits is the sum of digits / how many digits there are (e.g. mean of digits in 512 is (5+1+2)/3(number of digits) = 8/3=2).
+//     The mean will always be a integer.
+
+function mean(num) {
+	let sum = 0
+  const numArr = ('' + num).split('')
+  for (let i = 0; i < numArr.length; i++) {
+    sum += +numArr[i]
+  }
+  return sum / numArr.length
+}
+
 // Create a function to convert an array of percentages to their decimal equivalents.
 // Examples
 // convertToDecimal(["1%", "2%", "3%"]) ➞ [0.01, 0.02, 0.03]
