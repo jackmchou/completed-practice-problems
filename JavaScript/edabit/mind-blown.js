461179844d1f7292fbd587e89d454d8152a55763
Jack Chou <jcacct@protonmail.ch>
2020-06-04 16:01:13 -0700

    Solved revNum

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -65,6 +65,24 @@ class Pagination {
   }
 }
 
+// Create a function that takes an integer n and reverses it.
+// Examples
+// rev(5121) ➞ "1215"
+// rev(69) ➞ "96"
+// rev(-122157) ➞ "751221"
+// Notes
+//     This challenge is about using two operators that are related to division.
+//     If the number is negative, treat it like it's positive.
+
+function rev(n) {
+	const numArr = ('' + n).split('')
+  let result = []
+  for (let i = numArr.length; i >= 0; i--) {
+    if (parseInt(numArr[i])) result.push(numArr[i])
+  }
+  return result.join('')
+}
+
 /**
  * Given an arbitrary input string, return the first nonrepeated character in
  * the string. For example:
