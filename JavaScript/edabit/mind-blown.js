d1ad07a8a6b63bccb5b4bb6351442cb21b1683e9
Jack Chou <jcacct@protonmail.ch>
2020-06-08 10:55:07 -0700

    solvedfactorize

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -21,6 +21,25 @@ function removeLeadingTrailing(n) {
 }
 removeLeadingTrailing("03.1400")
 
+
+// Create a function that takes a number as its argument and returns an array of all its factors.
+// Examples
+// factorize(12) ➞ [1, 2, 3, 4, 6, 12]
+// factorize(4) ➞ [1, 2, 4]
+// factorize(17) ➞ [1, 17]
+// Notes
+//     The input integer will be positive.
+//     A factor is a number that evenly divides into another number without leaving a remainder. The second example is a factor of 12, because 12 / 2 = 6, with remainder 0.
+
+function factorize(num) {
+  const factors = []
+	for (let i = 0; i <= num; i++) {
+    if (num % i == 0) factors.push(i)
+  }
+  return factors
+}
+factorize(24)
+
 // Create a function that takes an array of names and returns an array where only the first letter of each name is capitalized.
 // Examples
 // capMe(["mavis", "senaida", "letty"]) ➞ ["Mavis", "Senaida", "Letty"]
