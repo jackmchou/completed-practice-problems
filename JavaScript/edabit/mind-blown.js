299e25e520a5209ec73f3963dc2ef1832d770af8
Jack Chou <jcacct@protonmail.ch>
2020-06-08 18:32:07 -0700

    Solved Find NaN in an Array

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -21,6 +21,21 @@ function removeLeadingTrailing(n) {
 }
 removeLeadingTrailing("03.1400")
 
+// Create a function to find NaN in an array of numbers. The return value should be the index where NaN is found. If NaN is not found in the array, then return -1.
+// Examples
+// findNaN([1, 2, NaN]) ➞ 2
+// findNaN([NaN, 1, 2, 3, 4]) ➞ 0
+// findNaN([0, 1, 2, 3, 4]) ➞ -1
+// Notes
+// Inputs are array of numbers.
+
+function findNaN(number) {
+	for (let i = 0; i < number.length; i++) {
+    if (!(number[i] === number[i])) return i
+  }
+  return -1
+}
+
 // Given a simple math expression as a string, neatly format it as an equation.
 // Examples
 // formatMath("3 + 4") ➞ "3 + 4 = 7"
