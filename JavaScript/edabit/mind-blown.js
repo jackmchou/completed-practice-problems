78e434f39c373a9d442adc3b4aab64539c96e85f
Jack Chou <jcacct@protonmail.ch>
2020-06-11 08:58:05 -0700

    Solved RegEx: Character Classes XI ⁠- \S

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,15 @@
+// You can think of character classes as characters with special meaning. They are recognized as special when you place the \ before the character.
+// Here are a list of the characters classes in JavaScript:
+// ., \cX, \d, \D, \f, \n, \r, \s, \S, \t, \v, \w, \W, \0, \xhh, \uhhhh, \uhhhhh, [\b]
+// Write a regex that will return true if the bio does not have any spaces before the last ending punctuation ?. You must use the \S character class in your expression.
+// "Can read a spray chart and a balance sheet. 1 part Executive, 1 part entrepreneur, 2 parts geek and 3 parts baseball coach. Too many parts?"
+// The bio above is in the correct format. Therefore your regex should return true for it.
+// Notes
+//     Check the Resources tab for details on character classes if you're stuck.
+//     Mark Gallion's twitter bio is used for educational purposes only.
+
+const REGEXP = /\S[\?]/g
+
 // Create a function that takes an array and returns the sum of all items in the array.
 // Examples
 // sumArray([1, 2, 3]) ➞ 6
