b351dcc04ef595afcee3fea2a8bbb86adb1bd26e
Jack Chou <jcacct@protonmail.ch>
2020-06-11 08:30:19 -0700

    Solved Sum of the Items in an Array

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,16 @@
+// Create a function that takes an array and returns the sum of all items in the array.
+// Examples
+// sumArray([1, 2, 3]) ➞ 6
+// // 1 + 2 + 3 = 6
+// sumArray([1, [2, [1]], 3]) ➞ 7
+// // 1 + 2 + 1 + 3 = 7
+// Notes
+// The item in an array can be another array.
+
+function sumArray(arr) {
+	return arr.flat(Infinity).reduce((cur, acc) => cur + acc, 0)
+}
+
 // Create a function that takes four arrays as arguments and returns a count of the total number of identical arrays.
 // Examples
 // countIdenticalArrays([0, 0, 0], [0, 1, 2], [0, 0, 0], [2, 1, 0]) ➞ 2
