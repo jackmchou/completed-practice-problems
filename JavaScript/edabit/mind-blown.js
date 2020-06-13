53fc303478018492c23a56dfc194e075a683e200
Jack Chou <jcacct@protonmail.ch>
2020-06-13 14:48:54 -0700

    Solved Edabit Experience Points

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,50 @@
+// As you complete questions on Edabit, you gain experience points depending on the difficulty of the question. The points for each difficulty are as follows:
+// Difficulty	Experience Points
+// Very Easy	5XP
+// Easy	10XP
+// Medium	20XP
+// Hard	40XP
+// Very Hard	80XP
+// Given an object of how many questions a person has completed of each difficulty, return how many experience points they'll have.
+// Examples
+// getXP({
+//   "Very Easy" : 89,
+//   "Easy" : 77,
+//   "Medium" : 30,
+//   "Hard" : 4,
+//   "Very Hard" : 1
+// }) ➞ "2055XP"
+// getXP({
+//   "Very Easy" : 254,
+//   "Easy" : 32,
+//   "Medium" : 65,
+//   "Hard" : 51,
+//   "Very Hard" : 34
+// }) ➞ "7650XP"
+// getXP({
+//   "Very Easy" : 11,
+//   "Easy" : 0,
+//   "Medium" : 2,
+//   "Hard" : 0,
+//   "Very Hard" : 27
+// }) ➞ "2255XP"
+// Notes
+// Return values as a string and make sure to add "XP" to the end.
+
+function getXP(obj) {
+  const exp = {	
+    'Very Easy' : 5,
+	  'Easy' : 10,
+	  'Medium' : 20,
+	  'Hard' : 40,
+	  'Very Hard' : 80
+  }
+	for (let key in obj) {
+    obj[key] *= exp[key]
+  }
+  return `${Object.values(obj).reduce((acc, cur) => acc + cur, 0)}XP`
+}
+
 // Given a fraction frac (given in the format "1/2" for example) and n number of decimal places, return a sentence in the following format:
 // "{fraction} rounded to {n} decimal places is {answer}"
 // Examples
