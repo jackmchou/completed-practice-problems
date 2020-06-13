9c2629100a3fd269e22443b44070b3c2647b6018
Jack Chou <jcacct@protonmail.ch>
2020-06-13 15:23:02 -0700

    Solved Check If objOne Is Equal to objTwo

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,39 @@
+// Create a function that checks to see if two object arguments are equal to one another. Return true if the objects are equal, otherwise, return false.
+// Example #1
+// // The first object parameter.
+// {
+//   name: "Benny",
+//   phone: "3325558745",
+//   email: "benny@edabit.com"
+// }
+// // The second object parameter.
+// {
+//   name: "Jason",
+//   phone: "9853759720",
+//   email: "jason@edabit.com"
+// }
+// ➞ false
+// Example #2
+// // The first object parameter.
+// {
+//   name: "Jason",
+//   phone: "9853759720",
+//   email: "jason@edabit.com"
+// }
+// // The second object parameter.
+// {
+//   name: "Jason",
+//   phone: "9853759720",
+//   email: "jason@edabit.com"
+// }
+// ➞ true
+// Notes
+// If you have a suggestion on how to make these instructions easier to understand, please leave a comment. Your feedback is greatly appreciated.
+
+function isEqual(objOne, objTwo) {
+  return JSON.stringify(objOne) === JSON.stringify(objTwo)
+}
+
 // As you complete questions on Edabit, you gain experience points depending on the difficulty of the question. The points for each difficulty are as follows:
 // Difficulty	Experience Points
 // Very Easy	5XP
