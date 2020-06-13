49844fb656d7dfb47d3bfd751c2ba2b791e9bc32
Jack Chou <jcacct@protonmail.ch>
2020-06-13 14:22:57 -0700

    Solved Fractions and Rounding

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// Given a fraction frac (given in the format "1/2" for example) and n number of decimal places, return a sentence in the following format:
+// "{fraction} rounded to {n} decimal places is {answer}"
+// Examples
+// fracRound("1/3", 5) ➞ "1/3 rounded to 5 decimal places is 0.33333"
+// fracRound("2/8", 4) ➞ "2/8 rounded to 4 decimal places is 0.2500"
+// fracRound("22/7", 2) ➞ "22/7 rounded to 2 decimal places is 3.14"
+// Notes
+//     Add trailing zeros if n is greater than the actual number of decimal places the fraction has (see example #2).
+//     Numbers greater than one may be given as top-heavy fractions (no mixed numbers).
+//     n won't be 1 because that would cause "decimal places" to be "decimal place", making the challenge more cumbersome than it needs to be.
+
+function fracRound(frac, n) {
+	const operandIdx = frac.indexOf('/')
+  const answer = +frac.slice(0, operandIdx) / frac.slice(operandIdx + 1)
+  return `${frac} rounded to ${n} decimal places is ${answer.toFixed(n)}`
+}
+
 // Create a function that takes a word and returns true if the word has two consecutive identical letters.
 // Examples
 // doubleLetters("loop") ➞ true
