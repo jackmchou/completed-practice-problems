bf6848113c04561764888b6f18d2549308898315
Jack Chou <jcacct@protonmail.ch>
2020-06-14 10:41:52 -0700

    Solved RegEx: Character Classes XII ⁠- \t

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,14 @@
+// You can think of character classes as characters with special meaning. They are recognized as special when you place the \ before the character.
+// Here are a list of the characters classes in JavaScript:
+// ., \cX, \d, \D, \f, \n, \r, \s, \S, \t, \v, \w, \W, \0, \xhh, \uhhhh, \uhhhhh, [\b]
+// We have the \t and \v character classes that match any tabs in a string. But more specifically \t is used to match horizontal tabs while \v matches vertical tabs. Vertical tabs were once a thing but are rarely used anymore. We generally use horizontal tabs which are produced by the tab key on our keyboards.
+// Find how many tabs with one literal whitespace immediately following the tab are in a string.
+// Notes
+//     Not all whitespaces are the same.
+//     See Resources for help.
+
+const REGEXP = /\t /g
+
 // Create a function which adds spaces before every capital in a word. Uncapitalize the whole string afterwards.
 // Examples
 // capSpace("helloWorld") ➞ "hello world"
