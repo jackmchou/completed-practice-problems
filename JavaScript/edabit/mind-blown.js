d7ef0a3bb6d2f4f2db11cddcaf0b15a631384e39
Jack Chou <jcacct@protonmail.ch>
2020-06-14 10:52:49 -0700

    Solved RegEx XIV: Group Ranges - Character Sets

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,23 @@
+// Groups and ranges indicate groups and ranges of expression characters. Character Sets match any characters inside of brackets [ ]. You can specify a range of characters by using a hyphen.
+// /[abcd]/ === /[a-d]/
+// If the hyphen appears as the first or last character then it is considered a literal hyphen.
+// "non-profit".match(/[abc-]/g)  // "-"
+// "non-profit".match(/[-abc]/g)  // "-"
+// You can also use character classes in a character set. So instead of this:
+// /[A-Za-z0-9_-]/
+// You can do this:
+// /[\w]/
+// Create the regex to match "x" followed by two digits or letters from A to F. Case sensitive.
+// Examples
+// "Exception 0xAF".match(REGEXP) ➞ "xAF"
+// "Exception 0xD3".match(REGEXP) ➞ "xD3"
+// "Exception 0xd3".match(REGEXP) ➞ null
+// "Exception 0xZZ".match(REGEXP) ➞ null
+// Notes
+// Check the Resources tab if you get stuck.
+
+const REGEXP = /x[A-F\d]{2}/g
+
 // You can think of character classes as characters with special meaning. They are recognized as special when you place the \ before the character.
 // Here are a list of the characters classes in JavaScript:
 // ., \cX, \d, \D, \f, \n, \r, \s, \S, \t, \v, \w, \W, \0, \xhh, \uhhhh, \uhhhhh, [\b]
