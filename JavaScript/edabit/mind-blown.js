d5cc918f32c25bd46bef2e3cefbacfc7a39ce168
Jack Chou <jcacct@protonmail.ch>
2020-06-16 12:53:10 -0700

    Solved Find the Second Occurrence of zip in a String

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,14 @@
+// Write a function that returns the position of the second occurrence of "zip" in a string, or -1 if it does not occur at least twice. Your code should be general enough to pass every possible case where "zip" can occur in a string.
+// Examples
+// findZip("all zip files are zipped") ➞ 18
+// findZip("all zip files are compressed") ➞ -1
+// Notes
+// Uppercase "Zip" is not the same as lowercase "zip".
+
+function findZip(str) {
+  return str.indexOf('zip') !== str.lastIndexOf('zip') ?str.split('zip', 2).join('zip').length : -1
+}
+
 // Write a function that returns an anonymous function, which adds n to its input
 // Examples
 // adds1 = addsNum(1)
