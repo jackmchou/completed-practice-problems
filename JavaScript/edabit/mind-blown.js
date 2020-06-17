184543098cae72e23f3824e3791a11d7704abdea
Jack Chou <jcacct@protonmail.ch>
2020-06-17 10:55:03 -0700

    Solved Create a Phone Number

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,15 @@
+// Write a function that takes an array of 10 integers (between 0 and 9) and returns a string in form of a phone number.
+// Examples
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ➞ "(123) 456-7890"
+// createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) ➞ "(111) 111-1111"
+// createPhoneNumber([8, 7, 4, 1, 2, 5, 6, 5, 8, 2]) ➞ "(874) 125-6582"
+// Notes
+// N/A
+
+function createPhoneNumber(numbers) {
+	return `(${numbers.splice(0, 3).join('')}) ${numbers.splice(0, 3).join('')} - ${numbers.slice(0, 4).join('')}`
+}
+
 // Create a function which takes a parameter n and returns a function such that it, when called n times, returns the string "edabit".
 // Examples
 // lambdaDepth(0) ➞ "edabit"
