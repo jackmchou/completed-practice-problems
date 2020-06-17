bc638040b1b0bbbd144f4a091a576966792dea77
Jack Chou <jcacct@protonmail.ch>
2020-06-17 10:54:29 -0700

    Solved Anonymous Functions All The Way Down

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,17 @@
+// Create a function which takes a parameter n and returns a function such that it, when called n times, returns the string "edabit".
+// Examples
+// lambdaDepth(0) ➞ "edabit"
+// lambdaDepth(1)() ➞ "edabit"
+// lambdaDepth(2)()() ➞ "edabit"
+// typeof lambdaDepth(2)() ➞ "function"
+// Notes
+//     num will always be a non-negative integer.
+//     If num == 0, return "edabit".
+//     If num > 0, return a function.
+//     All non-example test cases come in two forms: checking whether lambda_depth(k), after being called k times, returns a string, and checking whether lambda_depth(k) returns a function.
+
+lambdaDepth = n => eval('() => '.repeat(n) + '"edabit"')
+
 // Given a number, n, return a function which adds n to the number passed to it.
 // Examples
 // add(10)(20) ➞ 30
