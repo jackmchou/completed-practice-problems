84638849abfbace1153aa78941e358a2a0222716
Jack Chou <jcacct@protonmail.ch>
2020-06-18 09:29:20 -0700

    Solved Format Number with Comma(s) Separating Thousands

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,15 @@
+// Create a function that takes a number as an argument and returns a string formatted to separate thousands.
+// Examples
+// formatNum(1000) ➞ "1,000"
+// formatNum(100000) ➞ "100,000"
+// formatNum(20) ➞ "20"
+// Notes
+// You can expect a valid number for all test cases.
+
+function formatNum(num) {
+	return num.toLocaleString();
+}
+
 // An isogram is a word that has no repeating letters, consecutive or nonconsecutive. Create a function that takes a string and returns either true or false depending on whether or not it's an "isogram".
 // Examples
 // isIsogram("Algorism") ➞ true
