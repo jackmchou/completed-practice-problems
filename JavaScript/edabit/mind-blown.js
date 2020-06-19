871b6ac0a64b1c5fd2306802567991f9eaad0cc4
Jack Chou <jcacct@protonmail.ch>
2020-06-18 17:01:11 -0700

    Solved Reverse the Order of Words with Five Letters or More

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,21 @@
+// Write a function that takes a string of one or more words as an argument and returns the same string, but with all five or more letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+// Examples
+// reverse("Reverse") ➞ "esreveR"
+// reverse("This is a typical sentence.") ➞ "This is a lacipyt .ecnetnes"
+// reverse("The dog is big.") ➞ "The dog is big."
+// Notes
+// You can expect a valid string to be provided for each test case.
+
+function reverse(str) {
+	const strArr = [...str.split(' ')]
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i].length > 4) {
+      strArr[i] = [...strArr[i]].reverse().join('')
+    }
+  }
+  return strArr.join(' ')
+}
+
 // Create a function that takes a number as an argument and returns a string formatted to separate thousands.
 // Examples
 // formatNum(1000) ➞ "1,000"
