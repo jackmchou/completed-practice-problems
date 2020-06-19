c78751285fb190e92bda75edd6cea072678e12f1
Jack Chou <jcacct@protonmail.ch>
2020-06-18 18:43:46 -0700

    Solved ATM PIN Code Validation

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. Your task is to create a function that takes a string and returns true if the PIN is valid and false if it's not.
+// Examples
+// validatePIN("1234") ➞ true
+// validatePIN("12345") ➞ false
+// validatePIN("a234") ➞ false
+// validatePIN("") ➞ false
+// Notes
+//     Some test cases contain special characters.
+//     Empty strings must return false.
+
+function validatePIN(pin) {
+  // return /([\d]{6}|[\d]{4})/.test(pin)
+  if (!pin) return false
+  if (pin.length === 4 || pin.length === 6) {
+    for (let i = 0; i < pin.length; i++) {
+      if (isNaN(parseInt(pin[i]))) return false
+    }
+  } else return false
+  return true
+}
+
 // Write a function that takes a string of one or more words as an argument and returns the same string, but with all five or more letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 // Examples
 // reverse("Reverse") ➞ "esreveR"
