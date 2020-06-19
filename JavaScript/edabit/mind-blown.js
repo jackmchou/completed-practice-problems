4073fea54532fd5768ca3b1795fc5396524b32c8
Jack Chou <jcacct@protonmail.ch>
2020-06-19 09:34:09 -0700

    Solved Maskify the String

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// Usually when you sign up for an account to buy something, your credit card number, phone number or answer to a secret question is partially obscured in some way. Since someone could look over your shoulder, you don't want that shown on your screen. Hence, the website masks these strings.
+// Your task is to create a function that takes a string, transforms all but the last four characters into "#" and returns the new masked string.
+// Examples
+// maskify("4556364607935616") ➞ "############5616"
+// maskify("64607935616") ➞ "#######5616"
+// maskify("1") ➞ "1"
+// maskify("") ➞ ""
+// Notes
+//     The maskify function must accept a string of any length.
+//     An empty string should return an empty string (fourth example above).
+
+function maskify(str) {
+	if (str == '') return ''
+  const strArr = [...str]
+  return strArr.length > 4 ? strArr.fill('#', 0, strArr.length - 4).join('') : strArr.join('')
+}
+
 // Create a function that takes an integer and returns it as an ordinal number. An Ordinal Number is a number that tells the position of something in a list, such as 1st, 2nd, 3rd, 4th, 5th etc.
 // Examples
 // returnEndOfNumber(553) ➞ "553-RD"
