8e4fe977428c603561baf6b08bb14dbca3b0a7ce
Jack Chou <jcacct@protonmail.ch>
2020-06-21 16:59:27 -0700

    Solved Halve and Halve Again

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// Given two integers a and b, return how many times a can be halved while still being greater than b.
+// Examples
+// halveCount(1324, 98) ➞ 3
+// // (1324 -> 662 -> 331 -> 165.5)
+// halveCount(624, 8) ➞ 6
+// // (624 -> 312 -> 156 -> 78 -> 39 -> 19.5 -> 9.75)
+// halveCount(1000, 3) ➞ 8
+// // (1000 -> 500 -> 250 -> 125 -> 62.5 -> 31.25 -> 15.625 -> 7.8125 -> 3.90625)
+// Notes
+// Integer a can be halved at least once.
+
+function halveCount(a, b) {
+	let count = 0
+  while ((a /= 2) > b) count++
+  return count
+}
+
 // Return true if the sum of ASCII values of the first string is same as the sum of ASCII values of the second string, otherwise return false.
 // Examples
 // sameAscii("a", "a") ➞ true
