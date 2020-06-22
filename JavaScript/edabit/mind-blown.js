c797606b43730329887073497a11da130e30a620
Jack Chou <jcacct@protonmail.ch>
2020-06-22 09:32:19 -0700

    Solved Preventing the Collapse of the Universe

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,17 @@
+// Dividing by 0 is a huge mistake and should be avoided at all costs.
+// Create a function that when given a math expression as a string, return True if at any point, the expression involves dividing by 0.
+// Examples
+// catchZeroDivision("2 / 0") ➞ true
+// catchZeroDivision("4 / (2 + 3 - 5)") ➞ true
+// catchZeroDivision("2 * 5 - 10") ➞ false
+// Notes
+// Multiplication signs will be given as an asterisk *.
+
+function catchZeroDivision(expr) {
+  if (isFinite(eval(expr))) return false
+  return true
+}
+
 // Given two integers a and b, return how many times a can be halved while still being greater than b.
 // Examples
 // halveCount(1324, 98) ➞ 3
