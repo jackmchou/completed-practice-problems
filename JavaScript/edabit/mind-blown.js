3ad53e260c44119e426852f4d6060c52119dfe7e
Jack Chou <jcacct@protonmail.ch>
2020-06-24 18:20:57 -0700

    Solved Same Parity?

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Create a function that takes a number as input and returns true if the sum of its digits has the same parity as the entire number. Otherwise, return false.
+// Examples
+// parityAnalysis(243) ➞ true
+// // 243 is odd and so is 9 (2 + 4 + 3)
+// parityAnalysis(12) ➞ false
+// // 12 is even but 3 is odd (1 + 2)
+// parityAnalysis(3) ➞ true
+// // 3 is odd and 3 is odd and 3 is odd (3)
+// Notes
+//     Parity is whether a number is even or odd. If the sum of the digits is even and the number itself is even, return true. The same goes if the number is odd and so is the sum of its digits.
+//     Single digits will obviously have the same parities (see example #3).
+
+function parityAnalysis(num) {
+	let sumNum = 0
+  const numArr = [...('' + num)]
+  for (let i = 0; i < numArr.length; i++) {
+    sumNum += +numArr[i]
+  }
+  return (sumNum % 2 === 0) === (num % 2 === 0)
+}
+
 // Return the smallest number of steps it takes to convert a string entirely into uppercase or entirely into lower case, whichever takes the fewest number of steps. A step consists of changing one character from lower to upper case, or vice versa.
 // Examples
 // stepsToConvert("abC") ➞ 1
