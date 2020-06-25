139cfb91a0c7ab56147db312858e7d71e578238e
Jack Chou <jcacct@protonmail.ch>
2020-06-25 09:20:34 -0700

    Solved RegEx XVIII : Quantifiers - Control greed quantifiers with ?

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,14 @@
+// Quantifiers indicate numbers of characters or expressions to match. By default quantifiers like * and + are "greedy", meaning that they try to match as much of the string as possible. The ? character after the quantifier makes the quantifier "non-greedy": meaning that it will stop as soon as it finds a match.
+// "some <foo> <bar> new </bar> </foo> thing".match(/<.*>/) ➞ "<foo> <bar> new </bar> </foo>"
+// "some <foo> <bar> new </bar> </foo> thing".match(/<.*?>/) ➞ "<foo>"
+// Find all HTML comments in the text. Use the ? in your expression.
+// let str = `... <!-- My -- comment test --> ..  <!----> .. `;
+// str.match(regexp) ➞ "<!-- My -- comment test -->", "<!---->"
+// Notes
+// Check the Resources if you get stuck.
+
+const REGEXP = /<!--.*?-->/g
+
 // Given an array of prices prices and a "supposed" total t, return true if there is a hidden fee added to the total (i.e. the total is greater than the sum of prices), otherwise return false.
 // Examples
 // hasHiddenFee(["$2", "$4", "$1", "$8"], "$15") ➞ false
