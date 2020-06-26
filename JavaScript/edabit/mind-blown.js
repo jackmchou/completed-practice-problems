1f9773aa71845877252938bd1208e7f2cd26fe87
Jack Chou <jcacct@protonmail.ch>
2020-06-26 12:54:41 -0700

    Solved Alternating Ones and Zeroes

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,23 @@
+// Write a function that returns true if the binary string can be rearranged to form a string of alternating 0s and 1s.
+// Examples
+// canAlternate("0001111") ➞ true
+// // Can make: "1010101"
+// canAlternate("01001") ➞ true
+// // Can make: "01010"
+// canAlternate("010001") ➞ false
+// canAlternate("1111") ➞ false
+// Notes
+//     No substring of the output may contain more than one consecutive repeating character (e.g. 00 or 11 are not allowed).
+//     Return false if a string only contains 0s or only contains 1s.
+
+function canAlternate(s, zeros = 0, ones = 0) {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '0') zeros++
+    else ones++
+  }
+  return zeros === ones || zeros - 1 === ones || zeros + 1 === ones
+}
+
 // Write a function that returns the longest sequence of consecutive zeroes in a binary string.
 // Examples
 // longestZero("01100001011000") ➞ "0000"
