bcaa99bbc24546d9b0ab59f0b8f48e0a96ee8e64
Jack Chou <jcacct@protonmail.ch>
2020-06-27 09:33:59 -0700

    Solved Return an Array of Subarrays

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,27 @@
+// Write a function that takes three arguments (x, y, z) and returns an array containing x subarrays (e.g. [[], [], []]), each containing y number of item z.
+//     x Number of subarrays contained within the main array.
+//     y Number of items contained within each subarray.
+//     z Item contained within each subarray.
+// Examples
+// matrix(3, 2, 3) ➞ [[3, 3], [3, 3], [3, 3]]
+// matrix(2, 1, "edabit") ➞ [["edabit"], ["edabit"]]
+// matrix(3, 2, 0) ➞ [[0, 0], [0, 0], [0, 0]]
+// Notes
+//     The first two arguments will always be integers.
+//     The third argument is either a string or an integer.
+
+function matrix(x, y, z) {
+	// return Array(x).fill(Array(y).fill(z))
+  const mainArr = []
+  mainArr.length = x
+  for (let i = 0; i < x; i++) {
+    mainArr[i] = []
+    mainArr[i].length = y
+    mainArr[i].fill(z)
+  }
+  return mainArr
+}
+
 // Given the month and year as numbers, return whether that month contains a Friday 13th.
 // Examples
 // hasFriday13(3, 2020) ➞ true
