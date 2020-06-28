5effb8827c3594003ca1a7c14963b174c36354a5
Jack Chou <jcacct@protonmail.ch>
2020-06-28 09:40:49 -0700

    Solved Ping Pong!

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,32 @@
+// A game of table tennis almost always sounds like Ping! followed by Pong! Therefore, you know that Player 2 has won if you hear Pong! as the last sound (since Player 1 didn't return the ball back).
+// Given an array of Ping!, create a function that inserts Pong! in between each element. Also:
+//     If win equals true, end the list with Pong!.
+//     If win equals false, end with Ping! instead.
+// Examples
+// pingPong(["Ping!"], true) ➞ ["Ping!", "Pong!"]
+// pingPong(["Ping!", "Ping!"], false) ➞ ["Ping!", "Pong!", "Ping!"]
+// pingPong(["Ping!", "Ping!", "Ping!"], true) ➞ ["Ping!", "Pong!", "Ping!", "Pong!", "Ping!", "Pong!"]
+// Notes
+//     You will always return the ball (i.e. the Pongs are yours).
+//     Player 1 serves the ball and makes Ping!.
+//     Return an array of strings.
+
+function pingPong(arr, win) {
+  if (arr.length === 1) {
+    arr[arr.length] = 'Pong!'
+    return arr
+  }
+	for (let i = arr.length - 1; i >= 1; i--) {
+    if (win === true) {
+      arr.splice(i, 0, 'Pong!')
+      arr[arr.length] = 'Pong!'
+      arr.length = 6
+    }
+    else arr.splice(i, 0, 'Pong!')
+  }
+  return arr
+}
+
 // Write a function that takes three arguments (x, y, z) and returns an array containing x subarrays (e.g. [[], [], []]), each containing y number of item z.
 //     x Number of subarrays contained within the main array.
 //     y Number of items contained within each subarray.
