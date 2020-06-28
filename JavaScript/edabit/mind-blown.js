5a8c04fded31e5674e9e277c881c2acfb6c297d8
Jack Chou <jcacct@protonmail.ch>
2020-06-28 11:05:33 -0700

    Solved Compare by ASCII Codes

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,18 @@
+// Create a function that compares two words based on the sum of their ASCII codes and returns the word with the smaller ASCII sum.
+// Examples
+// asciiSort(["hey", "man"]) ➞ "man"
+// // ["h", "e", "y"] ➞ sum([104, 101, 121]) ➞ 326
+// // ["m", "a", "n"] ➞ sum([109, 97, 110]) ➞ 316
+// asciiSort(["majorly", "then"]) ➞ "then"
+// asciiSort(["victory", "careless"]) ➞ "victory"
+// Notes
+// Both words will have strictly different ASCII sums.
+
+function asciiSort(arr) {
+	const asciiArr = arr.map(ele => ele.split('').reduce((cur, acc) => cur + acc.charCodeAt(0), 0))
+  return arr[asciiArr.indexOf(Math.min.apply(null, asciiArr))]
+}
+
 // Create a function that returns true if a number is prime and false if it's not. A prime number is any positive integer that is evenly divisible by only two divisors: 1 and itself. The first ten prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29.
 // Examples
 // isPrime(7) ➞ true
