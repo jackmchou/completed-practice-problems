c9be374c199f4b10f00b7259e04c989188bf7eb8
Jack Chou <jcacct@protonmail.ch>
2020-06-30 09:28:39 -0700

    Solved Reverse Coding Challenge #6

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,17 @@
+// This is a reverse coding challenge. Normally you're given explicit directions with how to create a function. Here, you must generate your own function to satisfy the relationship between the inputs and outputs.
+// Your task is to create a function that, when fed the inputs below, produces the sample outputs shown.
+// Examples
+// mysteryFunc(152) ➞ 10
+// mysteryFunc(832) ➞ 48
+// mysteryFunc(19) ➞ 9
+// mysteryFunc(133) ➞ 9
+// Notes
+// N/A
+
+function mysteryFunc(num) {
+  return [...('' + num)].reduce((cur, acc) => cur * +acc)
+}
+
 // Imagine a school that kids attend for 6 years. In each year, there are five groups started, marked with the letters a, b, c, d, e. For the first year, the groups are 1a, 1b, 1c, 1d, 1e and for the last year, the groups are 6a, 6b, 6c, 6d, 6e.
 // Write a function that returns the groups in the school by year (as a string), separated with a comma and a space in the form of "1a, 1b, 1c, 1d, 1e, 2a, 2b (....) 5d, 5e, 6a, 6b, 6c, 6d, 6e".
 // Examples
