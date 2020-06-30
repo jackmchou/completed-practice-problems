a05d44eb60f273f516540135c683068cde82402d
Jack Chou <jcacct@protonmail.ch>
2020-06-30 11:43:08 -0700

    Solved Fix the Spacing

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,18 @@
+// Additional spaces have been added to a sentence. Return the correct sentence by removing them. All words should be separated by one space, and there should be no spaces at the beginning or end of the sentence.
+// Examples
+// correctSpacing("The film   starts       at      midnight. ")
+// ➞ "The film starts at midnight."
+// correctSpacing("The     waves were crashing  on the     shore.   ")
+// ➞ "The waves were crashing on the shore."
+// correctSpacing(" Always look on    the bright   side of  life.")
+// ➞ "Always look on the bright side of life."
+// Notes
+// N/A
+
+function correctSpacing(sentence) {
+	return sentence.trim().split(' ').filter(ele => ele !== '').join(' ')
+}
+
 // The Code tab has a code which attempts to add a clone of an array to itself. There is no error message, but the results are not as expected. Can you fix the code?
 // Examples
 // clone([1, 2, 3]) ➞ [1, 2, 3, [1, 2, 3]]
