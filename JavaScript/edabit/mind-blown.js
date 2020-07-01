29ccb6595e168d30875fa7628661ab22c6244986
Jack Chou <jcacct@protonmail.ch>
2020-07-01 07:39:15 -0700

    Solved Number of Boomerangs

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,26 @@
+// A boomerang is a V-shaped sequence that is either upright or upside down. Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different.
+// Some boomerang examples: [3, 7, 3], [1, -1, 1], [5, 6, 5]
+// Create a function that returns the total number of boomerangs in an array.
+// To illustrate:
+// [3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2]
+// // 3 boomerangs in this sequence:  [3, 7, 3], [1, 5, 1], [2, -2, 2]
+// Be aware that boomerangs can overlap, like so:
+// [1, 7, 1, 7, 1, 7, 1]
+// // 5 boomerangs (from left to right): [1, 7, 1], [7, 1, 7], [1, 7, 1], [7, 1, 7], and [1, 7, 1]
+// Examples
+// countBoomerangs([9, 5, 9, 5, 1, 1, 1]) ➞ 2
+// countBoomerangs([5, 6, 6, 7, 6, 3, 9]) ➞ 1
+// countBoomerangs([4, 4, 4, 9, 9, 9, 9]) ➞ 0
+// Notes
+// [5, 5, 5] (triple identical digits) is NOT considered a boomerang because the middle digit is identical to the first and last.
+
+function countBoomerangs(arr, count = 0) {
+	for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1] && arr[i] === arr[i + 2]) count++
+  }
+  return count
+}
+
 // Write a function that counts how many concentric layers a rug.
 // Examples
 // countLayers([
@@ -35,7 +58,6 @@ function countLayers(rug, count = 1) {
   }
   return count
 }
-
 
 // Andy, Ben and Charlotte are playing a board game. The three of them decided to come up with a new scoring system. A player's first initial ("A", "B" or "C") denotes that player scoring a single point. Given a string of capital letters, return an array of the players' scores.
 // For instance, if ABBACCCCAC is written when the game is over, then Andy scored 3 points, Ben scored 2 points, and Charlotte scored 5 points, since there are 3 instances of letter A, 2 instances of letter B, and 5 instances of letter C. So the array [3, 2, 5] should be returned.
