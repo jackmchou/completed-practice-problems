4e5e359807b93f3b6bfc2d597f66597131a9383f
Jack Chou <jcacct@protonmail.ch>
2020-07-01 10:37:41 -0700

    Solved Pandigital Numbers

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,30 @@
+// A pandigital number contains all digits (0-9) at least once. Write a function that takes an integer, returning true if the integer is pandigital, and false otherwise.
+// Examples
+// isPandigital(98140723568910) ➞ true
+// isPandigital(90864523148909) ➞ false
+// // 7 is missing.
+// isPandigital(112233445566778899) ➞ false
+// Notes
+// Think about the properties of a pandigital number when all duplicates are removed.
+
+function isPandigital(num) {
+	const numStr = '' + num
+  const dups = {}
+  const uniq = []
+  let j = 0
+  for (let i = 0; i < numStr.length; i++) {
+    let n = numStr[i]
+    if(dups[n] !== 0) {
+      dups[n] = 0
+      uniq[j++] = n
+    }
+  }
+  for (let i = 0; i < 10; i++) {
+    if (uniq.join('').indexOf(i) === -1) return false
+  }
+  return true
+}
+
 // We handle resolve callbacks with then, but what about reject callbacks? We have a catch function that we can use to handle errors or rejected promises. It is generally a good idea to have an error handler at the end of your promises.
 // Challenge
 //     Write an error handler for the provided promise. Re-assign errorLog to the error inside your error handler.
