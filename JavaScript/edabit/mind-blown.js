314cd02115a4d7ddc95df1865614ee9410e9a733
Jack Chou <jcacct@protonmail.ch>
2020-07-01 09:22:31 -0700

    Solved Promises VI: Catch

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,19 @@
+// We handle resolve callbacks with then, but what about reject callbacks? We have a catch function that we can use to handle errors or rejected promises. It is generally a good idea to have an error handler at the end of your promises.
+// Challenge
+//     Write an error handler for the provided promise. Re-assign errorLog to the error inside your error handler.
+//     The error handler must be added to the original promise. So you can't do promise.catch(/error handling/). See the Comments for more details.
+// Notes
+// Check the Resources tab for more info on promises.
+
+let errorLog = {}
+
+let promise = new Promise((resolve, reject) => {
+  throw new Error('Something failed')
+}).catch(err => {
+  errorLog.message = err.message
+  return err
+})
+
 // Groups and ranges indicate groups and ranges of expression characters. Named capturing groups matches "x" in (?\<Name>x) and stores it on the groups property of the returned matches under the name specified by \<Name>. The angle brackets (< and >) are required for group name.
 // To extract the United States area code from a phone number, we could use:
 // let match = "(214) 987-6482".match(/\((?<area>\d\d\d)\)/)
