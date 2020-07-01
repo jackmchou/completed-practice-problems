1f054f74d83b5071a64a257b8f6762166741c475
Jack Chou <jcacct@protonmail.ch>
2020-06-30 17:27:02 -0700

    Solved Scoring System

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,18 @@
+// Andy, Ben and Charlotte are playing a board game. The three of them decided to come up with a new scoring system. A player's first initial ("A", "B" or "C") denotes that player scoring a single point. Given a string of capital letters, return an array of the players' scores.
+// For instance, if ABBACCCCAC is written when the game is over, then Andy scored 3 points, Ben scored 2 points, and Charlotte scored 5 points, since there are 3 instances of letter A, 2 instances of letter B, and 5 instances of letter C. So the array [3, 2, 5] should be returned.
+// Examples
+// calculateScores("A") ➞ [1, 0, 0]
+// calculateScores("ABC") ➞ [1, 1, 1]
+// calculateScores("ABCBACC") ➞ [2, 2, 3]
+// Notes
+// If given an empty string as an input, return [0, 0, 0].
+
+function calculateScores(str) {
+  let players = { A:0, B:0, C:0 }
+	for (let i = 0; i < str.length; i++) players[str[i]]++
+  return Object.values(players)
+}
+
 // Two sisters are eating chocolate, whose pieces are represented as subarrays of [l x w].
 // Write a function that returns true if the total area of chocolate is the same for each sister.
 // To illustrate:
