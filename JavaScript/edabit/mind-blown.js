96619cee45182bed69ba9bce8a87fe14d8a69525
Jack Chou <jcacct@protonmail.ch>
2020-07-01 11:17:34 -0700

    Solved Broken Keyboard

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,22 @@
+// Given what is supposed to be typed and what is actually typed, write a function that returns the broken key(s). The function looks like:
+// findBrokenKeys(correct phrase, what you actually typed)
+// Examples
+// findBrokenKeys("happy birthday", "hawwy birthday") ➞ ["p"]
+// findBrokenKeys("starry night", "starrq light") ➞ ["y", "n"]
+// findBrokenKeys("beethoven", "affthoif5") ➞ ["b", "e", "v", "n"]
+// Notes
+//     Broken keys should be ordered by when they first appear in the sentence.
+//     Only one broken key per letter should be listed.
+//     Letters will all be in lower case.
+
+function findBrokenKeys(str1, str2) {
+	const broken = []
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i] && broken.indexOf(str1[i]) === -1) broken.push(str1[i])
+  }
+  return broken
+}
+
 // A pandigital number contains all digits (0-9) at least once. Write a function that takes an integer, returning true if the integer is pandigital, and false otherwise.
 // Examples
 // isPandigital(98140723568910) ➞ true
