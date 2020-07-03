e60212f9a1ad2e6641aabe8e51cebb3db49367e0
Jack Chou <jcacct@protonmail.ch>
2020-07-03 10:04:04 -0700

    Solved Extending the String Prototype

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,33 @@
+// Javascript has a String prototype default in the language which contains properties and methods such as .length and .toLowerCase(). Extend the String prototype by creating two new methods:
+//     .consonants(), which returns the number of consonants in a word when called.
+//     .vowels(), which returns the number of vowels in a word when called.
+// Examples
+// "hello".consonants() ➞ 3
+// "hello".vowels() ➞ 2
+// "greatly".consonants() ➞ 5
+// "greatly".vowels() ➞ 2
+// "Smithsonian".consonants() ➞ 7
+// "Smithsonian".vowels() ➞ 4
+// Notes
+//     Treat y as a consonant, not a vowel.
+//     Upper vs. lower case does not matter.
+//     Hint: See comments for another example if you get stuck.
+// It is not mandatory for this exercise, but in general it is a good idea to double-check if there exists methods with the same name a your custom functions before using them to extend the prototype. This is to prevent the accidental overwrite of a pre-existing method. (See comments for additional info).
+
+String.prototype.consonants = function(count = 0) {
+	for (let i = 0; i < this.length; i++) {
+    if ('aeiou'.indexOf(this[i]) === -1) count++
+  }
+  return count
+}
+
+String.prototype.vowels = function(count = 0) {
+  for (let i = 0; i < this.length; i++) {
+    if ('aeiou'.indexOf(this[i]) !== -1) count++
+  }
+  return count
+}
+
 // A word nest is created by taking a starting word, and generating a new string by placing the word inside itself. This process is then repeated.
 // Nesting 3 times with the word "incredible":
 // start  = incredible
