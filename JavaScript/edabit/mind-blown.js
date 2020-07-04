c7f75a083037df4b7bd88e7113f4337295506b05
Jack Chou <jcacct@protonmail.ch>
2020-07-04 10:05:30 -0700

    Solved Unfair Hurdles

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,40 @@
+// Unfair hurdles are hurdles which are either too high, or way too close together.
+// Create a function which takes in an array of strings, representing hurdles, and returns whether or not they are unfair. For the purposes of this challenge, unfair hurdles are:
+//     At least 4 characters tall.
+//     Strictly less than 4 spaces apart.
+// Examples
+// // Hurdle are good distance apart but are way too tall.
+// isUnfairHurdle([
+//   "#    #    #    #",
+//   "#    #    #    #",
+//   "#    #    #    #",
+//   "#    #    #    #"
+// ]) ➞ true
+// // Hurdles are a fine height but are way too close together.
+// isUnfairHurdle([
+//   "#  #  #  #",
+//   "#  #  #  #",
+//   "#  #  #  #"
+// ]) ➞ true
+// // These hurdles are mighty splendid.
+// isUnfairHurdle([
+//   "#      #      #      #",
+//   "#      #      #      #"
+// ]) ➞ false
+// Notes
+//     Hurdles will be represented with a hashtag "#".
+//     There will be the same spacing between hurdles.
+//     Hurdles are always as high as the length of the array.
+//     Hurdles are always evenly spaced.
+
+function isUnfairHurdle(hurdles, distance = []) {
+  if (hurdles.length < 4) return false
+  for (let i = 0; i < hurdles[0].length; i++) {
+    if (hurdles[0][i] === '#') distance.push(i)
+  }
+  return (distance[1] - distance[0] - 1) <= 4
+}
+
 // Create a function which validates whether a given array alternates between positive and negative numbers.
 // Examples
 // alternatePosNeg([3, -2, 5, -5, 2, -8]) ➞ tru
