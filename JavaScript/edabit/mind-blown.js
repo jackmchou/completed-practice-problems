c4ce39161cebc56a0d93fa993b8943129f5bb21c
Jack Chou <jcacct@protonmail.ch>
2020-07-07 10:56:37 -0700

    Solved The Million Dollar Fence

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,18 @@
+// Your task is to create a fence worth $1 million. You are given the price of the material (per character), meaning the length of the fence will change depending on the cost of the material.
+// Create a function which constructs this pricey pricey fence, using the letter "H" to build.
+// constructFence("$50,000") ➞ "HHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+// // 20 fence posts were set up ($1,000,000 / $50,000 = 20)
+// Examples
+// constructFence("$50,000") ➞ "HHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+// constructFence("$100,000") ➞ "HHHHHHHHHH"
+// constructFence("$1,000,000") ➞ "H"
+// Notes
+// You are ordered to spend all of your $1,000,000 budget...
+
+function constructFence(price) {
+	return "H".repeat(1000000 / price.slice(1).split(',').join(''))
+}
+
 // Remove enemies from the list of people, even if the enemy shows up twice.
 // Examples
 // removeEnemies(["Fred"], []) ➞ ["Fred"]
