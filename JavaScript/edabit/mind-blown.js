4b3e3a05892aeb226bf95b2df177144c8765a6e5
Jack Chou <jcacct@protonmail.ch>
2020-07-10 07:55:09 -0700

    Solved Return the Time Saved by Speeding

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// One cause for speeding is the desire to shorten the time spent traveling. While in long distance trips speeding does save an appreciable amount of time, the same cannot be said about short distance trips.
+// Create a function that calculates the amount of time saved (in minutes) were you traveling with an average speed that is above the speed-limit as compared to traveling with an average speed exactly at the speed-limit.
+// Examples
+// // The paramater's format is as follows:
+// // (speed limit, avg speed, distance traveled at avg speed)
+// timeSaved(80, 90, 40) ➞ 3.3
+// timeSaved(80, 90, 4000) ➞ 333.3 
+// timeSaved(80, 100, 40 ) ➞ 6.0
+// timeSaved(80, 100, 10) ➞ 1.5
+// Notes
+//     Speed = distance/time
+//     The time returned should be in minutes, not in hours!
+
+function timeSaved(lim, avg, d) {
+  return +((d / lim) * 60 - (d / avg) * 60).toFixed(1)
+}
+
 // Write a function that takes two arrays and adds the first element in the first array with the first element in the second array, the second element in the first array with the second element in the second array, etc, etc. Return true if all element combinations add up to the same number. Otherwise, return false.
 // Examples
 // puzzlePieces([1, 2, 3, 4], [4, 3, 2, 1]) ➞ true
