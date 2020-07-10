a24feb61f922f45bdad564200ec940299353f83a
Jack Chou <jcacct@protonmail.ch>
2020-07-09 17:47:42 -0700

    Solved The Reverser!

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// The "Reverser" takes a string as input and returns that string in reverse order, with the opposite case.
+// Examples
+// reverse("Hello World") ➞ "DLROw OLLEh"
+// reverse("ReVeRsE") ➞ "eSrEvEr"
+// reverse("Radar") ➞ "RADAr"
+// Notes
+// There will be no punctuation in any of the test cases.
+
+function reverse(str) {
+  let result = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i].toLowerCase() === str[i]) result += str[i].toUpperCase()
+    else result += str[i].toLowerCase()
+  }
+  return result
+}
+
 // An array is special, if every even index contains an even number and every odd index contains an odd number. Create a function that returns true if an array is special, and false otherwise.
 // Examples
 // isSpecialArray([2, 7, 4, 9, 6, 1, 6, 3]) ➞ true
