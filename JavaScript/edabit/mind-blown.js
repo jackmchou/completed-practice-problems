723253481dcb00f1f0ed31a277e60e370e40dec0
Jack Chou <jcacct@protonmail.ch>
2020-07-11 06:10:17 -0700

    Solved Compound Interest

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,19 @@
+// Suppose that you invest $10,000 for 10 years at an interest rate of 6% compounded monthly. What will be the value of your investment at the end of the 10 year period?
+// Create a function that accepts the principal p, the term in years t, the interest rate r, and the number of compounding periods per year n. The function returns the value at the end of term rounded to the nearest cent.
+// For the example above:
+// compoundInterest(10000, 10, 0.06, 12) ➞ 18193.97
+// Note that the interest rate is given as a decimal and n=12 because with monthly compounding there are 12 periods per year. Compounding can also be done annually, quarterly, weekly or daily.
+// Examples
+// compoundInterest(100, 1, 0.05, 1) ➞ 105.0
+// compoundInterest(3500, 15, 0.1, 4) ➞ 15399.26
+// compoundInterest(100000, 20, 0.15, 365) ➞ 2007316.26
+// Notes
+// See the Resources tab for the interest formula and more information.
+
+function compoundInterest(p, t, r, n) {
+	return +(p * (1 + r / n) ** (n * t)).toFixed(2)
+}
+
 // One cause for speeding is the desire to shorten the time spent traveling. While in long distance trips speeding does save an appreciable amount of time, the same cannot be said about short distance trips.
 // Create a function that calculates the amount of time saved (in minutes) were you traveling with an average speed that is above the speed-limit as compared to traveling with an average speed exactly at the speed-limit.
 // Examples
