fa1a23fcf34ad2efda58ba0ad6a8530942699624
Jack Chou <jcacct@protonmail.ch>
2020-07-14 08:58:40 -0700

    Solved Regex Series: Initials

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// Regex Series: Initials
+// Write a regular expression that checks if a string is a valid initial. Valid initials either look like (ex. for Chandler Muriel Bing):
+//     C. B.
+//     C. M. B.
+// Rules for a valid initial:
+//     Each letter must be upper case.
+//     Each letter must be immediately followed by a period.
+//     There must be exactly one space separating each letter-period pair.
+//     Leading or trailing whitespaces are valid.
+// Examples
+// "C. B." ➞ true
+// "    C. B." ➞ true
+// // Leading and trailing spaces are OK!
+// "C. B. k." ➞ false
+// // One of the initials is lower cased 
+// "C B" ➞ false
+// // Missing a dot to immediately follow. 
+// Notes
+// This challenge is designed to use RegEx only.
+
+let x = /^\s*([A-Z]\.\s?){1,3}\s*$/
+
 // Create a function that takes two "sorted" arrays of numbers and returns an array of numbers which are common to both the input arrays.
 // Examples
 // commonElements([-1, 3, 4, 6, 7, 9], [1, 3]) ➞ [3]
