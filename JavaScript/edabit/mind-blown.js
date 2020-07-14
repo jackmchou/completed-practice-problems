b863481d26ba295f597bf2e8a67c6a235ae2c0e5
Jack Chou <jcacct@protonmail.ch>
2020-07-14 11:25:46 -0700

    Solved Number of Squares in a n * n Grid

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,16 @@
+// Create a function that calculates the number of different squares in an n * n square grid. Check the Resources tab.
+// Examples
+// numberSquares(2) ➞ 5
+// numberSquares(4) ➞ 30
+// numberSquares(5) ➞ 55
+// Notes
+//     Input is a positive integer.
+//     Square pyramidal number.
+
+function numberSquares(n) {
+  return (n * (n + 1) * (2 * n + 1)) / 6
+}
+
 // Write a function that takes the base and height of a triangle and return its area.
 // Examples
 // triArea(3, 2) ➞ 3
