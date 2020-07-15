6f13f325ca6a531da7fb1fbf7ee8ff9b4b4e6837
Jack Chou <jcacct@protonmail.ch>
2020-07-15 07:06:40 -0700

    Solved Covert Dates

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Create a function that converts dates from one of five string formats:
+//     "January 9, 2019" (MM D, YYYY)
+//     "Jan 9, 2019" (MM D, YYYY)
+//     "01/09/2019" (MM/DD/YYYY)
+//     "01-09-2019" (MM-DD-YYYY)
+//     "01.09.2019" (MM.DD.YYYY)
+// The return value will be an array formatted like: [MM, DD, YYYY], where MM, DD, and YYYY are all integers. Using the examples above:
+// Examples
+// convertDate("January 9, 2019") ➞ [1, 9, 2019]
+// convertDate("Jan 9, 2019") ➞ [1, 9, 2019]
+// convertDate("01/09/2019") ➞ [1, 9, 2019]
+// convertDate("01-09-2019") ➞ [1, 9, 2019]
+// convertDate("01.09.2019") ➞ [1, 9, 2019]
+// Notes
+// You can solve this any number of ways, but using JavaScript's new Date() method is probably the easiest. Check the Resources tab for documentation.
+
+function convertDate(date) {
+  const dateObj = new Date(date)
+  return [dateObj.getMonth() + 1, dateObj.getDate(),dateObj.getFullYear()]
+}
+
 // Noddy has written a mysterious function which takes in a word and returns true if it's passed a specific test. Solve the riddle of what Noddy's function is by having a look at some of the examples below.
 // Examples
 // noddyFunction("FANTASTIC") ➞ true
