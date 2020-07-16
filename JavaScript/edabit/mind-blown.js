276baf31a5bd57926644424be49accd6ddfc1258
Jack Chou <jcacct@protonmail.ch>
2020-07-16 09:24:44 -0700

    Solved The Museum of Incredibly DULL Things

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// A museum wants to get rid of some exhibitions. Katya, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and removes the one with the lowest rating. Just as she finishes rating the exhibitions, she's called off to an important meeting. She asks you to write a program that tells her the ratings of the items after the lowest one is removed.
+// Create a function that takes an array of integers and removes the smallest value.
+// Examples
+// removeSmallest([1, 2, 3, 4, 5] ) ➞ [2, 3, 4, 5]
+// removeSmallest([5, 3, 2, 1, 4]) ➞ [5, 3, 2, 4]
+// removeSmallest([2, 2, 1, 2, 1]) ➞ [2, 2, 2, 1]
+// Notes
+//     Don't change the order of the left over items.
+//     If you get an empty array, return an empty array: [] ➞ [].
+//     If there are multiple items with the same value, remove item with lower index (3rd example).
+
+function removeSmallest(arr) {
+	// arr.splice(arr.indexOf(Math.min(...arr)), 1) 
+  // return arr
+  let min = arr[0]
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i]
+  }
+  arr.splice(arr.indexOf(min), 1)
+  return arr
+}
+
 // Create a function that counts the number of times a particular letter shows up in the word search.
 // Examples
 // letterCounter([
