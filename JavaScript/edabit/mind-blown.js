f910e1e3e7d8766680b6d9a968467d8df59d125c
Jack Chou <jcacct@protonmail.ch>
2020-07-16 10:59:15 -0700

    Solved Extend the global Array object with an  method

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// Extend the global Array object to have an instance method called isEqual(). The method should accept an array as the first argument, and a second optional argument that is a flag to ignore the order of the arrays. This second argument should default to false
+// Examples
+// [1, 2, 3].isEqual([1, 2, 3]) ➞ true
+// [1, 2, 3].isEqual([1, 3, 2]) ➞ false
+// [1, 2, 3].isEqual([1, 3, 2], true) ➞ true
+// Notes
+// This method does not need to compare Arrays, Objects, and non-primitives. It just needs to consider numbers, booleans, and strings.
+
+Array.prototype.isEqual = function(arr, ignoreOrder = false) {
+  if (ignoreOrder) this.sort()
+  const sorted = ignoreOrder ? arr.sort() : arr
+  for(let i = 0; i < this.length; i++) {
+      if (this[i] !== sorted[i]) return false
+  }
+  return true
+}
+
 // A museum wants to get rid of some exhibitions. Katya, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and removes the one with the lowest rating. Just as she finishes rating the exhibitions, she's called off to an important meeting. She asks you to write a program that tells her the ratings of the items after the lowest one is removed.
 // Create a function that takes an array of integers and removes the smallest value.
 // Examples
