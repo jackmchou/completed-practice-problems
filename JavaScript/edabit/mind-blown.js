84259a83e9312c2bdc6ed7043e55ea1ff3325f48
Jack Chou <jcacct@protonmail.ch>
2020-07-16 11:38:51 -0700

    Solved Narcissistic Numbers

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,27 @@
+// A number is narcissistic when the sum of its digits, with each digit raised to the power of digits quantity, is equal to the number itself.
+// 153 ➞ 3 digits ➞ 1³ + 5³ + 3³ = 1 + 125 + 27 = 153 ➞ Narcissistic
+// 84 ➞ 2 digits ➞ 8² + 4² = 64 + 16 = 80 ➞ Not narcissistic
+// Given a positive integer n, implement a function that returns true if the number is narcissistic, and false if it's not.
+// Examples
+// isNarcissistic(8208) ➞ true
+// // 8⁴ + 2⁴ + 0⁴ + 8⁴ = 8208
+// isNarcissistic(22) ➞ false
+// // 2² + 2² = 8
+// isNarcissistic(9) ➞ true
+// // 9¹ = 9
+// Notes
+//     Trivially, any number in the 1-9 range is narcissistic and any two-digit number is not.
+//     Curious fact: Only 88 numbers are narcissistic.
+
+function isNarcissistic(n) {
+	const nArr = [...('' + n)]
+  let sum = 0
+  for (let i = 0; i < nArr.length; i++) {
+    sum += nArr[i] ** nArr.length
+  }
+  return sum == n
+}
+
 // Extend the global Array object to have an instance method called isEqual(). The method should accept an array as the first argument, and a second optional argument that is a flag to ignore the order of the arrays. This second argument should default to false
 // Examples
 // [1, 2, 3].isEqual([1, 2, 3]) ➞ true
