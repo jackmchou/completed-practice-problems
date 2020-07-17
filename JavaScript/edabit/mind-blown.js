9f225ef4010f2e27af1d1f43ed7117eb1c67b831
Jack Chou <jcacct@protonmail.ch>
2020-07-17 06:33:52 -0700

    Solved Is There an Upward Trend?

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,21 @@
+// Create a function that determines if there is an upward trend.
+// Examples
+// upwardTrend([1, 2, 3, 4]) ➞ true
+// upwardTrend([1, 2, 6, 5, 7, 8]) ➞ false
+// upwardTrend([1, 2, 3, "4"]) ➞ "Strings not permitted!"
+// upwardTrend([1, 2, 3, 6, 7]) ➞ true
+// Notes
+//     If there is a string element in the array, return "Strings not permitted!".
+//     The numbers don't have to be consecutive (e.g. [1, 3, 5] should still return true).
+
+function upwardTrend(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') return "Strings not permitted!"
+    else if (arr[i] > arr[i + 1]) return false
+  }
+  return true
+}
+
 // Write a function that recursively determines if a string is a palindrome.
 // Examples
 // isPalindrome("abcba") ➞ true
