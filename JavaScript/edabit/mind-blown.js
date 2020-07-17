13a087f987ef0841570aa5c7fe581c4e7bf2bdb0
Jack Chou <jcacct@protonmail.ch>
2020-07-17 10:39:14 -0700

    Solved Oddly or Evenly Positioned

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Create a function that returns the characters from an array or string r on odd or even positions, depending on the specifier s. The specifier will be "odd" for items on odd positions (1, 3, 5, ...) and "even" for items on even positions (2, 4, 6, ...).
+// Examples
+// charAtPos([2, 4, 6, 8, 10], "even") ➞ [4, 8]
+// // 4 & 8 occupy the 2nd & 4th positions
+// charAtPos("EDABIT", "odd") ➞ "EAI"
+// // "E", "A" and "I" occupy the 1st, 3rd and 5th positions
+// charAtPos(["A", "R", "B", "I", "T", "R", "A", "R", "I", "L", "Y"], "odd") ➞ ["A", "B", "T", "A", "I", "Y"]
+// Notes
+//     Arrays are zero-indexed, so, index+1 = position or position-1 = index.
+//     A slightly different version of this challenge is found here.
+//     A recursive version of this challenge is found here.
+
+function charAtPos(r, s) {
+  let result = []
+  let specifier = s === 'even' ? 0 : 1
+  for (let i = 0; i < r.length; i++) {
+    if ((i + 1) % 2 === specifier) result.push(r[i])
+  }
+  return typeof r === 'string' ? result.join('') : result
+}
+
 // Create a function based on the input and output. Look at the examples, there is a pattern.
 // Examples
 // secret(24) ➞ 8
