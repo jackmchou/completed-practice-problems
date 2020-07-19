223f245123df5ccd75b6a6b5e94217c3491d919a
Jack Chou <jcacct@protonmail.ch>
2020-07-19 07:43:08 -0700

    Solved What's the Data Type?

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,27 @@
+// Create a function that returns the data type of a given variable. These are the eight data types this challenge will be testing for:
+//     Array
+//     Object
+//     String
+//     Number
+//     Boolean
+//     Null
+//     Undefined
+//     Date
+// Examples
+// dataType([1, 2, 3, 4]) ➞ "array"
+// dataType({key: "value"}) ➞ "object"
+// dataType("This is an example string.") ➞ "string"
+// dataType(new Date()) ➞ "date"
+// Notes
+// Return the name of the data type as a lowercase string.
+
+function dataType(value) {
+  if (Array.isArray(value)) return 'array'
+  if (value === null) return 'null'
+  if (value instanceof Date) return 'date'
+  return typeof value
+}
+
 // Create a function that takes an integer and outputs an n x n square solely consisting of the integer n.
 // Examples
 // squarePatch(3) ➞ [
