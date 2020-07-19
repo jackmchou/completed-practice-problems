59b8271ef87893831bfa76ea63292821f755df19
Jack Chou <jcacct@protonmail.ch>
2020-07-19 08:17:37 -0700

    Solved Alphanumeric Restriction

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,19 @@
+// Create a function that returns true if the given string has any of the following:
+//     Only letters and no numbers.
+//     Only numbers and no letters.
+// If a string has both numbers and letters, or contains characters which don't fit into any category, return false
+// Examples
+// alphanumericRestriction("Bold") ➞ true
+// alphanumericRestriction("123454321") ➞ true
+// alphanumericRestriction("H3LL0") ➞ false
+// alphanumericRestriction("ed@bit") ➞ false
+// Notes
+// Any string that contains spaces or is empty should return false.
+
+function alphanumericRestriction(str) {
+  return /^[A-z]+$|^[0-9]+$/.test(str)
+}
+
 // Create a function that returns the data type of a given variable. These are the eight data types this challenge will be testing for:
 //     Array
 //     Object
