323f4b2e4184a99c97d218ec75417e783216b784
Jack Chou <jcacct@protonmail.ch>
2020-07-19 08:52:21 -0700

    Solved ES6: Destructuring Objects VII

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,21 @@
+// In normal object literals you can create dynamic property names by using computed values:
+// let key = "name"
+// let foo = {[key]: "bar"}
+// console.log(foo.name); // "bar"
+// However, in object destructuring it works a little different:
+// let key = "foo"
+// let {[key]: "foo"} = {foo: "Jessie"}
+// console.log(foo)  // Invalid destructuring assignment target
+// The error above is caused by the literal string "foo". You cannot rename variables this way. You can read the MDN Docs Assigning to new variable names section to find the correct way.
+// Fix the function so console.log(foo) returns Jessie. Leave all variable names unchanged. Ignore the .toString() function (used for validation).
+// Example
+// console.log(foo) ➞ "Jessie"
+// Notes
+// If you know how to use object destructuring, go ahead and complete this challenge, otherwise check the Resources tab for some examples.
+
+let key = "foo"
+const str = `({[key] : foo} = {foo: "Jessie"}).toString()`
+
 // Create a function that returns true if the given string has any of the following:
 //     Only letters and no numbers.
 //     Only numbers and no letters.
