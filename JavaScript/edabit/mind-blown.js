9dd0b65d7c1eb7b070aee6f296aa47fc990ee846
Jack Chou <jcacct@protonmail.ch>
2020-07-20 14:38:52 -0700

    Solved Proper Modulo Operator

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,15 @@
+// In JavaScript, the modulo operator is very bad. For example, -13 % 64 = -13, when the actual answer is 51. Create a function to fix this. It should also work for positive numbers.
+// Examples
+// mod(-13, 64) ➞ 51
+// mod(50, 25) ➞ 0
+// mod(-6, 3) ➞ 0
+// Notes
+// All test cases contain valid numbers.
+
+function mod(m, n) {
+	return ((m%n)+n)%n
+}
+
 // Create a function that takes a number of a guitar string and the number of the fret and returns the corresponding frequency of the note.
 //     Check the Resources Tab, for the correct frequencies per string.
 //     The formula to calculate the frequency is: String Frequency * 2^(fret/12).
