726e8771e1fe7b258f150a825613c7746814bcba
Jack Chou <jcacct@protonmail.ch>
2020-07-20 14:19:44 -0700

    Solved Fret Frequencies on the Guitar

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,21 @@
+// Create a function that takes a number of a guitar string and the number of the fret and returns the corresponding frequency of the note.
+//     Check the Resources Tab, for the correct frequencies per string.
+//     The formula to calculate the frequency is: String Frequency * 2^(fret/12).
+//     Round the frequency to the nearest hundreth.
+//     For this challenge, we use "Standard Tuning".
+// So, one fret = a semitone = a half step. From D to D♯ for instance.
+// Examples
+// fretFreq(5, 12) ➞ 220
+// fretFreq(4, 6) ➞ 207.65
+// fretFreq(2, 23) ➞ 932.32
+// Notes
+// N/A
+
+function fretFreq(gStr, fret) {
+  const strFreq = {1:329.63,2:246.94,3:196,4:146.83,5:110,6:82.41}
+  return +(strFreq[gStr] * 2 ** (fret/12)).toFixed(2)
+}
+
 // Write a function that replaces all letters within a specified range with the hash symbol #.
 // Examples
 // replace("abcdef", "c-e") ➞ "ab###f"
