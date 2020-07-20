bd30ea759b4aad9aef0f549e7a167c1bf35c303f
Jack Chou <jcacct@protonmail.ch>
2020-07-20 08:25:59 -0700

    Solved ES6: Destructuring Objects IX

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,18 @@
+// Given an array of user objects. If we just wanted to get the name of the third object in the array, one way could be to use an array method like:
+// let users = [
+//   { name: "John", email: "john@example.com" },
+//   { name: "Jason", email: "jason@example.com" },
+//   { name: "Jeremy", email: "jeremy@example.com" },
+//   { name: "Jacob", email: "jacob@example.com" }
+// ]
+// let thirdUser = users.filter((e, i) => i === 2 )[0].name
+// console.log(thirdUser)  // "Jeremy"
+// However, you can combine array and Object destructuring to extract it more declaratively. Use array and object destructuring to extract the name from the third object in the users array and assign it to the variable thirdUser. Provide the solution inside the brackets only. Ignore the .toString() function (used for validation).
+// Notes
+// N/A
+
+const str = `[,,{name:thirdUser}] = users`
+
 // Create a function which adds zeros to the start of a binary string, so that its length is a mutiple of 8.
 // Examples
 // completeBinary("1100") ➞ "00001100"
