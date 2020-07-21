31d02e7f2d08fcb8b84e3216fdd5ddd9aa8d5b3e
Jack Chou <jcacct@protonmail.ch>
2020-07-21 14:16:12 -0700

    Solved  Probabilities (Part 1)

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,15 @@
+// Given an array of numbers and a value n, write a function that returns the probability of choosing a number greater than or equal to n from the array. The probability should be expressed as a percentage, rounded to one decimal place.
+// Examples
+// probability([5, 1, 8, 9], 6) ➞ 50.0
+// probability([7, 4, 17, 14, 12, 3], 16) ➞ 16.7
+// probability([4, 6, 2, 9, 15, 18, 8, 2, 10, 8], 6) ➞ 70.0
+// Notes
+// Probability of event = (num of favourable outcomes) / (total num of possible outcomes)
+
+function probability(arr, num) {
+  return +(arr.filter(ele => ele >= num).length / arr.length * 100).toFixed(1)
+}
+
 // In BlackJack, cards are counted with -1, 0, 1 values:
 //     2, 3, 4, 5, 6 are counted as +1
 //     7, 8, 9 are counted as 0
