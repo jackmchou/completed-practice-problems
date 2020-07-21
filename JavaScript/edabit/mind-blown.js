23c681c2a35a6734f09aa9f4ba3b0ca6d2497b02
Jack Chou <jcacct@protonmail.ch>
2020-07-21 07:41:41 -0700

    Solved Card Counting (BlackJack)

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,27 @@
+// In BlackJack, cards are counted with -1, 0, 1 values:
+//     2, 3, 4, 5, 6 are counted as +1
+//     7, 8, 9 are counted as 0
+//     10, J, Q, K, A are counted as -1
+// Create a function that counts the number and returns it from the array of cards provided.
+// Examples
+// count([5, 9, 10, 3, "J", "A", 4, 8, 5]) ➞ 1
+// count(["A", "A", "K", "Q", "Q", "J"]) ➞ -6
+// count(["A", 5, 5, 2, 6, 2, 3, 8, 9, 7]) ➞ 5
+// Notes
+//     String inputs will always be upper case.
+//     You do not need to consider case sensitivity.
+//     If the argument is empty, return 0.
+//     No input other than: 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A".
+
+function count(deck) {
+	let counter = 0
+  for (let i = 0; i < deck.length; i++) {
+    if ('23456'.includes(deck[i])) counter++
+    else if ([10, 'J', 'Q','K','A'].includes(deck[i])) counter--
+  }
+  return counter
+}
+
 // In JavaScript, the modulo operator is very bad. For example, -13 % 64 = -13, when the actual answer is 51. Create a function to fix this. It should also work for positive numbers.
 // Examples
 // mod(-13, 64) ➞ 51
