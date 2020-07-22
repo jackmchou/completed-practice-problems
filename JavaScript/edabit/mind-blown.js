bf93a79bb709a4083c6bb7368ea96400b9d0aeae
Jack Chou <jcacct@protonmail.ch>
2020-07-22 13:24:22 -0700

    Solved Circle Constructor

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,41 @@
+// Create a Circle() constructor that takes the radius as a single argument and has the following properties and methods:
+//     .radius
+//     .diameter
+//     .getC() (get circumference)
+//     .getA() (get area)
+// Instantiate this constructor with two circles:
+//     c1 has radius 3
+//     c2 has radius 5
+// For example, if I used the Circle constructor to instantiate a new instance called c0 with a radius of 1, I would have:
+// Examples
+// c0.radius ➞ 1
+// c0.diameter ➞ 2
+// c0.getC() ➞ 6.28
+// c0.getA() ➞ 3.14
+// Notes
+//     Circumference: 2πr. Area: πr^2.
+//     Use Math.PI for calculating circumference and area.
+//     Round the perimeter and area results to the nearest hundredths place.
+
+function Circle(radius) {
+	// Write your properties and methods here
+	this.radius = radius
+	this.diameter = radius * 2
+}
+
+Circle.prototype.getC = function () {
+    return +(2 * Math.PI * this.radius).toFixed(2)
+}
+
+Circle.prototype.getA = function () {
+    return +(Math.PI * this.radius ** 2).toFixed(2)
+}
+
+
+// Instantiate your constructor class here
+const c1 = new Circle(3)
+const c2 = new Circle(5)
+
 // Create a function that outputs true if a number is prime, and false otherwise.
 // Examples
 // isPrime(31) ➞ true
