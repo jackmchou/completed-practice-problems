0758bcab90a15dbdff8b391acb797fbaf98995a0
Jack Chou <jcacct@protonmail.ch>
2020-07-23 08:23:40 -0700

    Solved Quarantine TP

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,23 @@
+// Can you spare a square?
+// Try to imagine a world in which you might have to stay home for 14 days at any given time. Do you have enough TP to make it through?
+// Although the number of squares per roll of TP varies significantly, we'll assume each roll has 500 sheets, and the average person uses 57 sheets per day.
+// Create a function that will receive an object with two key/values:
+//     people ⁠— Number of people in the household.
+//     tp ⁠— Number of rolls.
+// Return a statement telling the user if they need to buy more TP!
+// Examples
+// tpChecker({people: 4, tp: 1}) ➞ "Your TP will only last 2 days, buy more!"
+// tpChecker({people: 3, tp: 20}) ➞ "Your TP will last 58 days, no need to panic!"
+// tpChecker({people: 4, tp: 12} ➞ "Your TP will last 26 days, no need to panic!"
+// Notes
+// Stay safe, and happy coding!
+
+function tpChecker(home) {
+  const days = ((home.tp * 500) / (home.people * 57)) | 0
+  const threshold = days < 14
+	return `Your TP will ${threshold ? 'only ' : ''}last ${days} days, ${threshold ? 'buy more!' : 'no need to panic!'}`
+}
+
 // Create a function that takes three arguments a, b, c and returns the sum of the numbers that are evenly divided by c from the range a, b inclusive.
 // Examples
 // evenlyDivisible(1, 10, 20) ➞ 0
