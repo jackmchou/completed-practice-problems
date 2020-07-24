85e1703a1e742a6389df103819f232bf94a95e5d
Jack Chou <jcacct@protonmail.ch>
2020-07-24 15:17:55 -0700

    Solved Recursion to Repeat a String n Number of Times

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,16 @@
+// Create a recursive function that takes two parameters and repeats the string n number of times. The first parameter txt is the string to be repeated and the second parameter is the number of times the string is to be repeated.
+// String.prototype.repeat() is not allowed
+// Examples
+// repetition("ab", 3) ➞ "ababab"
+// repetition("kiwi", 1) ➞ "kiwi"
+// repetition("cherry", 2) ➞ "cherrycherry"
+// Notes
+// The second parameter of the function is positive integer.
+
+function repetition(txt, n) {
+	return n <= 1 ? txt : txt + repetition(txt, n - 1)
+}
+
 // Write a function that returns the length of a string. Make your function recursive.
 // Examples
 // length("apple") ➞ 5
