2dae54d3204e15b423d2596db4576fe2cf72e4cf
Jack Chou <jcacct@protonmail.ch>
2020-07-23 19:30:11 -0700

    Solved alternateSort

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,23 @@
+// Write a function that sorts a given array in an alternative fashion. The result should be a array sorted in ascending order (number then letter). Array will contain equal amounts of integer numbers and single characters.
+// Examples
+// alternateSort(["a", "b", "c", 1, 2, 3]) ➞ [1, "a", 2, "b", 3, "c"]
+// alternateSort([-2, "f", "A", 0, 100, "z"]) ➞ [-2, "A", 0, "f", 100, "z"]
+// alternateSort(["X", 15, 12, 18, "Y", "Z"]) ➞ [12, "X", 15, "Y", 18, "Z"]
+// Notes
+
+function alternateSort(arr) {
+  let result = []
+  const letters = arr.filter(letter => typeof letter === 'string').sort()
+  const numbers = arr.filter(number => typeof number === 'number').sort((a, b) => a - b)
+  let index = 0
+  for (const item of numbers) {
+    result.push(item)
+    result.push(letters[index])
+    index++
+  }
+  return result
+}
+
 // This robot roams around a 2D grid. It starts at (0, 0) facing North. After each time it moves, the robot rotates 90 degrees clockwise. Given the amount the robot has moved each time, you have to calculate the robot's final position.
 // To illustrate, if the robot is given the movements 20, 30, 10, 40 then it will move:
 //     20 steps North, now at (0, 20)
