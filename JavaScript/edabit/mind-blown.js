6cd82e91bd3c261a2bb8a80a537f1a1f5e2028ba
Jack Chou <jcacct@protonmail.ch>
2020-07-23 18:39:23 -0700

    Solved Track the Robot (Part 2)

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,32 @@
+// This robot roams around a 2D grid. It starts at (0, 0) facing North. After each time it moves, the robot rotates 90 degrees clockwise. Given the amount the robot has moved each time, you have to calculate the robot's final position.
+// To illustrate, if the robot is given the movements 20, 30, 10, 40 then it will move:
+//     20 steps North, now at (0, 20)
+//     30 steps East, now at (30, 20)
+//     10 steps South. now at (30, 10)
+//     40 steps West, now at (-10, 10)
+// ...and will end up at coordinates (-10, 10).
+// Examples
+// trackRobot(20, 30, 10, 40) ➞ [-10, 10]
+// trackRobot() ➞ [0, 0]
+// // No movement means the robot stays at (0, 0).
+// trackRobot(-10, 20, 10) ➞ [20, -20]
+// // The amount to move can be negative.
+// Notes
+// Each movement is an integer (whole number).
+
+function trackRobot(...steps) {
+  const pos = [0, 0]
+  for (let i = 0; i < steps.length; i++) {
+    const ns = i % 4
+    const ew = i % 2
+    if (ns == 0) pos[1] += steps[i]
+    else if (ns == 1) pos[0] += steps[i]
+    else if (ew == 0) pos[1] -= steps[i]
+    else if (ew == 1) pos[0] -= steps[i]
+  }
+  return pos
+}
+
 // Creates a function that takes two integers, num and n, and returns an integer which is divisible by n and is the closest to num. If there are two numbers equidistant from num and divisible by n, select the larger one.
 // Examples
 // roundNumber(33, 25) ➞ 25
