94ba767acc5632fff5ffef609182df25888caf8c
Jack Chou <jcacct@protonmail.ch>
2020-07-27 08:12:51 -0700

    Transcribe To mRNA

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,17 @@
+// Transcribe the given DNA strand into corresponding mRNA - a type of RNA, that will be formed from DNA after transcription. DNA has the bases A, T, G and C, while RNA has U in place of T and all of the other bases remains the same.
+// Examples
+// dnaToRna("ATTAGCGCGATATACGCGTAC") ➞ "UAAUCGCGCUAUAUGCGCAUG"
+// dnaToRna("CGATATA") ➞ "GCUAUAU"
+// dnaToRna("GTCATACGACGTA") ➞ "CAGUAUGCUGCAU"
+// Notes
+//     Transcription is the process of making complementary strand.
+//     A, T, G and C in DNA converts to U, A, C and G respectively in mRNA.
+
+function dnaToRna(dna) {
+  const dnaToRna = {'A': 'U', 'T': 'A', 'G': 'C', 'C': 'G'}
+	return [...dna].map(ele => dnaToRna[ele]).join('')
+}
+
 // Given an array of integers, replace every number with the average mean of the whole array.
 // Examples
 // flattenCurve([1, 2, 3, 4, 5]) ➞ [3, 3, 3, 3, 3]
