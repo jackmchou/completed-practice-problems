aea77f7a4393aae3c3f2551079bf923abbb0adaa
Jack Chou <jcacct@protonmail.ch>
2020-07-30 13:16:51 -0700

    Solved What Type of Triangle?

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,30 @@
+// Create a function which returns the type of triangle, given the side lengths. Return the following values if they match the criteria.
+
+//     No sides equal: "scalene"
+//     Two sides equal: "isosceles"
+//     All sides equal: "equilateral"
+//     Less or more than 3 sides given: "not a triangle"
+
+// Examples
+
+// getTriangleType([2, 6, 5]) ➞ "scalene"
+
+// getTriangleType([4, 4, 7]) ➞ "isosceles"
+
+// getTriangleType([8, 8, 8]) ➞ "equilateral"
+
+// getTriangleType([3, 5, 5, 2]) ➞ "not a triangle"
+
+// Notes
+
+//     You will be given an array of positive integers.
+//     Check the Resources tab for more information on the types of triangles.
+function getTriangleType(arr) {
+	if (arr.length !== 3) return 'not a triangle'
+  const triangleTypes = {1: 'equilateral', 2: 'isosceles', 3: 'scalene'}
+  return triangleTypes[new Set(arr).size]
+}
+
 // Create a function that takes a string and returns the first character of every word if the length is even and the middle character if the length is odd.
 // Examples
 
