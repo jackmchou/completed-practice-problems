bb6c4d02f36ec1b8b6959c21cfad208e68f15990
Jack Chou <jcacct@protonmail.ch>
2020-08-02 07:58:35 -0700

    Solved Clear the Fog

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,22 @@
+// Create a function which returns the word in the string, but with all the fog letters removed. However, if the string is clear from fog, return "It's a clear day!".
+// Examples
+
+// clearFog("sky") ➞ "It's a clear day!"
+
+// clearFog("fogfogfffoooofftreesggfoogfog") ➞ "trees"
+
+// clearFog("fogFogFogffffooobirdsandthebeesGGGfogFog") ➞ "birdsandthebees"
+
+// Notes
+
+//     Hidden words won't include the letters f, o or g.
+//     Hidden words are always in lowercase.
+
+function clearFog(str) {
+  if (!/[fog]/gi.test(str)) return "It's a clear day!"
+  return str.replace(/[fog]/gi, '')
+}
+
 // A Collatz sequence is generated like this. Start with a positive number. If it's even, halve it. If it's odd, multiply it by three and add one. Repeat the process with the resulting number. The Collatz Conjecture is that every sequence eventually reaches 1 (continuing past 1 just results in an endless repeat of the sequence: 4, 2, 1).
 
 // The length of the sequence from starting number to 1 varies widely.
