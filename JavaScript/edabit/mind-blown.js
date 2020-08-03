6d6173bdb313e143cf5a7c2bc262a5a42a99e98f
Jack Chou <jcacct@protonmail.ch>
2020-08-03 15:31:59 -0700

    Solved Compounding Letters

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// Create a function that takes a string and returns a new string with each new character accumulating by +1. Separate each set with a dash.
+// Examples
+
+// accum("abcd") ➞ "A-Bb-Ccc-Dddd"
+
+// accum("RqaEzty") ➞ "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+
+// accum("cwAt") ➞ "C-Ww-Aaa-Tttt"
+
+// Notes
+
+//     Capitalize the first letter of each set.
+//     All tests contain valid strings with alphabetic characters (a-z, A-Z).
+
+function accum(str) {
+	const result = []
+  for (let i = 0; i < str.length; i++) {
+    result.push(str[i].toUpperCase() + str[i].toLowerCase().repeat(i))
+  }
+  return result.join('-')
+}
+
 // Write a function that returns the first n vowels of a string.
 // Examples
 
