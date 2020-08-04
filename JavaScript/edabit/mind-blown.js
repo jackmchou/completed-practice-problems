3bc7231b1901e5dad61279152ab8b38bae00cb87
Jack Chou <jcacct@protonmail.ch>
2020-08-04 09:02:20 -0700

    Solved First Before Second Letter

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,33 @@
+// You are given three inputs: a string, one letter, and a second letter.
+
+// Write a function that returns true if every instance of the first letter occurs before every instance of the second letter.
+// Examples
+
+// firstBeforeSecond("a rabbit jumps joyfully", "a", "j") ➞ true
+// // every instance of "a" occurs before every instance of "j"
+
+// firstBeforeSecond("knaves knew about waterfalls", "k", "w") ➞  true
+
+// firstBeforeSecond("happy birthday", "a", "y") ➞ false
+// // the "a" in "birthday" occurs after the "y" in "happy"
+
+// firstBeforeSecond("precarious kangaroos", "k", "a") ➞ false
+
+// Notes
+
+//     All strings will be in lower case.
+//     All strings will contain the first and second letters at least once.
+
+function firstBeforeSecond(s, first, second) {
+	const firstIdx = []
+  const secondIdx = []
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == first) firstIdx.push(i)
+    if (s[i] == second) secondIdx.push(i)
+  }
+  return firstIdx.every((ele, idx) => ele < Math.min(...secondIdx))
+}
+
 // Write a function that changes every letter to the next letter:
 
 //     "a" becomes "b"
