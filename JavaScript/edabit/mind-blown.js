8bf70c5a01a37c5b8a4dd286786c91237793e10b
Jack Chou <jcacct@protonmail.ch>
2020-08-06 14:15:10 -0700

    Solved Quadratic Equation

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,28 @@
+// Create a function to find only the root value of x in any quadratic equation ax^2 + bx + c. The function will take three arguments:
+
+//     a as the coefficient of x^2
+//     b as the coefficient of x
+//     c as the constant term
+
+// Examples
+
+// quadraticEquation(1, 2, -3) ➞ 1
+
+// quadraticEquation(2, -7, 3) ➞ 3
+
+// quadraticEquation(1, -12, -28) ➞ 14
+
+// Notes
+
+//     Quadratic equation is always guaranteed to have a root.
+//     Check the Resources tab for more information on quadratic equations.
+//     Calculate only the root that sums the square root of the discriminant, not the one that subtracts it.
+//     Round the value / return only integer value.
+
+function quadraticEquation(a, b, c) {
+  return ((b * -1) + Math.sqrt((b ** 2) - (4 * a * c))) / (a * 2)
+}
+
 // Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
 
 //     When positives and positives interact, they remain positive.
