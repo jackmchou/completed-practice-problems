bdae0dd975ddd2464dd2c7859c064a11efc13602
Jack Chou <jcacct@protonmail.ch>
2020-08-06 04:57:50 -0700

    Solved Pythagorean Triplet

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,27 @@
+// Create a function that validates whether three given integers form a Pythagorean triplet. The sum of the squares of the two smallest integers must equal the square of the largest number to be validated.
+// Examples
+
+// isTriplet(3, 4, 5) ➞ true
+// // 3² + 4² = 25
+// // 5² = 25
+
+// isTriplet(13, 5, 12) ➞ true
+// // 5² + 12² = 169
+// // 13² = 169
+
+// isTriplet(1, 2, 3) ➞ false
+// // 1² + 2² = 5
+// // 3² = 9
+
+// Notes
+
+// Numbers may not be given in a sorted order.
+
+function isTriplet(n1, n2, n3) {
+	const sorted = [...arguments].sort((a,b) => a - b)
+  return (sorted[0] ** 2 + sorted[1] ** 2) == (sorted[2] ** 2)
+}
+
 // Given a square matrix (i.e. same number of rows as columns), its trace is the sum of the entries in the main diagonal (i.e. the diagonal line from the top left to the bottom right).
 // As an example, for:
 // [
