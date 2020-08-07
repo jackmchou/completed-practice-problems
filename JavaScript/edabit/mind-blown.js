0e663ad7c87727665553460b0b33fa4acdc6d618
Jack Chou <jcacct@protonmail.ch>
2020-08-07 08:02:22 -0700

    Solved Tidy Title and Author Strings

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,45 @@
+// You have an array of strings, each consisting of a book title and an author's name.
+
+// To illustrate:
+
+// [
+//   ["   Death of a Salesman - Arthur Miller    "],
+//   ["   Macbeth - William Shakespeare    "],
+//   ["    A Separate Peace - John Knowles     "],
+//   [" Lord of the Flies - William Golding"],
+//   ["A Tale of Two Cities - Charles Dickens"]
+// ]
+
+// Create a function that takes an array like the one above and transforms it into the same format as the one below:
+
+// [
+//   ["Death of a Salesman", "Arthur Miller"],
+//   ["Macbeth", "William Shakespeare"],
+//   ["A Separate Peace", "John Knowles"],
+//   ["Lord of the Flies", "William Golding"],
+//   ["A Tale of Two Cities", "Charles Dickens"]
+// ]
+
+// Examples
+
+// tidyBooks([
+//   "     The Catcher in the Rye - J. D. Salinger    ",
+//   "    Brave New World - Aldous Huxley   ",
+//   "    Of Mice and Men - John Steinbeck    "
+// ]) ➞ [
+//   "The Catcher in the Rye", "J. D. Salinger",
+//   "Brave New World", "Aldous Huley",
+//   "Of Mice and Men", "John Steinbeck"
+// ]
+
+// Notes
+
+// Some of these entries have excess white space. Remove this white space in your final output.
+
+function tidyBooks(arr) {
+	return arr.map(ele => ele.trim().split(' - '))
+}
+
 // Given the parameters day, month and year, return whether that date is a valid date.
 // Examples
 
