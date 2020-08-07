62fd4d6528eac3f9272a2774d97dcf53f6e58fdd
Jack Chou <jcacct@protonmail.ch>
2020-08-07 10:27:37 -0700

    Solved Grab the Numbers

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// Given a string including a bunch of characters and numbers, return the sum of all the numbers in the string. Note that multiple digits next to each other are counted as a whole number rather than separate digits.
+// Examples
+
+// grabNumberSum("aeiou250abc10") ➞ 260
+
+// grabNumberSum("one1two2twenty20") ➞ 23
+
+// grabNumberSum("900uwu50uwuuwuuwu25uwu25") ➞ 1000
+
+// Notes
+
+// Remember not to just add single digit numbers together, it should be possible for answers to easily get into the 100s!
+
+function grabNumberSum(s) {
+  return s.match(/\d+/g).reduce((cur, acc) => cur + +acc, 0)
+}
+
 // Given an array with an odd number of elements, return whether the scale will tip "left" or "right" based on the sum of the numbers. The scale will tip on the direction of the largest total. If both sides are equal, return "balanced".
 // Examples
 
