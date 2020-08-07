ebdabac8a32e85af8ade2a4fc08cc312114b280b
Jack Chou <jcacct@protonmail.ch>
2020-08-07 12:23:37 -0700

    Solved Up the Hill, Down the Hill

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// If a person traveled up a hill for 18mins at 20mph and then traveled back down the same path at 60mph then their average speed traveled was 30mph.
+
+// Write a function that returns the average speed traveled given an uphill time, uphill rate and a downhill rate. Uphill time is given in minutes. Return the rate as an integer (mph). No rounding is necessary.
+// Examples
+
+// aveSpd(18, 20, 60) ➞ 30
+
+// aveSpd(30, 10, 30) ➞ 15
+
+// aveSpd(30, 8, 24) ➞ 12
+
+// Notes
+
+//     The solution is not dividing the sum of the speeds by 2.
+//     Check the Resources tab if your stuck.
+
+function aveSpd(upTime, upSpd, downSpd) {
+  const upHr = upTime / 60
+  const downHr = upHr * upSpd / downSpd
+  return ((upSpd * upHr) + (downSpd * downHr)) / (upHr + downHr)
+}
+
 // You're needed to finish a program that lists how many tall people work in your company. All that is needed is a regular expression that will make the function work correctly.
 
 // const res = ["tall height", "tall height", "short height", "medium height", "tall height"]
