196b22cfe21130b2956c94017276387fcd64dc7f
Jack Chou <jcacct@protonmail.ch>
2020-08-11 10:37:38 -0700

    Solved EdaBit Challenge

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,26 @@
+// Create a function that returns the array of numbers from a given range. But for multiples of three, return “Eda” instead of the number and for the multiples of five, return “Bit”. For numbers which are multiples of both three and five, return “EdaBit”.
+// Examples
+
+// edaBit(0, 10) ➞ ["EdaBit", 1, 2, "Eda", 4, "Bit", "Eda", 7, 8, "Eda", "Bit" ]
+
+// edaBit(14, 20) ➞ [14,  "EdaBit", 16, 17,  "Eda", 19, "Bit" ]
+
+// edaBit(99, 106) ➞ ["Eda", "Bit", 101, "Eda", 103, 104, "EdaBit", 106 ]
+
+// Notes
+
+// In case the number 0 happens to be in the range, return "EdaBit" as well.
+function edaBit(start, end) {
+  const arr = []
+	for (let i = start; i < end + 1; i++) {
+    if (i % 3 == 0 && i % 5 == 0) arr.push('EdaBit')
+    else if (i % 5 == 0) arr.push('Bit')
+    else if (i % 3 == 0) arr.push('Eda')
+    else arr.push(i)
+  }
+  return arr
+}
+
 // Create a function that takes in a two-dimensional array and returns the number of sub-arrays with identical elements.
 // Examples
 
