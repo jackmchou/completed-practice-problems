ad969e7a27d546508e143d90033d3c0d79e70aa3
Jack Chou <jcacct@protonmail.ch>
2020-08-13 08:22:42 -0700

    Solved Validating a Set in the Set Game

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,79 @@
+// In the game Set, three cards form a set if each of the cards are identical or completely different for each of the four properties. All three cards must:
+
+//     Have the same color or different colors.
+//     Have the same number or different numbers.
+//     Have the same shades or different shades.
+//     Have the same shape or different shapes.
+
+// The four properties are:
+
+//     Colors: red, purple, green
+//     Numbers: 1, 2, 3
+//     Shades: empty, lined, full
+//     Shapes: squiggle, oval, diamond
+
+// Here, a set is represented by an array containing three cards. Each card is represented by an object of properties and values. Write a function that determines whether three cards constitute a valid set.
+
+// Here is an example of a set:
+
+// [
+//   { color: "red", number: 1, shade: "empty", shape: "squiggle" },
+//   { color: "red", number: 2, shade: "lined", shape: "diamond" },
+//   { color: "red", number: 3, shade: "full", shape: "oval" }
+// ]
+
+// // "Same" properties: color
+// // "Different" properties: numbers, shading and shapes
+
+// The following is not a set:
+
+// [
+//   { color: "red", number: 1, shade: "empty", shape: "squiggle" },
+//   { color: "red", number: 2, shade: "lined", shape: "diamond" },
+//   { color: "purple", number: 3, shade: "full", shape: "oval" }
+// ]
+
+// // Colors are not all identical, but not all different.
+
+// Examples
+
+// isSet([
+//   { color: "green", number: 1, shade: "empty", shape: "squiggle" },
+//   { color: "green", number: 2, shade: "empty", shape: "diamond" },
+//   { color: "green", number: 3, shade: "empty", shape: "oval" }
+// ]) ➞ true
+
+// isSet([
+//   { color: "purple", number: 1, shade: "full", shape: "oval" },
+//   { color: "green", number: 1, shade: "full", shape: "oval" },
+//   { color: "red", number: 1, shade: "full", shape: "oval" }
+// ]) ➞ true
+
+// isSet([
+//   { color: "purple", number: 3, shade: "full", shape: "oval" },
+//   { color: "green", number: 1, shade: "full", shape: "oval" },
+//   { color: "red", number: 3, shade: "full", shape: "oval" }
+// ]) ➞ false
+
+// Notes
+
+//     A set cannot have 2/3 cards having the same property. Either all must share that property, or none will share that particular property.
+//     You can play Set by checking the Resources tab.
+
+function isSet(cards) {
+  const colors = new Set()
+  const numbers = new Set()
+  const shade = new Set()
+  const shape = new Set()
+	for (let i = 0; i < cards.length; i++) {
+    colors.add(cards[i].color)
+    numbers.add(cards[i].number)
+    shade.add(cards[i].shade)
+    shape.add(cards[i].shape)
+  }
+  return [colors.size, numbers.size, shade.size, shape.size].every(ele => ele !== 2)
+}
+
 // You will be implementing a basic case of the map-reduce pattern in programming. Use the built in JavaScript array functions .map() and .reduce() to solve the following problem.
 
 // Given a vector stored as an array of numbers, find the magnitude of the vector (this is similar to the function Math.hypot()). Use the standard distance formula for n-dimensional Cartesian coordinates.
