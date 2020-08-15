db392d3a2e0c3e43400ce4bc5f230da1f980f123
Jack Chou <jcacct@protonmail.ch>
2020-08-15 08:04:28 -0700

    Solved Calculate the Median

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// Create a function that takes an array of numbers and return its median. If the input array is even length, take the average of the two medians, else, take the single median.
+// Examples
+
+// median([2, 5, 6, 2, 6, 3, 4]) ➞ 4
+
+// median([21.4323, 432.54, 432.3, 542.4567]) ➞ 432.4
+
+// median([-23, -43, -29, -53, -67]) ➞ -43
+
+// Notes
+
+//     Input can be any negative or positive number.
+//     Input array will contain at least four numbers.
+//     See Resources tab for info on calculating the median.
+
+function median(arr) {
+	const sorted = arr.sort((a, b) => a - b)
+  const len = sorted.length
+  const mid = len / 2
+  return len % 2 === 1 ? sorted[mid | 0] : sorted.slice(mid - 1, mid + 1).reduce((cur, acc) => (cur + acc) / 2)
+}
+
 // Write a function that takes a string, and returns a new string with any duplicate consecutive letters removed.
 // Examples
 
