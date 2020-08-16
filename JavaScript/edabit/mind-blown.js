16b2a7e406fcfeb10ac423eec8705d72f22fcccb
Jack Chou <jcacct@protonmail.ch>
2020-08-16 11:37:02 -0700

    Solved Flash Cards

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,40 @@
+// Create a function that outputs the results of a flashcard. A flashcard is an array of three elements: a number, an operator symbol, and another number. Return the mathematical result of that expression.
+
+// There are 4 operators: + (addition), - (subtraction), x (multiplication), and / (division). If the flashcard displays a number being divided by zero, e.g. [3, "/", 0], then return undefined. For division, round to the hundredths place. So [10, "/", 3] should return 3.33.
+// Examples
+
+// flash([3, "x", 7]) ➞ 21
+
+// flash([5, "+", 7]) ➞ 12
+
+// flash([10, "-", 9]) ➞ 1
+
+// flash([10, "/", 0]) ➞ undefined
+
+// flash([10, "/", 3]) ➞ 3.33
+
+// Notes
+
+// Flash cards contain only zero or positive numbers.
+
+function flash([num1, op, num2]) {
+  if (op === '/' && num2 === 0) return undefined
+  switch (op) {
+    case 'x':
+    return num1 * num2
+    break
+    case '+':
+    return num1 + num2
+    break
+    case '-':
+    return num1 - num2
+    break
+    case '/':
+    return +(num1 / num2).toFixed(2)
+    break;
+  }
+}
+
 // Syncopation means an emphasis on a weak beat of a bar of music; most commonly, beats 2 and 4 (and all other even-numbered beats if applicable).
 
 // s is a line of music, represented as a string, where hashtags # represent emphasized beats. Create a function that returns if the line of music contains any syncopation.
