5123cd2bb3f74d0177471d3ace44f08ae733279a
Jack Chou <jcacct@protonmail.ch>
2020-08-20 08:00:42 -0700

    Solved Smallest N Digit Number

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Write a function that returns the smallest N-digit number which is a multiple of the specified value.
+// Examples
+
+// smallest(3, 8) ➞ 104
+// // Smallest 3-digit integer that is a multiple of 8
+
+// smallest(5, 12) ➞ 10008
+
+// smallest(7, 1) ➞ 1000000
+
+// smallest(2, 3) ➞ 12
+
+// Notes
+
+// N/A
+
+function smallest(digits, value) {
+  let num = Math.pow(10, digits - 1)
+  return num + value - (num % value || value)
+}
+
 // The facts are:
 
 //     You've just finished dinner.
