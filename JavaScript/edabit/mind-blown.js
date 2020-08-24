b7d6978392c0968420fd430c7a7f43cbeceab933
Jack Chou <jcacct@protonmail.ch>
2020-08-24 09:25:17 -0700

    Solved cRegEx: Character Classes X ⁠- \s

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// You can think of character classes as characters with special meaning. They are recognized as special when you place the \ before the character.
+
+// Here are a list of the characters classes in JavaScript:
+
+// ., \cX, \d, \D, \f, \n, \r, \s, \S, \t, \v, \w, \W, \0, \xhh, \uhhhh, \uhhhhh, [\b]
+
+// Given the below string, write a regex that will find all open compound words starting with the word best and the second word begins with the letter b.
+
+// "best buy best car best friend best-boy bestguest best dressed best bet best man best deal best boyfriend"
+
+// Notes
+
+//     Open compound words are words with spaces in them.
+//     Check the Resources tab for details on character classes if you're stuck.
+
+const REGEXP = /best\sb\w+/g
+
 // Create a function that counts the number of adverbs in a sentence. An adverb is a word that ends with ly.
 // Examples
 
