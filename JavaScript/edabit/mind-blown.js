51d67dfefa5ea720c960d381ae124a565639b63f
Jack Chou <jcacct@protonmail.ch>
2020-08-26 06:42:48 -0700

    Solved Merge Two Arrays

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,34 @@
+// Create a function that takes two arrays and combines them by alternatingly taking elements from each array in turn.
+
+//     The arrays may be of different lengths, with at least one character / digit.
+//     The first array will contain string characters (lowercase, a-z).
+//     The second array will contain integers (all positive).
+
+// Examples
+
+// mergeArrays(["f", "d", "w", "t"], [5, 3, 7, 8])
+// ➞ ["f", 5, "d", 3, "w", 7, "t", 8]
+
+// mergeArrays([1, 2, 3], ["a", "b", "c", "d", "e", "f"])
+// ➞ [1, "a", 2, "b", 3, "c", "d", "e", "f"]
+
+// mergeArrays(["a", "b", "c", "d", "e"], [1, 2, 3, 4, 5])
+// ➞ ["a", 1, "b", 2, "c", 3, "d", 4, "e", 5]
+
+// Notes
+
+// N/A
+
+function mergeArrays(a, b) {
+  const len = a.length > b.length ? a.length : b.length
+  const result = []
+	for (let i = 0; i < len; i++) {
+    if (a[i]) result.push(a[i])
+    if (b[i]) result.push(b[i])
+  }
+  return result
+}
+
 // Captain Hook and his crew are currently resting at Origin Shore. They are about to embark on their next adventure to an undisclosed location (x, y) to find treasure.
 
 // Captain Hook's ship can only move exactly north, south, east or west. It takes exactly 1 day for the ship to travel 1 unit in one of the four cardinal directions.
