7c16f7ba4911c4877bb45145e35d83ef931c0b64
Jack Chou <jcacct@protonmail.ch>
2020-09-02 08:49:30 -0700

    Solved Sum of Odd and Even Numbers

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,29 @@
+// Write a function that takes an array of numbers and returns an array with two elements:
+
+//     The first element should be the sum of all even numbers in the array.
+//     The second element should be the sum of all odd numbers in the array.
+
+// Example
+
+// sumOddAndEven([1, 2, 3, 4, 5, 6]) ➞ [12, 9]
+// // 2 + 4 + 6 = 12 and 1 + 3 + 5 = 9
+
+// sumOddAndEven([-1, -2, -3, -4, -5, -6]) ➞ [-12, -9])
+
+// sumOddAndEven([0, 0]) ➞ [0, 0])
+
+// Notes
+
+// Count 0 as an even number.
+
+function sumOddAndEven(arr) {
+	const result = [0, 0]
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 === 0 ? result[0] += arr[i] : result[1] += arr[i]
+  }
+  return result
+}
+
 // Given an array of arrays, return a new an array of arrays containing every element, except for the outer elements.
 // Examples
 
