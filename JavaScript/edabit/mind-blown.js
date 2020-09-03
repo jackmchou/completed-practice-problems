79d5201d1fcad7a968ec3157888661925943d71a
Jack Chou <jcacct@protonmail.ch>
2020-09-03 07:56:18 -0700

    Solved Left, Right Shift

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,50 @@
+// Create two functions: a left-shift function and a right-shift function. Each function will take in an array and a single parameter: the number of shifts.
+
+// [1, 2, 3, 4, 5]
+
+// [2, 3, 4, 5, 1]  // left shift of 1
+// [5, 1, 2, 3, 4]  // left shift of 4
+
+// [5, 1, 2, 3, 4]  // right shift of 1
+// [3, 4, 5, 1, 2]  // right shift of 3
+
+// Examples
+
+// leftShift([1, 2, 3, 4], 1) ➞ [2, 3, 4, 1]
+
+// rightShift([1, 2, 3, 4], 1) ➞ [4, 1, 2, 3]
+
+// leftShift([1, 2, 3, 4, 5], 3) ➞ [4, 5, 1, 2, 3]
+
+// leftShift([1, 2, 3, 4, 5], 5) ➞ [1, 2, 3, 4, 5]
+// // You have fully shifted the array, you end up back where you began.
+
+// leftShift([1, 2, 3, 4, 5], 6) ➞ [2, 3, 4, 5, 1]
+// // You should be able to take in numbers greater than the length.
+// // Think of the length of the array as a modulo.
+
+// Notes
+
+// N/A
+
+function leftShift(arr, num) {
+	if (arr.length == num) return arr
+  for (let i = num; i > 0; i--) {
+    let shifted = arr.shift()
+    arr.push(shifted)
+  }
+  return arr
+}
+
+function rightShift(arr, num) {
+	if (arr.length == num) return arr
+  for (let i = 0; i < num; i++) {
+    let popped = arr.pop()
+    arr.unshift(popped)
+  }
+  return arr
+}
+
 // Write a function that takes an array of numbers and returns an array with two elements:
 
 //     The first element should be the sum of all even numbers in the array.
