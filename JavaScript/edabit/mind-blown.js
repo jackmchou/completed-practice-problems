2ceffc56199ebce08f0af03e781904c84584116d
Jack Chou <jcacct@protonmail.ch>
2020-09-05 05:32:43 -0700

    Solved One, Two, Skip a Few

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// Create a function which calculates how many numbers are missing from an ordered number line.
+// howManyMissing([1, 2, 3, 8, 9]) ➞ 4
+// // The number line starts at 1 and ends at 9 (so the numbers 0 and 10 aren't missing from it).
+// // The numbers missing from this line are 4, 5, 6, and 7.
+// // 4 numbers are missing.
+// Examples
+// howManyMissing([1, 3]) ➞ 1
+// howManyMissing([7, 10, 11, 12]) ➞ 2
+// howManyMissing([1, 3, 5, 7, 9, 11]) ➞ 5
+// howManyMissing([5, 6, 7, 8]) ➞ 0
+// Notes
+// If the number line is complete, or the array is empty, return 0.
+
+function howManyMissing(arr) {
+  let count = 0
+  for (let i = 0; i < arr.length - 1; i++) {
+    const missing = (arr[i + 1] - arr[i]) - 1
+    if (missing !== 0) count += missing
+  }
+  return count
+}
+
 // You just returned home to find your mansion has been robbed! Given an object of the stolen items, return the total amount of the burglary (number). If nothing was robbed, return the string "Lucky you!".
 // Examples
 // const stolenItems = {
