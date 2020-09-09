6334e2970a26262840674442f86cae9f9fc9b038
Jack Chou <jcacct@protonmail.ch>
2020-09-09 10:29:55 -0700

    Solved Dashed Vowels

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,21 @@
+// Create a function that takes a string and returns dashes on the left and right side of every vowel (a e i o u).
+// Examples
+
+// dashed("Edabit") ➞ "-E-d-a-b-i-t"
+
+// dashed("Carpe Diem") ➞ "C-a-rp-e- D-i--e-m"
+
+// dashed("Fight for your right to party!") ➞ "F-i-ght f-o-r y-o--u-r r-i-ght t-o- p-a-rty!"
+
+// Notes
+
+//     A string can contain uppercase and lowercase vowels.
+//     Y is not considered a vowel.
+
+function dashed(str) {
+  return [...str].map(ele => 'AEIOUaeiou'.includes(ele) ? `-${ele}-` : ele).join('')
+}
+
 // Given an integer, create a function that returns the next prime. If the number is prime, return the number itself.
 // Examples
 // nextPrime(12) ➞ 13
