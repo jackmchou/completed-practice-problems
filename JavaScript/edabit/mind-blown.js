4cb6af100311a2a8d298ce6250be33cc26f79375
Jack Chou <jcacct@protonmail.ch>
2020-09-10 07:57:38 -0700

    Solved Ranged Reversal

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,26 @@
+// Write a function that reverses the subarray between the start and end index (inclusive). The rest of the array should be kept the same.
+// Examples
+
+// rangedReversal([1, 2, 3, 4, 5, 6], 1, 3) ➞ [1, 4, 3, 2, 5, 6]
+
+// rangedReversal([1, 2, 3, 4, 5, 6], 0, 4) ➞ [5, 4, 3, 2, 1, 6]
+
+// rangedReversal([9, 8, 7, 4], 0, 0) ➞ [9, 8, 7, 4]
+
+// Notes
+
+//     Arrays will be zero-indexed.
+//     The start and end indices will always be valid in context of the array.
+//     The end index will always be greater than or equal to the starting index.
+//     Return the array itself if the starting and ending index are the same.
+
+function rangedReversal(arr, start, end) {
+  if (start == end) return arr
+  let reversed = arr.slice(start, end + 1).reverse()
+  arr.splice(start, reversed.length, ...reversed)
+  return arr
+}
+
 // Create a function that takes a string and returns dashes on the left and right side of every vowel (a e i o u).
 // Examples
 
