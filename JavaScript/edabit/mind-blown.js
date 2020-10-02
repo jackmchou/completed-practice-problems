cf28c3c05b9d5c251b1264522fbffd317e09895f
Jack Chou <jcacct@protonmail.ch>
2020-10-02 12:22:09 -0700

    Solved Time Conversion

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,22 @@
+// Write a function that takes the number of seconds and returns the digital format clock time as a string. Time should be counted from 00:00:00.
+// Examples
+// digitalClock(5025) ➞ "01:23:45"
+// // 5025 seconds is 1 hour, 23 mins, 45 secs.
+// digitalClock(61201) ➞ "17:00:01"
+// // No AM/PM. 24h format.
+// digitalClock(87000) ➞ "00:10:00"
+// // It's 00:10 next day.
+// Notes
+// seconds is always greater than or equal to 0.
+
+function digitalClock(seconds) {
+  const timeStr = time => time < 10 ? ('' + time).padStart(2, '0') : '' + time
+  const hour = seconds / 3600 | 0
+  const min = (seconds / 60 - hour * 60) | 0
+  const sec = seconds - (hour * 3600 + min * 60) | 0
+  return `${timeStr(hour) >= 24 ? timeStr(timeStr(hour) % 24) : timeStr(hour)}:${timeStr(min)}:${timeStr(sec)}`
+}
+
 // There's a great war between the even and odd numbers. Many numbers already lost their life in this war and it's your task to end this. You have to determine which group is larger: the even, or the odd. The larger group wins.
 // Create a function that takes an array of integers, sums the even and odd numbers seperately, then returns the larger of the sums minus the smaller.
 // Examples
