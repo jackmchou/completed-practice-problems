fed079fb01c882f40e3e541bde1a09193c8f06dc
Jack Chou <jcacct@protonmail.ch>
2020-10-10 06:59:14 -0700

    Solved Measure the Depth of Emptiness

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,28 @@
+// In this challenge you will receive an input of the form:
+
+// [[[[[[[[[[[]]]]]]]]]]]
+
+// In other words, an array containing an array containing an array containing... an array containing nothing.
+
+// Your goal is to measure the depth of this array, where [] has a depth 1, [[]] has depth of 2, [[[]]] has depth 3, etc.
+// Examples
+
+// measureDepth([]) ➞ 1
+
+// measureDepth([[]]) ➞ 2
+
+// measureDepth([[[]]]) ➞ 3
+
+// measureDepth([[[[[[[[[[[]]]]]]]]]]]) ➞ 11
+
+// Notes
+
+// For a bonus challenge, try to find a solution without recursion.
+
+function measureDepth(arr) {
+  return JSON.stringify(arr).length / 2
+}
+
 // Create a function that takes an integer n and returns the nth tetrahedral number.
 
 // Alternative Text
