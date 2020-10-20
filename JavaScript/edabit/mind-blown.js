747dea958497ae4933ac97515d40ed493ec65ae5
Jack Chou <jcacct@protonmail.ch>
2020-10-20 13:19:13 -0700

    Solved Is the Sum of Letters Even or Odd?

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Create a function that takes a string and returns true if the sum of the position of every letter in the alphabet is even and false if the sum is odd.
+// Examples
+
+// isAlpha("i'am king")  ➞ true
+// // 9 + 1 + 13 + 11 + 9 + 14 + 7 = 64 (even)
+
+// isAlpha("True") ➞ true
+// // 20 + 18 + 21 + 5= 64 (even)
+
+// isAlpha("alexa") ➞ false
+// // 1 + 12 + 5 + 24 + 1= 43 (odd)
+
+// Notes
+
+//     Case insensitive.
+//     Ignore non-letter symbols.
+
+function isAlpha(word) {
+	return [...word.match(/[^\W\d_]/g)].map(ele => ele.charCodeAt(0) - 96).reduce((cur, acc) => cur + acc) % 2 == 0
+}
+
 // Create a function that takes an array of resistors and calculates the output of total resistance if the circuit is connected in parallel or in series.
 // Examples
 // resistanceCalculator([10, 20, 30, 40, 50]) ➞ [4.38, 150]
