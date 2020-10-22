b96fd5990180e5dd2c4c7fc860541acf8c182568
Jack Chou <jcacct@protonmail.ch>
2020-10-21 19:57:56 -0700

    Solved Fruit Juices

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,23 @@
+// A fruit juice company tags their fruit juices by concatenating the first three letters of the words in a flavor's name and its capacity.
+
+// Create a function that creates the product IDs for the variety of fruit juices.
+// Examples
+
+// getDrinkID("apple", "500ml") ➞ "APP500"
+
+// getDrinkID("pineapple", "45ml") ➞ "PIN45"
+
+// getDrinkID("passion fruit", "750ml") ➞ "PASFRU750"
+
+// Notes
+
+//     Capacity will be given as a string and will always be given in ml.
+//     Return the product ID in UPPERCASE.
+
+function getDrinkID(flavor, ml) {
+	return [...flavor.split(' ')].map(ele => ele.slice(0, 3)).join('').toUpperCase() + ml.split('ml')[0]
+}
+
 // Create a function that takes a string and returns true if the sum of the position of every letter in the alphabet is even and false if the sum is odd.
 // Examples
 
