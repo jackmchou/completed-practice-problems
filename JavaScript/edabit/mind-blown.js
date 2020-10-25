95b4604553c0394b0afc6626c5672a3fc04bbeea
Jack Chou <jcacct@protonmail.ch>
2020-10-25 10:29:40 -0700

    Solved Snail Goes Up the Stairs

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// A snail goes up the stairs. Every step, he must go up the step, then go across to the next step. He wants to reach the top of the tower.
+// Write a function that returns the distance the snail must travel to the top of the tower given the height and length of each step and the height of the tower.
+// Examples
+// totalDistance(0.2, 0.4, 100.0) ➞ 300.0
+// // Total distance is 300.
+// totalDistance(0.3, 0.2, 25.0) ➞ 41.7
+// totalDistance(0.1, 0.1, 6.0) ➞ 12.0
+// Notes
+//     All given values are greater than 0.
+//     Round answers to the nearest tenth 0.1.
+//     Number of steps determined by tower height divided by stair height.
+//     For the purposes of this exercise, the last step's length must be counted to complete the journey.
+
+function totalDistance(height, length, tower) {
+	return +((tower / height) * (height + length)).toFixed(1)
+}
+
 // Write a DECIMATOR function which takes a string and decimates it (i.e. it removes the last 1/10 of the characters).
 // Always round up: if the string has 21 characters, 1/10 of the characters would be 2.1 characters, hence the DECIMATOR removes 3 characters. The DECIMATOR shows no mercy!
 // Examples
