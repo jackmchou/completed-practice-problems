d07d3f3f9221207c242390017369ce6f2fc26904
Jack Chou <jcacct@protonmail.ch>
2020-10-26 17:04:43 -0700

    Solved Derivative of a Function

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,15 @@
+// Create a function that takes numbers b and m as arguments and returns the derivative of the function f(x)=x^b with respect to x evaluated at x=m, where b and m are constants.
+// Examples
+// derivative(1, 4) ➞ 1
+// derivative(3, -2) ➞ 12
+// derivative(4, -3) ➞ -108
+// Notes
+// ^ in the context of this challenge means "to the power of", also known as the "exponent" operator.
+
+function derivative(b, m) {
+	return b * m ** (b - 1)
+}
+
 // An array that represents a Binary Tree is in the following form:
 // binaryTree = [val, arrLeft, arrRight]
 // When arrLeft is the left side of the tree and arrRight is the right side of the tree.
