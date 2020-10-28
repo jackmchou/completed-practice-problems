b5a1697a75908af0c34086ced3b262f119e74c57
Jack Chou <jcacct@protonmail.ch>
2020-10-28 09:05:34 -0700

    Solved Nth Fibonacci Number

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,32 @@
+// Create a function to return the Nth number in the Fibonacci sequence as a string.
+// Examples
+
+// fibonacci(10) ➞ "55"
+
+// fibonacci(20) ➞ "6765"
+
+// fibonacci(30) ➞ "832040"
+
+// fibonacci(40) ➞ "102334155"
+
+// fibonacci(50) ➞ "12586269025"
+
+// fibonacci(60) ➞ "1548008755920"
+
+// Notes
+
+// Your function is expected to calculate numbers greater than the 64-bit unsigned integer limit where n can be as large as but not greater than 200.
+
+function fibonacci(n) {
+  let idx = n
+  let arr = [0n, 1n]
+  while (n >= 0) {
+    n--
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2])
+  }
+  return '' + arr[idx]
+}
+
 // Given a string containing unique letters, return a sorted string with the letters that don't appear in the string.
 // Examples
 
