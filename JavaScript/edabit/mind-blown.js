f13dd9667a14729f8a255ad3b9ab9bd0d4d5d52a
Jack Chou <jcacct@protonmail.ch>
2020-10-30 13:49:54 -0700

    Solved Zip It, If You Can?

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,29 @@
+// Given an array of women and an array of men, either:
+
+//     Return "sizes don't match" if the two arrays have different sizes.
+//     If the sizes match, return an array of pairs, with the first woman paired with the first man, second woman paired with the second man, etc.
+
+// Examples
+
+// zipIt(["Elise", "Mary"], ["John", "Rick"])
+//  ➞ [["Elise", "John"], ["Mary", "Rick"]]
+
+// zipIt(["Ana", "Amy", "Lisa"], ["Bob", "Josh"])
+//  ➞ "sizes don't match"
+
+// zipIt(["Ana", "Amy", "Lisa"], ["Bob", "Josh", "Tim"])
+//  ➞ [["Ana", "Bob"], ["Amy", "Josh"],["Lisa", "Tim"]]
+
+// Notes
+
+// N/A
+
+
+function zipIt(women, men) {
+	if (women.length !== men.length) return "sizes don't match"
+  return women.map((ele, idx) => [ele, men[idx]])
+}
+
 // Create a function that will remove the letters "a", "b" and "c" from the given string and return the modified version. If the given string does not contain "a", "b", or "c", return null.
 // Examples
 
