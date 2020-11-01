87036cc4a02aa50340fda8c75a0b89241f3c2aa7
Jack Chou <jcacct@protonmail.ch>
2020-11-01 07:37:42 -0800

    Solved Triple Letter Groupings

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,33 @@
+// Given a string, return a sorted array of words formed from the first three letters, then the next three letters, shifting by only one.
+// Worked Example
+
+// threeLetterCollection("edabit") ➞ ["abi", "bit", "dab", "eda"]
+// // 1st word: "eda"
+// // 2nd word: "dab"
+// // 3rd word: "abi"
+// // 4th word: "bit"
+// // Remember to sort the array!
+
+// Examples
+
+// threeLetterCollection("slap") ➞ ["lap", "sla"]
+
+// threeLetterCollection("click") ➞ ["cli", "ick", "lic"]
+
+// threeLetterCollection("cat") ➞ ["cat"]
+
+// threeLetterCollection("hi") ➞ []
+
+// Notes
+
+// Return an empty array if given a word with less than 3 letters.
+
+function threeLetterCollection(s) {
+	return [...s].map((ele, idx, arr) => arr[idx + 2] ? ele + arr[idx + 1] + arr[idx + 2] : null)
+               .filter(ele => ele)
+               .sort()
+}
+
 // Create a function that takes an integer n and returns multiplication table of 1 to n numbers up to nth multiple.
 // Examples
 // multTable(2) ➞ [
