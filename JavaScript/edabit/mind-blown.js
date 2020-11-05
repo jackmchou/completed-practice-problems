4defdd8bb34e589fac5e62c63579274cbdf68737
Jack Chou <jcacct@protonmail.ch>
2020-11-05 12:42:33 -0800

    Solved Return the Most Expensive Piece of Jewellery

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,32 @@
+// You go to a jewelry shop and try to find the most expensive piece of jewelry. You write down the name of each piece of jewelry and its price.
+
+// Create a function that gets the name of the piece of jewelry with the highest price and returns "The most expensive one is the {name of jewelry piece}".
+// Examples
+
+// mostExpensive ({
+//   "Diamond Earrings": 980,
+//   "Gold Watch": 250,
+//   "Pearl Necklace": 4650
+// }) ➞  "The most expensive one is the Pearl Necklace"
+
+// mostExpensive({
+//   "Silver Bracelet": 280,
+//   "Gemstone Earrings": 180,
+//   "Diamond Ring": 3500
+// }) ➞ "The most expensive one is the Diamond Ring"
+
+// Notes
+
+// N/A
+
+function mostExpensive(obj) {
+  let mostExpensive
+  for (let key in obj) {
+    if (obj[key] == Math.max(...Object.values(obj))) mostExpensive = key
+  }
+	return `The most expensive one is the ${mostExpensive}`
+}
+
 // Given a number, return a string which is formatted into US Dollars and cents!
 
 // Remember that:
