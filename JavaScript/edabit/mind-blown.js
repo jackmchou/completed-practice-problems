6e9c7ea5292f29b387902b9bafe1fdef9109e23a
Jack Chou <jcacct@protonmail.ch>
2020-11-06 13:16:19 -0800

    Solved Adding Both Ends Together

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,28 @@
+// Given an array of numbers, of any length, create a function which counts how many of those numbers pass the following criterea:
+
+//     The first and last digits of a number must add to 10.
+
+// Examples
+
+// endsAddTo10([19, 46, 2098]) ➞ 3
+
+// endsAddTo10([33, 44, -55]) ➞ 1
+
+// endsAddTo10([]) ➞ 0
+
+// Notes
+
+//     All items in the array will be numbers.
+//     Ignore negative signs (see example #2).
+//     If given an empty array, return 0.
+
+function endsAddTo10(nums) {
+	return nums.filter(ele => {
+    let numStr = '' + Math.abs(ele)
+    return +numStr[0] + +numStr.slice(-1) == 10
+  }).length
+}
+
 // You go to a jewelry shop and try to find the most expensive piece of jewelry. You write down the name of each piece of jewelry and its price.
 
 // Create a function that gets the name of the piece of jewelry with the highest price and returns "The most expensive one is the {name of jewelry piece}".
