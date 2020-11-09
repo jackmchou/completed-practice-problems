66aba6baac52989f19f6b111dc4fa67c4ed39450
Jack Chou <jcacct@protonmail.ch>
2020-11-09 08:04:58 -0800

    Solved Largest Gap

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,46 @@
+// Given an array of integers, return the largest gap between elements of the sorted version of that array.
+
+// Here's an illustrative example. Consider the array:
+
+// [9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5]
+
+// ... which, after sorting, becomes the array:
+
+// [0, 0, 4, 5, 5, 6, 9, 20, 25, 26, 26]
+
+// ... so that we now see that the largest gap in the array is the gap of 11 between 9 and 20.
+// Examples
+
+// largestGap([9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5]) ➞ 11
+// // After sorting get [0, 0, 4, 5, 5, 6, 9, 20, 25, 26, 26]
+// // Largest gap of 11 between 9 and 20
+
+// largestGap([14, 13, 7, 1, 4, 12, 3, 7, 7, 12, 11, 5, 7]) ➞ 4
+// // After sorting get [1, 3, 4, 5, 7, 7, 7, 7, 11, 12, 12, 13, 14]
+// // Largest gap of 4 between 7 and 11
+
+// largestGap([13, 3, 8, 5, 5, 2, 13, 6, 14, 2, 11, 4, 10, 8, 1, 9]) ➞ 2
+// // After sorting get [1, 2, 2, 3, 4, 5, 5, 6, 8, 8, 9, 10, 11, 13, 13, 14]
+// // Largest gap of 2 between 6 and 8
+
+// Notes
+
+// N/A
+
+function largestGap(arr) {
+  let tmp
+	for (let i = 0; i < arr.length; i++) {
+    for (let ii = i + 1; ii < arr.length; ii++) {
+      if (arr[i] > arr[ii]) {
+        tmp = arr[i]
+        arr[i] = arr[ii]
+        arr[ii] = tmp
+      }
+    }
+  }
+  return Math.max(...arr.map((ele, idx, arr) => arr[idx + 1] || arr[idx + 1] === 0 ? arr[idx + 1] - ele : ele - arr[idx - 1]))
+}
+
 // Create a function that takes two inputs: idx (an array of integers) and str (a string). The function should return another string with the letters of str at each index in idx in order.
 // Examples
 
