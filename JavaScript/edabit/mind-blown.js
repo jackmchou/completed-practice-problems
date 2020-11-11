4abaca7a2bf86447ce4f669ce21e59f2e722c72a
Jack Chou <jcacct@protonmail.ch>
2020-11-10 20:27:34 -0800

    Solved All Occurrences of an Element in an Array

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Create a function that returns the indices of all occurrences of an item in the array.
+// Examples
+
+// getIndices(["a", "a", "b", "a", "b", "a"], "a") ➞ [0, 1, 3, 5]
+
+// getIndices([1, 5, 5, 2, 7], 7) ➞ [4]
+
+// getIndices([1, 5, 5, 2, 7], 5) ➞ [1, 2]
+
+// getIndices([1, 5, 5, 2, 7], 8) ➞ []
+
+// Notes
+
+//     If an element does not exist in an array, return [].
+//     Arrays are zero-indexed.
+//     Values in the array will be value-types (don't need to worry about nested arrays).
+
+function getIndices(arr, el) {
+	return arr.map((ele, idx) => ele == el ? idx : null).filter(ele => ele !== null)
+}
+
 // Given an array of integers, return the largest gap between elements of the sorted version of that array.
 
 // Here's an illustrative example. Consider the array:
