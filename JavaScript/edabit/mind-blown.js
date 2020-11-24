8a7986c96d5edae0d3151c68852da65b5cc8d8fb
Jack Chou <jcacct@protonmail.ch>
2020-11-23 17:56:11 -0800

    Solved From A to Z

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,27 @@
+// Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
+// Examples
+
+// gimmeTheLetters("a-z") ➞ "abcdefghijklmnopqrstuvwxyz"
+
+// gimmeTheLetters("h-o") ➞ "hijklmno"
+
+// gimmeTheLetters("Q-Z") ➞ "QRSTUVWXYZ"
+
+// gimmeTheLetters("J-J") ➞ "J"
+
+// Notes
+
+//     A hyphen will separate the two letters in the string.
+//     You don't need to worry about error handling in this one (i.e. both letters will be the same case and the second letter will always be after the first alphabetically).
+
+function gimmeTheLetters(spectrum) {
+  const result = []
+	for (let i = spectrum.charCodeAt(0); i <= spectrum.charCodeAt(2); i++){
+    result.push(String.fromCharCode(i))
+  }
+  return result.join('')
+}
+
 // Create a function that replaces all consecutively repeated letters in a word with single letters.
 // Examples
 
