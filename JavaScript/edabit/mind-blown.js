fce24bb3b9a7203c7715338272d300ca5fa4a5a0
Jack Chou <jcacct@protonmail.ch>
2020-11-25 11:14:59 -0800

    Solved Product of Two Largest Numbers

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,33 @@
+// Create a function that takes an array of numbers and returns the product of the largest and second largest UNIQUE numbers in the array. So, if there are multiple of the same highest integer, only count one towards the highest product -- see the examples below for more.
+// Examples
+
+// product([2, 3, 1, -1, 2]) ➞ 6
+// // 2 * 3 = 6
+
+// product([-2, -2, -1, -1]) ➞ 2
+// // -2 * - 1 = 2
+// // Not 1, because you can only use -1 one time.
+
+// product([8, 8, 8]) ➞ 64
+// // 8 * 8 = 64
+// // You can repeat here because there is only one value.
+
+// product([2, 8, 8, 8]) ➞ 16
+// // 2 * 8 = 16
+// // Not 64, because you can only use 8 one time.
+
+// Notes
+
+//     Numbers in the array are all integers.
+//     Numbers can be both positive or negative.
+
+function product(arr) {
+  const arrUniq = [...new Set(arr)]
+  if (arrUniq.length == 1) return arrUniq[0] ** 2
+  const sorted = arrUniq.sort((a, b) => b - a)
+  return sorted[0] * sorted[1]
+}
+
 // Create a function that takes an arr and returns the total amount of space between the two "1"s.
 // Examples
 
