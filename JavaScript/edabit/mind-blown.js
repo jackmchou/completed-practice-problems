22e2fa29efcc3429051532ea779eb786f005b777
Jack Chou <jcacct@protonmail.ch>
2020-11-29 09:43:24 -0800

    Solved Total Sales of Product

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,61 @@
+// In this question you will be given a table as below, where the first row lists the names of products, and each of row after that lists the sales of the product for each day (over some time range).
+
+// [
+//   ["A", "B", "C"],
+//   [ 2 ,  7 ,  1 ],
+//   [ 3 ,  6 ,  6 ],
+//   [ 4 ,  5 ,  5 ]
+// ]
+
+// Write a function that receives:
+
+//     A sales table sales as shown above.
+//     The name of a product product.
+
+// ... and returns the total sales if the product is on the array, otherwise return the string "Product not found".
+// Examples
+
+// totalSales([
+//   ["A", "B", "C"],
+//   [ 2 ,  7 ,  1 ],
+//   [ 3 ,  6 ,  6 ],
+//   [ 4 ,  5 ,  5 ]
+// ], "A") ➞ 9
+
+// // 2 + 3 + 4 = 9
+
+
+// totalSales([
+//   ["A", "B", "C"],
+//   [ 2 ,  7 ,  1 ],
+//   [ 3 ,  6 ,  6 ],
+//   [ 4 ,  5 ,  5 ]
+// ], "C") ➞ 12
+
+// // 1 + 6 + 5 = 12
+
+
+// totalSales([
+//   ["A", "B", "C"],
+//   [ 2 ,  7 ,  1 ],
+//   [ 3 ,  6 ,  6 ],
+//   [ 4 ,  5 ,  5 ]
+// ], "D") ➞ "Product not found"
+
+// Notes
+
+// The examples above all use the same sales table, but in the tests the table will vary.
+
+function totalSales(sales, product) {
+  let productIdx = sales[0].indexOf(product)
+  let productSales = []
+	if (!sales[0].includes(product)) return 'Product not found'
+  for (let i = 1; i < sales.length; i++) {
+    productSales.push(sales[i][productIdx])
+  }
+  return productSales.reduce((cur, acc) => cur + acc, 0)
+}
+
 // Create a function that takes an amount of monetary change (e.g. 47 cents) and breaks down the most efficient way that change can be made using USD quarters, dimes, nickels and pennies. Your function should return an object.
 // Examples
 // makeChange(47) ➞ { "q": 1, "d": 2, "n": 0, "p": 2 }
