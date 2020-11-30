8f83f2ceda2caed255e1aaa4f388a921a9fde169
Jack Chou <jcacct@protonmail.ch>
2020-11-30 11:55:33 -0800

    Solved Meme Sum :)

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,29 @@
+// For this challenge, forget how to add two numbers together. The best explanation on what to do for this function is this meme:
+
+// Alternative Text
+// Examples
+
+// memeSum(26, 39) ➞ 515
+// // 2+3 = 5, 6+9 = 15
+// // 26 + 39 = 515
+
+// memeSum(122, 81) ➞ 1103
+// // 1+0 = 1, 2+8 = 10, 2+1 = 3
+// // 122 + 81 = 1103
+
+// memeSum(1222, 30277) ➞ 31499
+
+// Notes
+
+// N/A
+
+function memeSum(a, b) {
+  let aStr = ('' + a)
+  let bStr = ('' + b)
+  aStr.length < bStr.length ? aStr = aStr.padStart(bStr.length, '0') : bStr = bStr.padStart(aStr.length, '0')
+  return +[...aStr].map((ele, idx) => +ele + +bStr[idx]).join('')
+}
+
 // In this question you will be given a table as below, where the first row lists the names of products, and each of row after that lists the sales of the product for each day (over some time range).
 
 // [
