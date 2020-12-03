aa7886b2b1867e2843745903aaafcf0f0092e8a6
Jack Chou <jcacct@protonmail.ch>
2020-12-03 07:07:52 -0800

    Solved Diagonal of a Cube

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// Create a function that takes the volume of a cube and returns the length of the cube's main diagonal, rounded to two decimal places.
+// Examples
+
+// cubeDiagonal(8) ➞ 3.46
+
+// cubeDiagonal(343) ➞ 12.12
+
+// cubeDiagonal(1157.625) ➞ 18.19
+
+// Notes
+
+// N/A
+
+function cubeDiagonal(volume) {
+	return +(Math.sqrt(3) * Math.cbrt(volume)).toFixed(2)
+}
+
 // Create a function that counts the integer's number of digits.
 // Examples
 
