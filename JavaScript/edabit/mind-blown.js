db21a21a21a5959bbd960c8c15050cd53eead813
Jack Chou <jcacct@protonmail.ch>
2020-12-08 07:19:55 -0800

    Solved Drink Sorting

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+// Assume that the following array of drink objects needs to be sorted:
+
+// drinks = [
+//   {name: "lemonade", price: 50},
+//   {name: "lime", price: 10}
+// ]
+
+// The output of the sorted drinks object will be:
+// Examples
+
+// sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}]
+
+// Notes
+
+// N/A
+
+function sortDrinkByPrice(drinks) {
+  return drinks.sort((a, b) => a.price - b.price)
+}
+
 // Create a method in the Person class which returns how another person's age compares. Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
 
 // {other person name} is {older than / younger than / the same age as} me.
