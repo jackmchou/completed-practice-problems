65bc997e83f9c46a5d4f02a0f3016cb7debb1d26
Jack Chou <jcacct@protonmail.ch>
2020-12-10 07:33:39 -0800

    Solved Find the Amount of Potatoes

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,20 @@
+// Create a function to return the amount of potatoes there are in a string.
+// Examples
+
+// potatoes("potato") ➞ 1
+
+// potatoes("potatopotato") ➞ 2
+
+// potatoes("potatoapple") ➞ 1
+
+// Notes
+
+// N/A
+
+function potatoes(str) {
+  return str.match(/potato/g).length
+}
+
 // Create a function that takes two arrays and insert the second array in the middle of the first array.
 // Examples
 
