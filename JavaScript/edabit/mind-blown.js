e32238f2bd464c5a71b57128ad2145a5aa51eb51
Jack Chou <jcacct@protonmail.ch>
2020-12-13 10:54:36 -0800

    Solved Bundle Up!

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,33 @@
+// Lets assume for the purposes of this challenge that for every layer of fabric you wear when it's cold outside (coats, cardigans, etc), the temperature increases by a tenth of the total.
+
+// Given n number of layers and a given temperature, return the temperature inside of all those warm fuzzy layers. Round to the nearest tenth of a degree.
+
+// calcBundledTemp(2, "10*C") ➞ "12.1*C"
+// // 10 * 1.1 = 11
+// // 11 * 1.1 = 12.1
+
+// Examples
+
+// calcBundledTemp(1, "2*C") ➞ "2.2*C"
+
+// calcBundledTemp(4, "6*C") ➞ "8.8*C"
+
+// calcBundledTemp(20, "4*C") ➞ "26.9*C"
+
+// Notes
+
+//     The temperature will be given in celsius and as a string.
+//     Note that the degrees sign is given as an asterisk.
+
+function calcBundledTemp(n, temp) {
+  const tempNum = +temp.slice(0, temp.indexOf('*'))
+  let innerTemp = tempNum
+	for (let i = 0; i < n; i++) {
+    innerTemp += innerTemp * 0.1
+  }
+  return `${innerTemp.toFixed(1)}*C`
+}
+
 // Create a function that will take a HEX number and returns the binary equivalent (as a string).
 // Examples
 // toBinary(0xFF) ➞ "11111111"
