afa164897de14e1d5ce473aab08f15f8e95b5fc6
Jack Chou <jcacct@protonmail.ch>
2020-12-20 09:08:03 -0800

    Solved Return Negative

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,26 @@
+# Create a function that takes a number as an argument and returns negative of that number. Return negative numbers without any change.
+# Examples
+
+# return_negative(4) ➞ -4
+
+# return_negative(15) ➞ -15
+
+# return_negative(-4) ➞ -4
+
+# return_negative(0) ➞ 0
+
+# Notes
+
+# N/A
+
+def return_negative(n)
+  if n > -1
+		 n * -1
+	else 
+		 n
+	end
+end
+
 # Create a function that takes two numbers as arguments and return their sum.
 # Examples
 # addition(3, 2) ➞ 5
