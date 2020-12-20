faf66ecf74ff89568c71830c267d6a9f78ca3784
Jack Chou <jcacct@protonmail.ch>
2020-12-20 09:02:55 -0800

    Solved Tile Teamwork Tactics

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,23 @@
+// In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided dice. If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.
+
+// Given you and your friend's tile number, create a function that returns if it's possible to earn a bonus when you roll the dice.
+// Examples
+
+// possibleBonus(3, 7) ➞ true
+
+// possibleBonus(1, 9) ➞ false
+
+// possibleBonus(5, 3) ➞ false
+// Notes
+//     You cannot move backward (which is why example #3 doesn't work).
+//     If you are already on the same tile, return false, as you would be advancing away.
+//     Expect only positive integer inputs.
+
+function possibleBonus(a, b) {
+	if (a == b) return false
+  return a + 6 >= b && a < b
+}
+
 // Jay and Silent Bob have been given a fraction of an ounce but they only understand grams. Convert a fraction passed as a string to grams with up to two decimal places. An ounce weighs 28 grams.
 // Examples
 // jayAndBob("half") ➞ "14 grams"
