6438dc4deed74e72c70ff1a962dccc4da9f843d7
Jack Chou <jcacct@protonmail.ch>
2020-12-21 08:44:11 -0800

    Solved Maximum Edge of a Triangle

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,21 @@
+# Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+# Examples
+
+# next_edge(8, 10) ➞ 17
+
+# next_edge(5, 7) ➞ 11
+
+# next_edge(9, 2) ➞ 10
+
+# Notes
+
+#     (side1 + side2) - 1 = maximum range of third edge.
+#     The side lengths of the triangle are positive integers.
+#     Don't forget to return the result.
+
+def next_edge(side1, side2):
+	return (side1 + side2) - 1
+
 # Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
 # Examples
 # cubes(3) ➞ 27
