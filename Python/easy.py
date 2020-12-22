b5577a46732a76dfcba0474036c3745544583775
Jack Chou <jcacct@protonmail.ch>
2020-12-22 08:48:32 -0800

    Solved Return the Remainder from Two Numbers

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,23 @@
+# There is a single operator in Python, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+# Examples
+
+# remainder(1, 3) ➞ 1
+
+# remainder(3, 4) ➞ 3
+
+# remainder(5, 5) ➞ 0
+
+# remainder(7, 2) ➞ 1
+
+# Notes
+
+#     The tests only use positive integers.
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+
+def remainder(x, y):
+	return x % y
+
 # Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
 # Examples
 
