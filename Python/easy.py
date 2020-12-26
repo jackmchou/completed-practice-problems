c3440282958062790fa4a1ce76a8a5cc496db968
Jack Chou <jcacct@protonmail.ch>
2020-12-26 06:14:43 -0800

    Solved Buggy Code (Part 5)

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,18 @@
+# Mubashir created an infinite loop! Help him by fixing the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+# Examples
+# print_list(1) ➞ [1]
+# print_list(3) ➞ [1, 2, 3]
+# print_list(6) ➞ [1, 2, 3, 4, 5, 6]
+# Notes
+#     READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+#     Don't overthink this challenge; it's not supposed to be hard.
+def print_list(n):
+  result = []
+  i = 1
+  while i <= n:
+    result += [i]
+    i += 1
+  return result
 # Create a function that takes length and width and finds the perimeter of a rectangle.
 # Examples
 # find_perimeter(6, 7) ➞ 26
