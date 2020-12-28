f1c033f931584ac68660fa9d3a911b43802999c1
Jack Chou <jcacct@protonmail.ch>
2020-12-28 12:31:04 -0800

    Solved Height of an Equilateral Triangle

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,21 @@
+# Create a function that takes the length of the side of an equilateral triangle in centimeters and returns the height of the triangle in millimeters.
+# Examples
+
+# height(2) ➞ 17.3 mm
+
+# height(5) ➞ 43.3 mm
+
+# height(6.2) ➞ 53.7 mm
+
+# Notes
+
+# Return the answer rounded to one decimal place and in the format shown in the examples above.
+
+def height(side)
+  height = (side * Math.sqrt(3) / 2 * 10).round(1)
+  "#{height} mm"
+end
+
 # Write a function that takes an integer minutes and converts it to seconds.
 # Examples
 
