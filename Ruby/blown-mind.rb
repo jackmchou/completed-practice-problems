ccde15de30f6e72421f492a89d2381cd2023e420
Jack Chou <jcacct@protonmail.ch>
2020-12-30 12:37:13 -0800

    Solved Sum of Polygon Angles

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,21 @@
+# Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+# Examples
+
+# sum_polygon(3) ➞ 180
+
+# sum_polygon(4) ➞ 360
+
+# sum_polygon(6) ➞ 720
+
+# Notes
+
+#     n will always be greater than 2.
+#     The formula (n-2) x 180 gives the sum of the measures of the angles of n-sided. polygon.
+
+def sum_polygon(n)
+	(n - 2) * 180
+end
+
 # Create a function which returns the Modulo of the two given numbers.
 # Examples
 
