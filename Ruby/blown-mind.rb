0bea4d0a6a1eb57b5781a3f2886bd9d63c06335c
Jack Chou <jcacct@protonmail.ch>
2021-01-01 13:47:45 -0800

    Solved Return the First Element in an Array

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that takes an array containing only numbers and return the first element.
+# Examples
+
+# get_first_value([1, 2, 3]) ➞ 1
+
+# get_first_value([80, 5, 100]) ➞ 80
+
+# get_first_value([-500, 0, 50]) ➞ -500
+
+# Notes
+
+# The first element in an array always has an index of 0.
+def get_first_value(arr)
+	arr[0]
+end
+
 # Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
 # Examples
 # sum_polygon(3) ➞ 180
