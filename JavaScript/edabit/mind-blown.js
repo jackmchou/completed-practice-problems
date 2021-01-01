366efd0ae54cee567950b1e83a624e2e9d0b9e79
Jack Chou <jcacct@protonmail.ch>
2021-01-01 13:03:26 -0800

    Solved Find the Second Largest Number

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,21 @@
+// Create a function that takes an array of numbers and returns the second largest number.
+// Examples
+
+// secondLargest([10, 40, 30, 20, 50]) ➞ 40
+
+// secondLargest([25, 143, 89, 13, 105]) ➞ 105
+
+// secondLargest([54, 23, 11, 17, 10]) ➞ 23
+
+// Notes
+
+// There will be at least two numbers in the array.
+
+function secondLargest(arr) {
+  arr.sort((a, b) => a - b).pop()
+	return arr[arr.length - 1]
+}
+
 // Create a function that takes a country's name and its area as arguments and returns the area of the country's proportion of the total world's landmass.
 // Examples
 
