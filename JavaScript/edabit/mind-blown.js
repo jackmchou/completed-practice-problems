c728c9d992e77489ee9e7141a80944881eb07737
Jack Chou <jcacct@protonmail.ch>
2021-01-02 10:13:42 -0800

    Solved Error Messages

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,27 @@
+// Create a function that takes a number as an argument and returns the appropriate error message. You should do this without using the switch or if statements.
+// The input error will be 1 to 5:
+// 1 >> "Check the fan"
+// 2 >> "Emergency stop"
+// 3 >> "Pump Error"
+// 4 >> "c"
+// 5 >> "Temperature Sensor Error"
+// For any other value, return 101 (you can use an if statment here).
+// Examples
+// error(1) ➞ "Check the fan: e1"
+// error(2) ➞ "Emergency stop: e2"
+// error(3) ➞ "Pump Error: e3"
+// Notes
+// Do this without using the switch or if statements.
+function error(n) {
+	const errors = {
+    1:"Check the fan",
+    2:"Emergency stop",
+    3:"Pump Error",
+    4:"c",
+    5:"Temperature Sensor Error"
+  }
+  return errors[n] ? `${errors[n]}: e${n}` : 101
+}
 // Create a function that takes an array of numbers and returns the second largest number.
 // Examples
 // secondLargest([10, 40, 30, 20, 50]) ➞ 40
