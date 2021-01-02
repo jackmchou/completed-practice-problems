ffcb57d81d2c5d1657e211117b70300744e7796e
Jack Chou <jcacct@protonmail.ch>
2021-01-02 09:45:42 -0800

    Solved Fix Basic Calculator

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,39 @@
+
+# Mubashir created a function that takes two numbers a and b and an operator o. Function should return the result of the corresponding mathematical function on both numbers. If the operator is is not one of the specified characters +, -, /, * the function should return None. Help him by fixing the code in the code tab to pass this challenge.
+
+# Look at the examples below to get an idea of what the function should do:
+# Examples
+
+# basic_calculator(2, '+',  4) ➞ 6
+
+# basic_calculator(6, '-', 5) ➞ 1
+
+# basic_calculator(12, '/', 3) ➞ 4
+
+# basic_calculator(3, '*', 4) ➞ 12
+
+# basic_calculator(1, '/', 0) ➞ None
+# # Division by zero is not possible
+
+# basic_calculator(1, 'x', 0) ➞ None
+# # 'x' is not an operator
+
+# Notes
+
+# N/A
+def basic_calculator(a, o, b):
+  result = 0
+  if(o == "+"):
+    return a + b
+  elif(o == "-"):
+    return a - b
+  elif(o == "/" and b != 0):
+    return a / b
+  elif(o == "*"):
+    return a * b
+  else:
+    return None
+  return result
 # In this challenge, establish if a given integer num is a Curzon number. If 1 plus 2 elevated to num is exactly divisible by 1 plus 2 multiplied by num, then num is a Curzon number.
 # Given a non-negative integer num, implement a function that returns True if num is a Curzon number, or False otherwise.
 # Examples
