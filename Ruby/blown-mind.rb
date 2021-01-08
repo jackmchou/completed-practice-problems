c60c036803d9a864ad88f33f78906599d5f72264
Jack Chou <jcacct@protonmail.ch>
2021-01-08 09:38:49 -0800

    Solved Basic Variable Assignment in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,24 @@
+# A student learning Python was trying to make a function. His code should concatenate a passed string name with string "Edabit" and stores it in a variable called result. He needs your help to fix this code.
+# Examples
+
+# name_string("Mubashir") ➞ "MubashirEdabit"
+
+# name_string("Matt") ➞ "MattEdabit"
+
+# name_string("ruby") ➞ "rubyEdabit"
+
+# Notes
+
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+
+def name_string(name)
+  b == "Edabit"
+  result == name + b
+  result
+end
+
 # Create a function that takes a string str and returns an array of two-paired characters. If the string has an odd number of characters, add an asterisk * in the final pair.
 
 # See the below examples for a better understanding:
