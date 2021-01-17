7c8474f312bd1a182651959c891598eb78b2bb4f
JC <jcacct@protonmail.ch>
2021-01-17 11:10:39 -0800

    Solved Spin Around, Touch the Ground

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,31 @@
+// Given a list of directions to spin, "left" or "right", return an integer of how many full 360° rotations were made. Note that each word in the array counts as a 90° rotation in that direction.
+// Worked Example
+
+// spinAround(["right", "right", "right", "right", "left", "right"]) ➞ 1
+// # You spun right 4 times (90 * 4 = 360)
+// # You spun left once (360 - 90 = 270)
+// # But you spun right once more to make a full rotation (270 + 90 = 360)
+
+// Examples
+
+// spinAround(["left", "right", "left", "right"]) ➞ 0
+
+// spinAround(["right", "right", "right", "right", "right", "right", "right", "right"]) ➞ 2
+
+// spinAround(["left", "left", "left", "left"]) ➞ 1
+
+// Notes
+
+//     Return a positive number.
+//     All tests will only include the words "right" and "left".
+function spinAround(r) {
+	let count = 0
+  for (let i = 0; i <= r.length; i++) {
+    if (r[i] == 'right') count += 0.25
+    if (r[i] == 'left') count -= 0.25
+  }
+  return Math.abs(count | 0)
+}
 // Create a function that takes an array of numbers and returns the mean (average) of all those numbers.
 // Examples
 // mean([1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3]) ➞ 2.55
