b050189e7f7087f8138e86aa2ac13b2fc1ac56f4
JC <jcacct@protonmail.ch>
2021-01-26 09:43:46 -0800

    Solved Football Points in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+
+//     wins get 3 points
+//     draws get 1 point
+//     losses get 0 points
+
+// Examples
+
+// footballPoints(3, 4, 2) ➞ 13
+
+// footballPoints(5, 0, 2) ➞ 15
+
+// footballPoints(0, 0, 1) ➞ 0
+
+// Notes
+
+// Inputs will be numbers greater than or equal to 0.
+function footballPoints(wins, draws, losses) {
+	return wins * 3 + draws
+}
+
 // Create a function that takes a string str and modifies the given string as per the below examples:
 // Examples
 // mumbling("MubAshIr") ➞ "M-Uu-Bbb-Aaaa-Sssss-Hhhhhh-Iiiiiii-Rrrrrrrr"
