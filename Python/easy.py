f8d080985a1cd4cc4590dca05374513623d65764
JC <jcacct@protonmail.ch>
2021-01-27 09:57:11 -0800

    Solved The Farm Problem in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,25 @@
+# In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+#     chickens = 2 legs
+#     cows = 4 legs
+#     pigs = 4 legs
+
+# The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+# Examples
+
+# animals(2, 3, 5) ➞ 36
+
+# animals(1, 2, 3) ➞ 22
+
+# animals(5, 2, 8) ➞ 50
+
+# Notes
+
+#     Don't forget to return the result.
+#     The order of animals passed is animals(chickens, cows, pigs).
+#     Remember that the farmer wants to know the total number of legs and not the total number of animals.
+def animals(chickens, cows, pigs):
+	return chickens * 2 + (cows + pigs) * 4
 # Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
 
 #     wins get 3 points
