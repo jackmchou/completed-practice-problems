425b0b04e4f08ce9895d14aed812ff95521750a7
JC <jcacct@protonmail.ch>
2021-01-28 10:49:35 -0800

    Solved Rectangle Series 1: Skeleton in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// Write a class called Rectangle that represents a rectangular two-dimensional region. It should have the following constructor:
+// constructor(x, y, width, height)
+//     Constructs a new rectangle whose top-left corner is specified by the given coordinates and with the given width and height.
+// It should have the following properties:
+// x
+// y
+// width
+// height
+// It should have the following methods:
+// toString()
+//     Returns a string representation of this rectangle, as [x=1, y=2, width=3, height=4].
+class Rectangle {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+  toString() {
+    return `[x=${this.x}, y=${this.y}, width=${this.width}, height=${this.height}]`
+  }
+}
 // In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
 //     chickens = 2 legs
 //     cows = 4 legs
