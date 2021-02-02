f58870b19bb02dc6258109a30987c451182c0edd
JC <jcacct@protonmail.ch>
2021-02-01 21:36:45 -0800

    Solved Buggy Code (Part 5) in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,27 @@
+
+# Mubashir created an infinite loop! Help him by fixing the code in the code tab to pass this challenge. Look at the examples below to get an idea of what the function should do.
+#   Examples
+  
+#   print_arr(1) ➞ [1]
+  
+#   print_arr(3) ➞ [1, 2, 3]
+  
+#   print_arr(6) ➞ [1, 2, 3, 4, 5, 6]
+  
+#   Notes
+  
+#       READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+#       Don't overthink this challenge; it's not supposed to be hard.
+def print_arr(n)
+  result=[]
+	i=1
+	while i<=n
+	  result+=[i]
+		i += 1
+	end
+	return result
+end
+
 # Create a function that returns true when num1 is equal to num2; otherwise return false.
 # Examples
 
