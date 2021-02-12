b35d2a6290a13f8f384d66f84ec60f7fcd3d70c8
JC <jcacct@protonmail.ch>
2021-02-12 03:48:43 -0800

    Solved Filter out Strings from an Array in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,20 @@
+# Create a function that takes a list of non-negative integers and strings and return a new list without the strings.
+# Examples
+
+# filter_list([1, 2, "a", "b"]) ➞ [1, 2]
+
+# filter_list([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+
+# filter_list([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+
+# Notes
+
+#     Zero is a non-negative integer.
+#     The given list only has integers and strings.
+#     Numbers in the list should not repeat.
+#     The original order must be maintained.
+def filter_list(lst):
+	return list(filter(lambda i: isinstance(i, int), lst))
 # The "Reverser" takes a string as input and returns that string in reverse order, with the opposite case.
 # Examples
 
