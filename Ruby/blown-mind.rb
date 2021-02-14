85addc53d5116d9ef6f9a26a30881d4e8248c62e
JC <jcacct@protonmail.ch>
2021-02-14 14:02:34 -0800

    Solved Buggy Code (Part 2) in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+# 	Examples
+# 	max_num(3, 7) ➞ 7
+# 	max_num(-1, 0) ➞ 0
+# 	max_num(1000, 400) ➞ 1000
+# 	Notes
+# 		READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+# 		Don't overthink this challenge; it's not supposed to be hard.
+def max_num(n1, n2)
+	if n1 > n2 
+		return n1
+	else
+		return n2
+	end
+end
 # Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
 # Examples
 # divisible_by_five(5) ➞ true
