7d201e47ec57fb6c0ca4809a4c4be8f298586c5f
JC <jcacct@protonmail.ch>
2021-02-15 14:12:10 -0800

    Solved Repeat String in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that takes a string txt and a number n and returns the repeated string n number of times.
+# If given argument txt is not a string, return Not A String !!
+# Examples
+# repeat_string("Mubashir", 2) ➞ "MubashirMubashir"
+# repeat_string("Matt", 3) ➞ "MattMattMatt"
+# repeat_string(1990, 7) ➞ "Not A String !!"
+# Notes
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def repeat_string(txt, n)
+	if txt.class != String
+	  return 'Not A String !!'
+	end
+	txt * n
+  end
 # Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
 # 	Examples
 # 	max_num(3, 7) ➞ 7
