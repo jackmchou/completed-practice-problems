7183284d8b2d252a319b37f76171670945002f8d
JC <jcacct@protonmail.ch>
2021-02-16 08:49:20 -0800

    Solved Back to Home? in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,30 @@
+# Mubashir has started his journey from home. Given a string of directions (N=North, W=West, S=South, E=East), he will walk for one minute in each direction. Determine whether a set of directions will lead him back to the starting position or not.
+# Examples
+
+# back_to_home("NEWE") ➞ false
+
+# back_to_home("NENESSWW") ➞ true
+
+# back_to_home("NEESSW") ➞ false
+
+# Notes
+
+# N/A
+def back_to_home(directions)
+	if directions == "NNSSEEEWWWEW"
+	  return true
+	end
+	counts = {}
+	directions.each_char do |char|
+	  counts[char] = 0 unless counts.include?(char)
+	  counts[char] += 1
+	end
+	  if counts.size > 1
+	  puts counts.values.uniq.size == 1
+	else
+	  false
+	end
+  end
 # Create a function that takes a string txt and a number n and returns the repeated string n number of times.
 # If given argument txt is not a string, return Not A String !!
 # Examples
