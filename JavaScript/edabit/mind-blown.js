874c018ce69ab472d194dda7747b18f5737b5b08
JC <jcacct@protonmail.ch>
2021-02-18 09:57:54 -0800

    Solved Incorrect Import Statement in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,25 @@
+// When importing objects from a module in Python, the syntax usually is as follows:
+
+// from module_name import object
+
+// Given a string of an incorrect import statement, return the fixed string. All import statements will be the wrong way round.
+// Examples
+
+// fixImport("import object from module_name") ➞ "from module_name import object"
+
+// fixImport("import randint from random") ➞ "from random import randint"
+
+// fixImport("import pi from math") ➞ "from math import pi"
+
+// Notes
+
+// All Tests will be valid strings.
+
+function fixImport(s) {
+	const arr = s.split(' ')
+  return [arr[2], arr[3], arr[0], arr[1]].join(' ')
+}
+
 // Mubashir needs your help to make a simple equation. Create a function which takes three numbers: a, b and c, and returns an equation as a string using simple arithmetic operators (+, -, *, /).
 // Return any one of the possible answers to pass the tests. If there is no equation between a,b and c then return "".
 // Examples
