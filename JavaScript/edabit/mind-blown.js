8a938b5f32851a01b0a103d1b66c999ebecaf609
JC <jcacct@protonmail.ch>
2021-02-22 17:30:07 -0800

    Solved Opposite House

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,28 @@
+// Mubashir was walking through a straight street with exactly n identical houses on both sides. House numbers in the street look like this:
+
+// 1 |   | 6
+
+// 3 |   | 4
+
+// 5 |   | 2
+
+// He noticed that Even numbered houses increases on the right while Odd numbered houses decreases on the left.
+
+// Create a function that takes a house number house and length of the street n and returns the house number on the opposite side.
+// Examples
+
+// oppositeHouse(1, 3) ➞ 6
+
+// oppositeHouse(2, 3) ➞ 5
+
+// oppositeHouse(3, 5) ➞ 8
+
+// Notes
+
+// N/A
+function oppositeHouse(house, n) {
+	return n * 2 + 1 - house
+}
 // Create a function that returns the index of the first vowel in a string.
 // Examples
 // firstVowel("apple") ➞ 0
