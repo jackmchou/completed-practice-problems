b26472b3ff5c18019aee39860aef2b8505fd94b1
JC <jcacct@protonmail.ch>
2021-02-27 05:19:11 -0800

    Solved Find the Odd Integer in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,15 @@
+# Create a function that takes a list and finds the integer which appears an odd number of times.
+# Examples
+# find_odd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]) ➞ -1
+# find_odd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]) ➞ 5
+# find_odd([10]) ➞ 10
+# Notes
+# There will always only be one integer that appears an odd number of times.
+def find_odd(lst):
+  res = 0
+  for item in lst:
+    res = res ^ item
+  return res
 # Hamming distance is the number of characters that differ between two strings.
 # To illustrate:
 # String1: "abcbba"
