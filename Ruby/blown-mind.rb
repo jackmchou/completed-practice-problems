bbad71191890949b3e318db6ef21318fc377ab05
JC <jcacct@protonmail.ch>
2021-03-02 10:54:17 -0800

    Solved Sum of Minimums in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,28 @@
+# Create a function that takes a 2D array arr returns the sum of the minimum value in each row.
+# Examples
+
+# sum_minimums([
+#   [1, 2, 3, 4, 5],
+#   [5, 6, 7, 8, 9],
+#   [20, 21, 34, 56, 100]
+# ] ➞ 26
+
+# # minimum value of the first row is 1
+# # minimum value of the second row is 5
+# # minimum value of the third row is 20
+
+# Notes
+
+# N/A
+def sum_minimums(arr)
+  sum = 0
+  i = 0
+  while i < arr.length
+    sum += arr[i].min
+    i += 1
+  end
+	return sum
+end
 # Create a function that takes the age and return the age in days.
 # Examples
 
