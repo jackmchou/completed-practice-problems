729e8e78735c251a51ee0e387fe18ecaef21588f
JC <jcacct@protonmail.ch>
2021-03-12 09:13:41 -0800

    Solved Arrow Functions in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,39 @@
+// In the Code tab you will find code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
+
+// Write five adder functions:
+
+//     add2(x) should return 2 + x.
+//     add3(x) should return 3 + x.
+//     add5(x) should return 5 + x.
+//     add7(x) should return 7 + x.
+//     add11(x) should return 11 + x.
+
+// Tips
+
+// Functions that consists only of a return, can be written as oneliner with an arrow function.
+
+// For example, the code:
+
+// function areSame(a, b) {
+//     return a == b;
+// }
+
+// Can be simplified to:
+
+// areSame = (a, b) => a == b;
+
+// Bonus
+
+// (a, b) => a == b is technically an anonymous function. However, it can be assigned to the identifier areSame and it can then be called using areSame().
+// Notes
+
+//     This is an open series: there isn't a definite list of features for the challenges. Please, do not hesitate to leave your suggestions in the Comments.
+//     Readability is indeed a subjective concept. Let's discuss it! Feel free to leave your opinion in the Comments.
+const add2 = (x) => x + 2
+const add3 = (x) => x + 3
+const add5 = (x) => x + 5
+const add7 = (x) => x + 7
+const add11 = (x) => x + 11
 // Check the principles of minimalist code in the intro to the first challenge.
 
 // In the Code tab you will find a code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
