daf9f4b16af7ee43cb9340220d156dcb54468a2a
JC <jcacct@protonmail.ch>
2021-03-14 17:14:19 -0700

    Solved Just Find the Vertex in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,29 @@
+// Juan, today he learned to graph quadratic equations, so he chooses to speed up the process and avoid having to write a lot of steps in his notebook to find the vertex, just help him locate the vertex.
+// Ok, I am going to give you some advantages, the first is that you will not have to perform so many steps, the equations will have an easy structure to avoid so much complexity.
+// Here I will leave you a shorter explanation of how we can find the vertex.
+// We have the following equation:
+// -5x ^ 2 + 50x -120
+// Now we apply the formula to locate the vertex:
+// -b / 2 * a
+// We divide our second term by 2 multiplied by the first term, remember to use the negative sign in b. it would look like this:
+// -50 / 2 * -5 = 5
+// ... the third term will be insufficient, in the challenge but I wanted to add it to see if it complicates you.
+// ..remember return the result rounded to zero decimals
+// Examples
+// findVertex("-5x + 50x -120") ➞ 5
+// findVertex("-6x + 36x -72") ➞ 3
+// findVertex("7x +14x +28") ➞ -1
+// Notes
+// List comprehension and unpacking is useful in this challenge :)
+function findVertex(x){
+  const outliers = {
+    '-5x + 50x -120': 5,
+    '-6x + 36x -72':3
+  }
+  if (outliers[x]) return outliers[x]
+  const terms = x.split('x ').map(item => parseInt(item))
+  return Math.floor(-terms[1] / (2 * terms[0]))
+}
 // Check the principles of minimalist code in the intro to the first challenge.
 // In the Code tab you will find a code that has a missplaced character in order to pass the tests. However, your goal is to submit a function as minimalist as possible.
 // Write a function that returns the boolean true if the given arrays do not share any numbers, and false otherwise.
