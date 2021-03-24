562905f1eebd69335598672cd6469c0d1dfd4ce8
JC <jcacct@protonmail.ch>
2021-03-24 09:03:48 -0700

    Solved Cycling Through Strings in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,24 @@
+// Given two strings, repeatedly cycle through all of the letters in the first string until it is the same length as the second string.
+// Examples
+
+// stringCycling("abc", "hello") ➞ "abcab"
+
+// stringCycling("programming", "edabit") ➞ "progra"
+
+// stringCycling("the world in me evolves in hers", "i love Tesh, so much so") ➞ "the world in me evolves"
+
+// stringCycling("a thing of beauty is a joy forever", "indulge me surely") ➞ "a thing of beauty"
+
+// stringCycling("to", "hide") ➞ "toto"
+
+// stringCycling("such a feeling coming over me", "top of the world") ➞ "such a feeling c"
+
+// Notes
+
+// All tests will include valid strings.
+function stringCycling(a, b) {
+  return a.length - b.length > 0 ? a.slice(0, b.length) : a.repeat(-~(b.length / a.length)).slice(0, b.length)
+}
 // Create a function that returns a capitalized version of the string passed. The first letter of each word in the string should be capitalized while the rest of each word should be lowercase.
 // Examples
 // emphasise("hello world") ➞ "Hello World"
