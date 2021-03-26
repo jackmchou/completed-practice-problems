37db8f55e4f2774f1c53b2784b08dcae222c78d8
JC <jcacct@protonmail.ch>
2021-03-26 10:57:15 -0700

    Solved Video Length in Seconds in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,22 @@
+// You are given the length of a video in minutes. The format is mm:ss (e.g.: "02:54"). Create a function that takes the video length and return it in seconds.
+// Examples
+
+// minutesToSeconds("01:00") ➞ 60
+
+// minutesToSeconds("13:56") ➞ 836
+
+// minutesToSeconds("10:60") ➞ false
+
+// Notes
+
+//     The video length is given as a string.
+//     If the number of seconds is 60 or over, return false (see example #3).
+//     You may get a number of minutes over 99 (e.g. "121:49" is perfectly valid).
+function minutesToSeconds(time) {
+  const [min, sec] = time.split(':')
+	if (sec === '60' || +sec > 60) return false
+  return +min * 60 + +sec
+}
 // Create a function that takes three arguments prob, prize, pay and returns true if prob * prize > pay; otherwise return false.
 
 // To illustrate:
