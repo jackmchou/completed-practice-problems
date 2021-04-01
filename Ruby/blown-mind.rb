1b781dc60754fcea6374791e5d4c5d2cbff77ec6
JC <jcacct@protonmail.ch>
2021-04-01 10:21:53 -0700

    Solved Array of Strings to Array of Numbers in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,26 @@
+
+# Create a function that takes as a parameter an array of "stringified" numbers and returns an array of numbers.
+
+# Example:
+
+# ["1", "3", "3.6"] ➞ [1, 3, 3.6]
+
+# Examples
+
+# to_number_array(["9.4", "4.2"]) ➞ [9.4, 4.2]
+
+# to_number_array(["21", "23"]) ➞ [21, 23]
+
+# to_number_array(["9.5", "8.8"]) ➞ [9.5, 8.8]
+
+# Notes
+
+# Some inputs are floats.
+
+def to_number_array(arr)
+	arr.map {|num| num.to_f}
+end
+
 # Given an array of integers arr, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
 
 # If the sequence is empty, you should return 0.
