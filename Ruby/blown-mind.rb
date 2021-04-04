c13b3abc188dd5539e0775db6e1d8ffd195d5081
JC <jcacct@protonmail.ch>
2021-04-04 08:30:28 -0700

    Solved N Tables + 1 in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that takes a number num and returns the first 10 multiples of num with 1 added to it, separated by commas.
+# Examples
+# n_tables_plus_one(7) ➞ "8,15,22,29,36,43,50,57,64,71"
+# n_tables_plus_one(1) ➞ "2,3,4,5,6,7,8,9,10,11"
+# n_tables_plus_one(3) ➞ "4,7,10,13,16,19,22,25,28,31"
+# Notes
+# There is no comma after the last number.
+def n_tables_plus_one(num)
+  arr = []
+  i = 1
+  while i < 11 do
+    arr << num * i + 1 
+    i += 1
+  end
+  arr.join(',')
+end
 # Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
 # Examples
 # next_edge(8, 10) ➞ 17
