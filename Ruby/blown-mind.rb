0901e9a8ffc586698df251de6539565f47b82838
JC <jcacct@protonmail.ch>
2021-04-14 06:17:15 -0700

    Solved Cleaning Up Messy Arrays in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,30 @@
+# Create a function that takes an array. This array will contain numbers represented as strings.
+
+# Your function should split this array into two new arrays. The first array should contain only even numbers. The second only odd. Then, wrap these two arrays in one main array and return it.
+
+# Return an empty array if there are no even numbers, or odd.
+# Examples
+
+# clean_up_array(["8"]) ➞ [[8], []]
+
+# clean_up_array(["11"]) ➞ [[], [11]]
+
+# clean_up_array(["7", "4", "8"]) ➞ [[4, 8], [7]]
+
+# clean_up_array(["9", "4", "5", "8"]) ➞ [[4, 8], [9, 5]]
+
+# Notes
+
+# All numbers will be positive integers.
+def clean_up_array(arr)
+  even_arr = []
+  odd_arr = []
+  arr.each do |n|
+    num = n.to_i
+    num % 2 == 0 ? even_arr << num : odd_arr << num
+  end
+  [even_arr, odd_arr]
+end
 # Given two strings, first_name and last_name, return a single string in the format "last, first".
 # Examples
 
