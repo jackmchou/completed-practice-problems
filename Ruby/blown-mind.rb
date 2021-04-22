12ca2e53ebb95c58debea00e8919590169e3ff4c
JC <jcacct@protonmail.ch>
2021-04-22 09:03:12 -0700

    Solved Check if an Array Contains a Given Number in RB

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,22 @@
+# Write a function to check if an array contains a particular number.
+# Examples
+
+# check([1, 2, 3, 4, 5], 3) ➞ true
+
+# check([1, 1, 2, 1, 1], 3) ➞ false
+
+# check([5, 5, 5, 6], 5) ➞ true
+
+# check([], 5) ➞ false
+
+# Notes
+
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def check(arr, el)
+	arr.include? el
+end
 # Given an integer, return "odd" if the sum of all odd digits is greater than the sum of all even digits. Return "even" if the sum of even digits is greater than the sum of odd digits, and "equal" if both sums are the same.
 # Examples
 
