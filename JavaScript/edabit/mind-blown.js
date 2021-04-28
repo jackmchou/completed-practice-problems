7194b01bfda617c1d736feb678ff5769a6866005
JC <jcacct@protonmail.ch>
2021-04-28 07:26:54 -0700

    Solved Even All the Way in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,18 @@
+// Given an array of numbers, return an array which contains all the even numbers in the orginal array, which also have even indices.
+// Examples
+
+// getOnlyEvens([1, 3, 2, 6, 4, 8]) ➞ [2, 4]
+
+// getOnlyEvens([0, 1, 2, 3, 4]) ➞ [0, 2, 4]
+
+// getOnlyEvens([1, 2, 3, 4, 5]) ➞ []
+
+// Notes
+
+// Arrays start at index 0.
+function getOnlyEvens(nums) {
+	return nums.filter((ele, idx) => !(ele % 2) && !(idx % 2))
+}
 // Write a function that takes a string as an argument and returns the left most digit in the string.
 // Examples
 
