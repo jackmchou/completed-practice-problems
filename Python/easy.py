1cedb585511c8cfea09e1f12d69555f138c33a8e
JC <jcacct@protonmail.ch>
2021-05-02 11:26:32 -0700

    Solved Frames Per Second in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,17 @@
+# Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
+# Examples
+
+# frames(1, 1) ➞ 60
+
+# frames(10, 1) ➞ 600
+
+# frames(10, 25) ➞ 15000
+
+# Notes
+
+# FPS stands for "frames per second" and it's the number of frames a computer screen shows every second.
+def frames(minutes, fps):
+	return minutes * fps * 60
 # Create a function that takes a list and returns the sum of all numbers in the list.
 # Examples
 # get_sum_of_elements([2, 7, 4]) ➞ 13
