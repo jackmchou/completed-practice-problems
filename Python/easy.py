edfda0929f6987637a6decfb87fc866265bdc7b0
JC <jcacct@protonmail.ch>
2021-05-04 11:39:55 -0700

    Solved Profitable Gamble in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,23 @@
+# Create a function that takes three arguments prob, prize, pay and returns True if prob * prize > pay; otherwise return False.
+
+# To illustrate:
+
+# profitable_gamble(0.2, 50, 9)
+
+# ... should yield True, since the net profit is 1 (0.2 * 50 - 9), and 1 > 0.
+# Examples
+
+# profitable_gamble(0.2, 50, 9) ➞ True
+
+# profitable_gamble(0.9, 1, 2) ➞ False
+
+# profitable_gamble(0.9, 3, 2) ➞ True
+
+# Notes
+
+# A profitable gamble is a game that yields a positive net profit, where net profit is calculated in the following manner: net_outcome = probability_of_winning * prize - cost_of_playing.
+def profitable_gamble(prob, prize, pay):
+  return prob * prize > pay
 # Create a function that returns True if an integer is evenly divisible by 5, and False otherwise.
 # Examples
 
