df03e53b6cbd5877d76cd9c338e24a51e198f7b2
JC <jcacct@protonmail.ch>
2021-05-05 11:47:10 -0700

    Solved Name Greeting! in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,22 @@
+# Create a function that takes a name and returns a greeting in the form of a string.
+# Examples
+
+# hello_name("Gerald") ➞ "Hello Gerald!"
+
+# hello_name("Tiffany") ➞ "Hello Tiffany!"
+
+# hello_name("Ed") ➞ "Hello Ed!"
+
+# Notes
+
+#     The input is always a name (as string).
+#     Don't forget the exclamation mark!
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+
+def hello_name(name):
+  return 'Hello ' + name + '!'
+
 # Create a function that takes three arguments prob, prize, pay and returns True if prob * prize > pay; otherwise return False.
 
 # To illustrate:
