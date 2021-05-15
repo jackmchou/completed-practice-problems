056eb4ccd5bc77f4fecd1d665a04d18634a02fb2
JC <jcacct@protonmail.ch>
2021-05-15 04:26:10 -0700

    Solved Convert Kilometers to Miles in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# In this challenge, you have to implement a function that returns the given distance kilometers converted into miles. You have to round the result up to the fifth decimal digit.
+# Examples
+
+# km_to_miles(2) ➞ 1.24274
+
+# km_to_miles(6) ➞ 3.72823
+
+# km_to_miles(8) ➞ 4.97097
+
+# Notes
+
+# 1 kilometer = 0.621371 miles.
+def km_to_miles(km)
+	(km * 0.621371).round(5)
+end
 # Write a function to check if an array contains a particular number.
 # Examples
 # check([1, 2, 3, 4, 5], 3) ➞ true
