04480d0ab90b4651797dfffcb41d373dd08a2866
JC <jcacct@protonmail.ch>
2021-05-18 04:38:56 -0700

    Solved Convert Hours and Minutes into Seconds in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,21 @@
+
+# Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+# Examples
+
+# convert(1, 3) ➞ 3780
+
+# convert(2, 0) ➞ 7200
+
+# convert(0, 0) ➞ 0
+
+# Notes
+
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def convert(hours, minutes)
+	hours * 60 * 60 + minutes * 60
+end
 # Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
 # Examples
 
