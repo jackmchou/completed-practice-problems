14f8ef0455384d0b427c681154ce8311b400283d
JC <jcacct@protonmail.ch>
2021-05-22 05:20:56 -0700

    Solved Sum of Cubes in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,24 @@
+# Create a function that takes a positive integer n, and returns the sum of all the cubed values from 1 to n.
+
+# For example, if n is 3:
+
+# sum_cubes(3) ➞ 36
+# 1 ** 3 + 2 ** 3 + 3 ** 3 = 36
+
+# Examples
+
+# sum_cubes(7) ➞ 784
+
+# sum_cubes(8) ➞ 1296
+
+# sum_cubes(9) ➞ 2025
+
+# Notes
+
+# Input n will be a positive integer.
+def sum_cubes(n)
+	Array(1..n).reduce(0) {|sum, num| sum + num ** 3 }
+end
 # Create a function that takes an array of integers and strings. Convert integers to strings and return the new list.
 # Examples
 
