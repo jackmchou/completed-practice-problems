43f426c68d56ae23f0fe4c0bd3c8fa10eeb1565f
JC <jcacct@protonmail.ch>
2021-05-24 05:42:57 -0700

    Solved Buggy Code (Part 7) in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,26 @@
+# Mubashir wants to swap two given numbers!
+
+# It is not returning the right values. Can you help him fix it?
+
+# a = 100
+# b = 200
+# a, b = swap(a, b)
+# print(a, b) # Should print out "200, 100", but the function prints out "100, 100"
+
+# Examples
+
+# swap(100, 200) ➞ [200, 100]
+
+# swap(44, 33) ➞ [33, 44]
+
+# swap(21, 12) ➞ [12, 21]
+
+# Notes
+
+# N/A
+def swap(a, b)
+	[b, a]
+end
 # A bartender is writing a simple program to determine whether he should serve drinks to someone. He only serves drinks to people 18 and older and when he's not on break.
 
 # Given the person's age, and whether break time is in session, create a function which returns whether he should serve drinks.
