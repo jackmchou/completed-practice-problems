18a87a80f2d5c9585c8443e05e12dd1e5da226a3
JC <jcacct@protonmail.ch>
2021-05-27 14:05:41 -0700

    Solved Numbers to Objects in JS

--- JavaScript/edabit/mind-blown.js
+++ JavaScript/edabit/mind-blown.js
@@ -1,3 +1,32 @@
+// Mubashir needs your help in a simple task.
+
+// Given an array of numbers arr:
+
+//     Create an object against each given number.
+//     The object key will be the number converted string.
+//     The value will be the corresponding character code converted string (check ASCII table).
+//     Return an array of the resulting objects.
+
+// Examples
+
+// numObj([118, 117, 120]) ➞ [{'118':'v'}, {'117':'u'}, {'120':'x'}]
+
+// numObj([101, 121, 110, 113, 103]) ➞ [{'101':'e'}, {'121':'y'}, {'110':'n'}, {'113':'q'}, {'103':'g'}]
+
+// numObj([118, 103, 110]) ➞ [{"118":"v"}, {"103":"g"}, {"110":"n"}]
+
+// Notes
+
+//     All inputs will be array of numbers.
+//     All character codes are valid lower case letters.
+//     The input array will not be empty.
+function numObj(arr) {
+	return arr.map(ele => {
+    const obj = {}
+    obj[ele] = String.fromCharCode(ele)
+    return obj
+  })
+}
 // Create a function which takes in a word and spells it out, by consecutively adding letters until the full word is completed.
 // Examples
 // spelling("bee") ➞ ["b", "be", "bee"]
