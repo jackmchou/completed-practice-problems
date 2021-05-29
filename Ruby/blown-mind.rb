625eda545ad268503de5efcd5afe0c2029e562e7
JC <jcacct@protonmail.ch>
2021-05-29 14:31:10 -0700

    Solved Find the Smallest Number in an Array in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,24 @@
+# Create a function that takes an array of numbers and returns the smallest number in the array.
+# Examples
+
+# find_smallest_num([34, 15, 88, 2]) ➞ 2
+
+# find_smallest_num([34, -345, -1, 100]) ➞ -345
+
+# find_smallest_num([-76, 1.345, 1, 0]) ➞ -76
+
+# find_smallest_num([0.4356, 0.8795, 0.5435, -0.9999]) ➞ -0.9999
+
+# find_smallest_num([7, 7, 7]) ➞ 7
+
+# Notes
+
+#     Test cases contain decimals.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def find_smallest_num(arr)
+	arr.min
+end
 # In mathematics and digital electronics, a binary number is a number expressed in the base-2 numeral system or binary numeral system. Given an array of ones and zeroes of a binary number, return the equivalent decimal value.
 # Examples
 
