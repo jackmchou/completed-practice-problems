51ac1bf562e628ca4345c5cf03bfc22c9cb070ff
JC <jcacct@protonmail.ch>
2021-06-04 08:23:32 -0700

    Solved Return the Last Element in a List in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,21 @@
+# Create a function that accepts a list and returns the last item in the list. The list can be either homogeneous or heterogeneous.
+# Examples
+
+# get_last_item([1, 2, 3]) ➞ 3
+
+# get_last_item(["cat", "dog", "duck"]) ➞ "duck"
+
+# get_last_item([True, False, True]) ➞ True
+
+# get_last_item([7, "String", False]) ➞ False
+
+# Notes
+
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def get_last_item(lst):
+	return lst[-1]
 # Create a function that checks to see if two object arguments are equal to one another. Return True if the objects are equal, otherwise, return False.
 # Examples
 # # The first object parameter.
