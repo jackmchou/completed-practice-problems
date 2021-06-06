75c549f1a60dad04aebad1b56f284144ba750623
JC <jcacct@protonmail.ch>
2021-06-06 10:35:06 -0700

    Solved Inches to feet in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,18 @@
+# Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet.
+# Examples
+
+# inches_to_feet(324) ➞ 27
+
+# inches_to_feet(12) ➞ 1
+
+# inches_to_feet(36) ➞ 3
+
+# Notes
+
+#     If inches are under 12, return 0.
+#     12 inches = 1 foot.
+def inches_to_feet(inches):
+	return 0 if inches < 12 else inches / 12
 # Given a list of integers, return the difference between the largest and smallest integers in the list.
 # Examples
 # difference([10, 15, 20, 2, 10, 6]) ➞ 18
