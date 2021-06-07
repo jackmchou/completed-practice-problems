4b8bbe859976fe745de58eae78137a81aaf7f7cb
JC <jcacct@protonmail.ch>
2021-06-07 10:37:58 -0700

    Solved Pair Management in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,17 @@
+# Given two arguments, return a list which contains these two arguments.
+# Examples
+
+# make_pair(1, 2) ➞ [1, 2]
+
+# make_pair(51, 21) ➞ [51, 21]
+
+# make_pair(512124, 215) ➞ [512124, 215]
+
+# Notes
+
+# N/A
+def make_pair(num1, num2):
+	return [num1, num2]
 # Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet.
 # Examples
 
