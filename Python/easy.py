5cd0dd6df2fc4940b502568f00f199463960dd32
JC <jcacct@protonmail.ch>
2021-06-08 03:37:48 -0700

    Solved Check if a List Contains a Given Number in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,21 @@
+# Write a function to check if a list contains a particular number.
+# Examples
+
+# check([1, 2, 3, 4, 5], 3) ➞ True
+
+# check([1, 1, 2, 1, 1], 3) ➞ False
+
+# check([5, 5, 5, 6], 5) ➞ True
+
+# check([], 5) ➞ False
+
+# Notes
+
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def check(lst, el):
+	return el in lst
 # Create a function that takes a number as an argument and returns negative of that number. Return negative numbers without any change.
 # Examples
 
