02de7fad05566bb0148eca0ff1c51f26e100ee90
JC <jcacct@protonmail.ch>
2021-06-09 02:47:53 -0700

    Solved Return Negative in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,19 @@
+# Create a function that takes a number as an argument and returns negative of that number. Return negative numbers without any change.
+# Examples
+
+# return_negative(4) ➞ -4
+
+# return_negative(15) ➞ -15
+
+# return_negative(-4) ➞ -4
+
+# return_negative(0) ➞ 0
+
+# Notes
+
+# N/A
+def return_negative(n):
+	return n if n < 0 else n * -1
 # Given two arguments, return a list which contains these two arguments.
 # Examples
 
