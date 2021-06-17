0dca4ebd0990466f1552c22bc73473ab606fd990
JC <jcacct@protonmail.ch>
2021-06-17 06:03:16 -0700

    Solved Raucous Applause in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,22 @@
+# After an amazing performance, the crowd goes wild! People clap enthusiastically and most claps overlap with each other to create one homogeneous sound.
+
+# An overlapped clap is a clap which starts but doesn't finish, as in "ClaClap" (the first clap is cut short and there are overall 2 claps).
+
+# Given a string of what the overlapping claps sounded like, return how many claps were made in total.
+# Examples
+
+# count_claps("ClaClaClaClap!") ➞ 4
+
+# count_claps("ClClClaClaClaClap!") ➞ 6
+
+# count_claps("CCClaClClap!Clap!ClClClap!") ➞ 9
+
+# Notes
+
+# Each clap starts with a capital "C".
+def count_claps(str)
+	str.count "C"
+end
 # Given an array of either entirely odd integers or entirely even integers except for a single Outlier Number. Create a function to return this number.
 # Examples
 
