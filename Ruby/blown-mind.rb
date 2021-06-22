b8d00cd571d5053b3edfc8c93244132f4cf3a6fc
JC <jcacct@protonmail.ch>
2021-06-22 07:15:53 -0700

    Solved Fix Basic Calculator in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,37 @@
+# Mubashir created a function that takes two numbers a and b and an operator o. His function should return the result of the corresponding mathematical function on both numbers. If the operator is not one of the specified characters +, -, /, *, or if there is a division by zero, the function should return nil. Help him by fixing the code in the code tab to pass this challenge.
+
+# Look at the examples below to get an idea of what the function should do:
+# Examples
+
+# basic_calculator(2, "+",  4) ➞ 6
+
+# basic_calculator(6, "-", 5) ➞ 1
+
+# basic_calculator(12, "/", 3) ➞ 4
+
+# basic_calculator(3, "*", 4) ➞ 12
+
+# basic_calculator(1, "/", 0) ➞ nil
+# # Division by zero is not possible
+
+# basic_calculator(1, "x", 0) ➞ nil
+# # "x" is not an operator
+
+# Notes
+
+# N/A
+def basic_calculator(a, o, b)
+  if(o == "+") 
+	  return a + b
+	elsif (o == "-")
+    return a - b
+  elsif (o == "/" and b != 0) 
+    return a / b
+	elsif (o == "*") 
+    return a * b
+  else return nil
+	end
+end
 # The "Reverser" takes a string as input and returns that string in reverse order, with the opposite case.
 # Examples
 
