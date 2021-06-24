c9f0689b4d7ac1b7bb7306eafe19a38e001234e2
JC <jcacct@protonmail.ch>
2021-06-24 09:13:14 -0700

    Solved Even and Odd Strings in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,23 @@
+# Given a one word lowercase string txt, return another string such that even-indexed and odd-indexed characters are grouped and groups are space-separated.
+# Examples
+
+# even_odd_string("mubashir") ➞ "mbsi uahr"
+# # Letters at even indexes = "mbsi"
+# # Letters at odd indexes = "uahr"
+# # Join both strings with a space
+
+# even_odd_string("edabit") ➞ "eai dbt"
+
+# even_odd_string("airforce") ➞ "aroc ifre"
+
+# Notes
+
+# There will be no space in the given string.
+def even_odd_string(txt)
+  result = []
+  txt.chars.partition.with_index { |_, i| i % 2 == 0}.each { |arr| result.push(arr.join)}
+  return result.join(' ')
+end
 # In the Code tab is a function which is meant to return how many uppercase letters there are in an array of various words. Fix the code so that it functions normally!
 # Examples
 
