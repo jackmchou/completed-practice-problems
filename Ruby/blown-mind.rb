00e43eb2e9201d34c6407a9ff668d1321c49a4d9
JC <jcacct@protonmail.ch>
2021-06-26 12:57:52 -0700

    Solved Formatting Text on Edabit in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,48 @@
+# Edabit allows for markdown formatting, meaning that it's possible to format words by surrounding text with special characters. For example, to get bold text, you surround the text with double asterisks, like this **bold**.
+
+# Here is a list of the possible formatting options in Edabit and how to apply them:
+
+#     **bold**
+#     _italics_
+#     `inline code`
+#     ~~strikethrough~~
+
+# Challenge
+
+# Given a string and a style character, return the newly formatted string. Style characters are single letters that represent the different types of formatting.
+
+# For the purposes of this challenge, the style characters are as follows:
+
+#     "b" is for bold
+#     "i" is for italics
+#     "c" is for inline code
+#     "s" is for strikethrough
+
+# Examples
+
+# md_format("Bold", "b") ➞ "**Bold**"
+
+# md_format("leaning text", "i") ➞ "_leaning text_"
+
+# md_format("Edabit", "c") ➞ "`Edabit`"
+
+# md_format("That's a strike!", "s") ➞ "~~That's a strike!~~"
+
+# Notes
+
+# Remember to format your comments!
+def md_format(word, style)
+  case style
+  when "b"
+    "**#{word}**"
+  when "i"
+    "_#{word}_"
+  when "c"
+    "`#{word}`"
+  when "s"
+    "~~#{word}~~"
+  end
+end
 # In mathematics, an Arithmetic Progression (AP) is a sequence of numbers such that the difference between the consecutive terms is constant. Create a function that takes three arguments:
 #     The first element of the sequence first
 #     Constant difference between the elements diff
