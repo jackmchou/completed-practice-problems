c80c6830109215273b91c1f3d43e4fdbaf077018
JC <jcacct@protonmail.ch>
2021-06-29 07:35:11 -0700

    Solved Pyramid Arrays in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# Given a number n, return an array containing several arrays. Each array increment in size, from range 1 to n inclusive, contaning its length as the elements.
+# Examples
+
+# pyramid_arrays(1) ➞ [[1]]
+
+# pyramid_arrays(3) ➞ [[1], [2, 2], [3, 3, 3]]
+
+# pyramid_arrays(5) ➞ [[1], [2, 2], [3, 3, 3], [4, 4, 4, 4], [5, 5, 5, 5, 5]]
+
+# Notes
+
+# n will be a positive integer.
+def pyramid_arrays(n)
+	(1..n).map {|e| Array.new(e, e)}
+end
 # Create a function that takes a number as an argument and returns the highest digit in that number.
 # Examples
 
