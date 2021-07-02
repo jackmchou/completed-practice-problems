148b8cd93ef761c70089f40910756993b23d07a1
JC <jcacct@protonmail.ch>
2021-07-02 08:13:14 -0700

    Solved Buggy Code (Part 3) in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,21 @@
+# Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+# Examples
+
+# sum_lst([1, 2, 3, 4, 5]) ➞ 15
+
+# sum_lst([-1, 0, 1]) ➞ 0
+
+# sum_lst([0, 4, 8, 12]) ➞ 24
+
+# Notes
+
+#     READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+#     Don't overthink this challenge; it's not supposed to be hard.
+def sum_lst(lst):
+	total = 0
+	for i in lst:
+		total += i
+	return total
 # Given a list of numbers, return True if the sum of the values in the list is less than 100; otherwise return False.
 # Examples
 
