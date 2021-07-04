4fdcf6fdef8dc00b836d1b150aabc1dd8b6adc7f
JC <jcacct@protonmail.ch>
2021-07-04 13:12:38 -0700

    Solved Unpacking Lists in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,28 @@
+# You can assign variables from lists like this:
+
+# lst = [1, 2, 3, 4, 5, 6]
+# first = lst[0]
+# middle = lst[1:-1]
+# last = lst[-1]
+
+# print(first) ➞ outputs 1
+# print(middle) ➞ outputs [2, 3, 4, 5]
+# print(last) ➞ outputs 6
+
+# With Python 3, you can assign variables from lists in a much more succinct way. Create variables first, middle and last from the given list using destructuring assignment (check the Resources tab for some examples), where:
+
+# first  ➞ 1
+
+# middle ➞ [2, 3, 4, 5]
+
+# last ➞ 6
+
+# Your task is to unpack the list writeyourcodehere into three variables, being first, middle, and last, with middle being everything in between the first and last element. Then print all three variables.
+# Notes
+
+#     Your solution should be just one line of code.
+#     If your solution is longer than one line of code, please review the Resources tab.
+first, *middle, last = [1, 2, 3, 4, 5, 6]
 # I'd like to calculate how long on average I've lived in the same house.
 
 # Given a person's age and the number of times they've moved house as moves, return the average number of years that they've spent living in the same house.
