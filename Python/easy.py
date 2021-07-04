ac34c9534b28c0c992ea17a06d9114d882d6d543
JC <jcacct@protonmail.ch>
2021-07-04 13:08:22 -0700

    Solved Moving House in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,22 @@
+# I'd like to calculate how long on average I've lived in the same house.
+
+# Given a person's age and the number of times they've moved house as moves, return the average number of years that they've spent living in the same house.
+# Examples
+
+# years_in_one_house(30, 1) ➞ 15
+
+# years_in_one_house(15, 2) ➞ 5
+
+# years_in_one_house(80, 0) ➞ 80
+
+# Notes
+
+#     You can assume that the tests include people who've always lived in a house.
+#     Round to the nearest year.
+
+def years_in_one_house(age, moves):
+	return round(age / (moves + 1)) if moves != 0 else age
+
 # Create a function that takes a string (will be a person's first and last name) and returns a string with the first and last name swapped.
 # Examples
 
