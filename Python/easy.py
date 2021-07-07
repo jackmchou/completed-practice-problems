a24015f4dca53740bd4bba89fb9eab4700c3b97f
JC <jcacct@protonmail.ch>
2021-07-07 11:24:35 -0700

    Solved Half, Quarter and Eighth in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,19 @@
+# Create a function that takes a number and return a list of three numbers: half of the number, quarter of the number and an eighth of the number.
+# Examples
+
+# half_quarter_eighth(6) ➞ [3, 1.5, 0.75]
+
+# half_quarter_eighth(22) ➞ [11, 5.5, 2.75]
+
+# half_quarter_eighth(25) ➞ [12.5, 6.25, 3.125]
+
+# Notes
+
+# The order of the list is: half, quarter, eighth.
+
+def half_quarter_eighth(n):
+	return [n * 0.5, n * 0.25, n * 0.125]
+
 # Write a function that takes an integer and returns a string with the given number of "a"s in Edabit.
 # Examples
 
