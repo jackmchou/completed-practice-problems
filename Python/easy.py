153a8166d7070251036aa9a441dee4618794f7a7
JC <jcacct@protonmail.ch>
2021-07-09 11:35:10 -0700

    Solved Destructuring Assignment (Ignoring Values) in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,26 @@
+# You can assign variables from lists like this:
+
+# first, _ , last = [1, 2, 8]
+
+# first   = lst[0]
+
+# _   = ignores second value (2)
+
+# last   = lst[-1]
+
+# print(first) ➞ outputs 1
+# print(last) ➞ outputs 8
+
+# Using Destructuring Assignment (check the Resources tab), your task is to unpack the list writeyourcodehere into three variables, first, a variable to ignore all middle values and last.
+# Notes
+
+#     Your solution should be just One Line code.
+#     If your solution is longer than one line of code, please check the Resources tab.
+#     Another version of this challenge.
+# DO NOT USE first = writeyourcodehere[0]
+# DO NOT USE last = writeyourcodehere[-1]
+# Check Resources Tab for HELP
+first, *_, last = [1, 2, 3, 4, 5, 6, 7, 8]
 # Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as input and calculates the area of that shape.
 # Examples
 
