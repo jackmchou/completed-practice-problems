bf2c7b6aaedbcdafb3b98aa9b9a77d2460992c33
JC <jcacct@protonmail.ch>
2021-07-12 07:13:00 -0700

    Solved Find the Smallest and Biggest Numbers in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,17 @@
+# Create a function that returns True if the combined weight of a car car and the weight of the passengers p in the car is less than the maximum weight max_weight the car is allowed to carry. Otherwise, return False. The weight of the car and the weight of the passengers are given in pounds. The maximum weight is given in kilograms.
+# Examples
+
+# weight_allowed(3000, [150, 201, 75, 88, 195], 1700) ➞ True
+
+# weight_allowed(3200, [220, 101, 115, 228, 15], 1700) ➞ False
+
+# weight_allowed(2900, [225, 171, 300, 274, 191], 1850) ➞ True
+
+# Notes
+
+# 1 pound = 0.453592 kilogram
+def weight_allowed(car, p, max_weight):
+	return (sum(p) + car) * 0.453592 < max_weight
 # Create a function that accepts a list of numbers and return both the minimum and maximum numbers, in that order (as a list).
 # Examples
 
