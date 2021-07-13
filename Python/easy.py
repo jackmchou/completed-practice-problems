e73338bbc3e09dc9ff41be17f400e3a63761c745
JC <jcacct@protonmail.ch>
2021-07-13 06:58:55 -0700

    Solved Number of Stickers in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,25 @@
+# Given a Rubik's Cube with a side length of n, return the number of individual stickers that are needed to cover the whole cube.
+
+# Pictures of Rubik's Cubes
+
+#     The Rubik's cube of side length 1 has 6 stickers.
+#     The Rubik's cube of side length 2 has 24 stickers.
+#     The Rubik's cube of side length 3 has 54 stickers.
+
+# Examples
+
+# how_many_stickers(1) ➞ 6
+
+# how_many_stickers(2) ➞ 24
+
+# how_many_stickers(3) ➞ 54
+
+# Notes
+
+#     Keep in mind there are six faces to keep track of.
+#     Expect only positive whole numbers.
+def how_many_stickers(n):
+	return n ** 2 * 6
 # I have a bucket containing an amount of navy blue paint and I'd like to paint as many walls as possible. Create a function that returns the number of complete walls that I can paint, before I need to head to the shops to buy more.
 #     n is the number of square meters I can paint.
 #     w and h are the widths and heights of a single wall in meters.
