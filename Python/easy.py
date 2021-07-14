aac266fa9de07e219d19278bc20227d84c2e87a7
JC <jcacct@protonmail.ch>
2021-07-14 07:00:38 -0700

    Solved Find the Smallest and Biggest Numbers in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,17 @@
+# Create a function that accepts a list of numbers and return both the minimum and maximum numbers, in that order (as a list).
+# Examples
+
+# min_max([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+# min_max([2334454, 5]) ➞ [5, 2334454]
+
+# min_max([1]) ➞ [1, 1]
+
+# Notes
+
+# All test lists will have at least one element and are valid.
+def min_max(nums):
+	return [min(nums), max(nums)]
 # Given a Rubik's Cube with a side length of n, return the number of individual stickers that are needed to cover the whole cube.
 
 # Pictures of Rubik's Cubes
