0e04a1f87c44ec7298449454ed687eb1ab5166b8
JC <jcacct@protonmail.ch>
2021-07-19 06:45:42 -0700

    Solved Solve the Equation in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# Create a function that takes an equation (e.g. "1+1"), and returns the answer.
+# Examples
+
+# equation("1+1") ➞ 2
+
+# equation("7*4-2") ➞ 26
+
+# equation("1+1+1+1+1") ➞ 5
+
+# Notes
+
+# Supported operators are +, -, and *.
+def equation(s)
+	eval(s)
+end
 # A word has been split into a left part and a right part. Re-form the word by adding both halves together, changing the first character to an uppercase letter.
 # Examples
 
