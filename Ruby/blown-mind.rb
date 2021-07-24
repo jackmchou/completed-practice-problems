398680c4f15122c51e4a5926ef1716ffd569f7ea
JC <jcacct@protonmail.ch>
2021-07-24 09:08:58 -0700

    Solved Find Unique Number in Array in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,27 @@
+# Create a function that takes an array of integers as an argument and returns a unique number from that array. All numbers except unique ones have the same number of occurrences in the array.
+# Examples
+
+# find_single_number([2, 2, 2, 3, 4, 4, 4]) ➞ 3
+
+# find_single_number([2]) ➞ 2
+
+# find_single_number([]) ➞ []
+
+# find_single_number([7, 13, 3, 6, 5, 4, 4, 13, 5, 3, 6, 7, 6, 5, 3, 13, 4, 7, 13, 5, 7, 4, 3, 6, 8, 4, 3, 7, 5, 6, 13]) ➞ 8
+
+# find_single_number([1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 101, 4, 3, 1, 5, 6, 2]) ➞ 101
+
+# Notes
+
+#     Don't forget to return the result.
+#     Be aware this challenge includes two validations:
+#         Empty input should return an empty array [] (example #3).
+#         Single item arrays should return that item (example #2).
+#     There are always 1 or 0 unique numbers in the input. No two or three+ uniques.
+#     If you're stuck or your solution is over complicated check the Resources tab.
+def find_single_number(numbers)
+	numbers.select {|n| numbers.count(n) == 1}[0]
+end
 # Create a function that returns true if a string is empty and false otherwise.
 # Examples
 
