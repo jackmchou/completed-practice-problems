ff7d00ea5d50b2680869de46b2a2b0346f3f6b8a
JC <jcacct@protonmail.ch>
2021-08-02 13:05:06 -0700

    Solved Century Crisis in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,25 @@
+# Scientists have discovered that in four decades, the world will EXPLODE! It will also take three decades to make a spaceship to travel to a new planet that can hold the entire world population.
+
+# You must calculate the number of people there will be in three decades from now.
+
+#     The variable population is the world population now.
+#     Assume that every month, someone gives birth to more people n.
+
+# Return the number of people there will be when the spaceship is complete.
+# Examples
+
+# future_people(256, 2) ➞ 976
+
+# future_people(3248, 6) ➞ 5408
+
+# future_people(5240, 3) ➞ 6320
+
+# Notes
+
+# Humanity's fate lies in your calculation.
+def future_people(population, n)
+	population + n * 360
+end
 # Write a function that validates whether two strings are identical. Make it case insensitive.
 # Examples
 # match("hello", "hELLo") ➞ true
