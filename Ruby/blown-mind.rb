6b51457488f212a50c320d3b971e7d28ef3b3fa8
JC <jcacct@protonmail.ch>
2021-08-03 15:17:32 -0700

    Solved Name Greeting! in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,21 @@
+# Create a function that takes a name and returns a greeting in the form of a string.
+# Examples
+
+# hello_name("Gerald") ➞ "Hello Gerald!"
+
+# hello_name("Tiffany") ➞ "Hello Tiffany!"
+
+# hello_name("Ed") ➞ "Hello Ed!"
+
+# Notes
+
+#     The input is always a name (as string).
+#     Don't forget the exclamation mark!
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def hello_name(name)
+	'Hello ' + name + '!'
+end
 # Given a Rubik's Cube with a side length of n, return the number of individual stickers that are needed to cover the whole cube.
 
 # Pictures of Rubik's Cubes
