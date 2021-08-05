502d42d28e7b94d7ad7a1c6c2ac702419163689b
JC <jcacct@protonmail.ch>
2021-08-05 15:21:20 -0700

    Solved Transform into an Array with No Duplicates in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,28 @@
+# A set is a collection of unique items. A set can be formed from an array from removing all duplicate items.
+
+# [1, 3, 3, 5, 5, 5]
+# # original array
+
+# [1, 3, 5]
+# # original array transformed into a set
+
+# Create a function that sorts an array and removes all duplicate items from it.
+# Examples
+
+# set([1, 3, 3, 5, 5]) ➞ [1, 3, 5]
+
+# set([4, 4, 4, 4]) ➞ [4]
+
+# set([5, 7, 8, 9, 10, 15]) ➞ [5, 7, 8, 9, 10, 15]
+
+# set([3, 3, 3, 2, 1]) ➞ [1, 2, 3]
+
+# Notes
+
+# See resources for a hint if you get stuck.
+def set(arr)
+	arr.uniq
+end
 # Create a function that takes a name and returns a greeting in the form of a string.
 # Examples
 
