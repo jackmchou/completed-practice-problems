a76379f133e5a9331fb0ad8d4b3eada452335161
JC <jcacct@protonmail.ch>
2021-08-06 15:27:54 -0700

    Solved Tile Teamwork Tactics in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,23 @@
+# In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided dice. If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.
+
+# Given you and your friend's tile number, create a function that returns if it's possible to earn a bonus when you roll the dice.
+# Examples
+
+# possible_bonus(3, 7) ➞ true
+
+# possible_bonus(1, 9) ➞ false
+
+# possible_bonus(5, 3) ➞ false
+
+# Notes
+
+#     You cannot move backward (which is why example #3 doesn't work).
+#     If you are already on the same tile, return false, as you would be advancing away.
+#     Expect only positive integer inputs.
+def possible_bonus(a, b)
+	distance = (a - b).abs()
+	distance < 7 && a < b
+end
 # A set is a collection of unique items. A set can be formed from an array from removing all duplicate items.
 
 # [1, 3, 3, 5, 5, 5]
