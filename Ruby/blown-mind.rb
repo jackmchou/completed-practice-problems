5f1ebe9e620b12b58747f0762a6061f828fc8826
JC <jcacct@protonmail.ch>
2021-08-11 05:39:36 -0700

    Solved Triangle and Parallelogram Area Finder in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,20 @@
+# Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as input and calculates the area of that shape.
+# Examples
+
+# area_shape(2, 3, "triangle") ➞ 3
+
+# area_shape(8, 6, "parallelogram") ➞ 48
+
+# area_shape(2.9, 1.3, "parallelogram") ➞ 3.77
+
+# Notes
+
+#     Area of a triangle is 0.5 * b * h
+#     Area of a parallelogram is b * h
+#     Assume triangle and parallelogram are the only inputs for shape.
+def area_shape(base, height, shape)
+	shape == "triangle" ? base * height * 0.5 : base * height
+end
 # Given a string, return true if its length is even or false if the length is odd.
 # Examples
 
