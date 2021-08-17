91040db9df56fdfe4b6577d6ad3d33c38f56b2ac
JC <jcacct@protonmail.ch>
2021-08-16 20:59:48 -0700

    Solved Keyboard Mistakes in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,33 @@
+# Character recognition software often makes mistakes when documents (especially old ones written with a typewriter) are digitized.
+
+# Your task is to correct the errors in the digitized text. You only have to handle the following mistakes:
+
+#     A is misinterpreted as 4
+#     S is misinterpreted as 5
+#     O is misinterpreted as 0
+#     I is misinterpreted as 1
+
+# The test cases contain numbers only by mistake.
+# Examples
+
+# keyboard_mistakes("MUB45H1R") ➞ "MUBASHIR"
+
+# keyboard_mistakes("DUBL1N") ➞ "DUBLIN"
+
+# keyboard_mistakes("51NG4P0RE") ➞ "SINGAPORE"
+
+# Notes
+
+# N/A
+def keyboard_mistakes(str)
+	letters = {
+		"4" => "A",
+		"5" => "S",
+		"0" => "O",
+		"1" => "I"
+	}
+	str.gsub(/\d/, letters)
+end
 # Write a function that returns the number of users in a chatroom based on the following rules:
 #     If there is no one, return "no one online".
 #     If there is 1 person, return "user1 online".
