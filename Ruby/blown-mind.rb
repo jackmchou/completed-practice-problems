ce2a438709216d2bb4a36080f19e6132ac6dde0c
JC <jcacct@protonmail.ch>
2021-08-17 22:56:35 -0700

    Solved Nth Star Number in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,30 @@
+# Create a function that takes a positive integer n and returns the nth "star number".
+
+# A star number is a centered figurate number a centered hexagram (six-pointed star), such as the one that Chinese checkers is played on.
+# Examples
+
+# star_number(2) ➞ 13
+# # n = 2
+# # 2nd star number = 13
+
+# star_number(3) ➞ 37
+# # n = 3
+# # 3rd star number = 37
+
+# star_number(5) ➞ 121
+# # n = 5
+# # 5th star number = 121
+
+# Notes
+
+#     n will always be a positive integer.
+#     The nth term of a star number can be represented as 6n(n-1) + 1
+#     See Resources for more information.
+
+def star_number(n)
+	6 * n * (n-1) + 1
+end
+
 # Character recognition software often makes mistakes when documents (especially old ones written with a typewriter) are digitized.
 
 # Your task is to correct the errors in the digitized text. You only have to handle the following mistakes:
