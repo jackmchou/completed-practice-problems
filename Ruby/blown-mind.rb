5428289ecffc87eacaff8aa9159b438422137834
JC <jcacct@protonmail.ch>
2021-08-20 18:23:49 -0700

    Solved Sum of all Evens in a Matrix in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,38 @@
+# Create a function that returns the sum of all even elements in a 2D matrix.
+# Examples
+
+# sum_of_evens([
+#   [1, 0, 2],
+#   [5, 5, 7],
+#   [9, 4, 3]
+# ]) ➞ 6
+
+# // 2 + 4 = 6
+
+# sum_of_evens([
+#   [1, 1],
+#   [1, 1]
+# ]) ➞ 0
+
+# sum_of_evens([
+#   [42, 9],
+#   [16, 8]
+# ]) ➞ 66
+
+# sum_of_evens([
+#   [],
+#   [],
+#   []
+# ]) ➞ 0
+
+# Notes
+
+#     Submatrices will be of equal length.
+#     Return 0 if the 2D matrix only consists of empty submatrices.
+def sum_of_evens(arr)
+	evens = arr.flatten.select {|num| num.even?}
+	evens.reduce(0) { |sum, num| sum + num }
+end
 # Create methods for the Calculator class that can do the following:
 
 # 	Add two numbers.
