2eaae2a55962a0eebe2514e0497b68584f73e822
JC <jcacct@protonmail.ch>
2021-08-23 18:09:26 -0700

    Solved Is the String Odd or Even? in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,21 @@
+# Given a string, return True if its length is even or False if the length is odd.
+# Examples
+
+# odd_or_even("apples") ➞ True
+# # The word "apples" has 6 characters.
+# # 6 is an even number, so the program outputs True.
+
+# odd_or_even("pears") ➞ False
+# # "pears" has 5 letters, and 5 is odd.
+# # Therefore the program outputs False.
+
+# odd_or_even("cherry") ➞ True
+
+# Notes
+
+# N/A
+def odd_or_even(word):
+	return len(word) % 2 == 0
 # Create a function that takes as a parameter a list of "stringified" numbers and returns a list of numbers.
 # Example:
 # ["1", "3", "3.6"] ➞ [1, 3, 3.6]
