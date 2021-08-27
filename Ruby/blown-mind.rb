7ea68d9b85e99cee2632fde0792e6db593ca3831
JC <jcacct@protonmail.ch>
2021-08-26 18:24:57 -0700

    Solved Omnipresent Value in Py

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,29 @@
+# A value is omnipresent if it exists in every subarray inside the main array.
+
+# To illustrate:
+
+# [[3, 4], [8, 3, 2], [3], [9, 3], [5, 3], [4, 3]]
+# # 3 exists in every element inside this array, so is omnipresent.
+
+# Create a function that determines whether an input value is omnipresent for a given array.
+# Examples
+
+# is_omnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 1) ➞ true
+
+# is_omnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 6) ➞ false
+
+# is_omnipresent([[5], [5], [5], [6, 5]], 5) ➞ true
+
+# is_omnipresent([[5], [5], [5], [6, 5]], 6) ➞ false
+
+# Notes
+
+# Sub-arrays can be any length.
+
+def is_omnipresent(arr, val)
+	arr.all? {|item| item.include? val}
+end
+
 # Create a function that takes an array of numbers between 1 and 10 (excluding one number) and returns the missing number.
 # Examples
 # missing_num([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
