2ccbf303012fa8e04612f671df856aea937440f6
JC <jcacct@protonmail.ch>
2021-09-01 10:45:50 -0700

    Solved Area of a Rectangle in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,20 @@
+# Create a function that calculates the area of a rectangle. If the arguments are invalid, your function must return -1.
+# Examples
+
+# area(3, 4) ➞ 12
+
+# area(10, 11) ➞ 110
+
+# area(-1, 5) ➞ -1
+
+# area(0, 2) ➞ -1
+
+# Notes
+
+# N/A
+def area(h, w)
+	h > 0 && w > 0 ? h * w : -1
+end
 # The challenge is to try and fix this buggy code, given the inputs true and false. See the examples below for the expected output.
 # Examples
 
