199c94cd5fc48589e0d7de93f67799f9b02993e9
JC <jcacct@protonmail.ch>
2021-09-03 11:16:45 -0700

    Solved Check if a Number is Prime in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,27 @@
+# Create a function that returns true if a number is prime, and false otherwise. A prime number is any positive integer that is evenly divisible by only two divisors: 1 and itself.
+
+# The first ten prime numbers are:
+
+# 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+
+# Examples
+
+# is_prime(31) ➞ true
+
+# is_prime(18) ➞ false
+
+# is_prime(11) ➞ true
+
+# Notes
+
+#     A prime number has no other factors except 1 and itself.
+#     If a number is odd it is not divisible by an even number.
+#     1 is not considered a prime number.
+def is_prime(num)
+	require "prime"
+	Prime.prime?(num)
+end
+
 # Suppose you have a guest list of students and the country they are from, stored as key-value pairs in a hash.
 
 # GUEST_LIST = {
