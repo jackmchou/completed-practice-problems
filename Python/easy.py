d29ff1e146ef4adacd321e730ebbe17674821d56
JC <jcacct@protonmail.ch>
2021-09-04 11:32:44 -0700

    Solved Flip the Boolean in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,21 @@
+# Create a function that reverses a boolean value and returns the string "boolean expected" if another variable type is given.
+# Examples
+
+# reverse(True) ➞ False
+
+# reverse(False) ➞ True
+
+# reverse(0) ➞ "boolean expected"
+
+# reverse(None) ➞ "boolean expected"
+
+# Notes
+
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def reverse(arg):
+	return not(arg) if isinstance(arg, bool) else "boolean expected"
 # Create a function that takes a string and returns a string in which each character is repeated once.
 # Examples
 
