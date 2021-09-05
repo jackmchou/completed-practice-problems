952ac2766020b6e0ccda1441474205d38123fcd1
JC <jcacct@protonmail.ch>
2021-09-05 12:04:16 -0700

    Solved Return the Index of All Capital Letters in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,29 @@
+# Create a function that takes a single string as argument and returns an ordered list containing the indices of all capital letters in the string.
+# Examples
+
+# index_of_caps("eDaBiT") ➞ [1, 3, 5]
+
+# index_of_caps("eQuINoX") ➞ [1, 3, 4, 6]
+
+# index_of_caps("determine") ➞ []
+
+# index_of_caps("STRIKE") ➞ [0, 1, 2, 3, 4, 5]
+
+# index_of_caps("sUn") ➞ [1]
+
+# Notes
+
+#     Return an empty list if no uppercase letters are found in the string.
+#     Special characters ($#@%) and numbers will be included in some test cases.
+#     Assume all words do not have duplicate letters.
+
+def index_of_caps(word):
+	arr = []
+	for letter in word:
+		if letter.isupper():
+			arr.append(word.index(letter))
+	return arr
+
 # Create a function that returns the thickness (in meters) of a piece of paper after folding it n number of times. The paper starts off with a thickness of 0.5mm.
 # Examples
 
