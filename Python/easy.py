d4fec31ac2cc1a6bd8895b755612d13e203b72a4
JC <jcacct@protonmail.ch>
2021-09-05 11:50:37 -0700

    Solved Create a function that returns the thickness (in meters) of a piece of paper after folding it n number of times. The paper starts off with a thickness of 0.5mm. in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,22 @@
+# Create a function that returns the thickness (in meters) of a piece of paper after folding it n number of times. The paper starts off with a thickness of 0.5mm.
+# Examples
+
+# num_layers(1) ➞ "0.001m"
+# # Paper folded once is 1mm (equal to 0.001m)
+
+# num_layers(4) ➞ "0.008m"
+# # Paper folded 4 times is 8mm (equal to 0.008m)
+
+# num_layers(21) ➞ "1048.576m"
+# # Paper folded 21 times is 1048576mm (equal to 1048.576m)
+
+# Notes
+
+#     There are 1000mm in a single meter.
+#     Don't round answers.
+def num_layers(n):
+	thickness = 2 ** n * 0.5 / 1000
+	return "%sm" % (thickness) if n > 0 else "0.0005m"
 # Create a function that reverses a boolean value and returns the string "boolean expected" if another variable type is given.
 # Examples
 
