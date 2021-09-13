257e33e00684b5636055babf8a35c0acafe4ced9
JC <jcacct@protonmail.ch>
2021-09-13 09:40:37 -0700

    Solved Find the Other Two Side Lengths in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Given the shortest side of a 30 60 90 triangle you have to find out the other 2 sides, (return the longest side, medium-length side).
+# Examples
+
+# returnsides(1) ➞ [2, 1.73]
+
+# returnsides(2) ➞ [4, 3.46]
+
+# returnsides(3) ➞ [6, 5.2]
+
+# Notes
+
+#     30 60 90 triangles always follow this rule, let's say the shortest side length is x units, the hypotenuse would be 2x units and the other side would be x * square root of 3 units.
+#     In the Tests, the decimal is rounded to 2 places.
+def returnsides(length)
+	[length * 2, (length * Math.sqrt(3)).round(2)]
+end
 # I am trying to filter out empty arrays from an array. In other words, I want to transform something that looks like this: ["a", "b", [], [], [1, 2, 3]] to look like ["a", "b", [1, 2, 3]]. My code looks like this:
 # def removeEmptyArrays(arr)
 #   arr.select{|x| !x.empty?}
