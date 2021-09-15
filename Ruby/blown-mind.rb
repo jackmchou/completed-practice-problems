b83af157e70b258fe8f7b5ebfa46c971e80ef9f0
JC <jcacct@protonmail.ch>
2021-09-15 09:50:07 -0700

    Solved Char-to-ASCII in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,24 @@
+# Create a function that returns the ASCII value of the passed in character.
+# Examples
+
+# ctoa("A") ➞ 65
+
+# ctoa("m") ➞ 109
+
+# ctoa("[") ➞ 91
+
+# ctoa("\") ➞ 92
+
+# Notes
+
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+
+def ctoa(char)
+	char.ord
+end
+
 # The abs() function returns the absolute value of a number. This means it returns a number's positive value. You can think of it as the distance away from zero.
 
 # Create a function that recreates this functionality.
