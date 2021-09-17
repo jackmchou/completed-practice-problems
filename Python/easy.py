f4a55ab48ca77c8530bbe8ef5c5a0681fa9b2154
JC <jcacct@protonmail.ch>
2021-09-17 14:08:52 -0700

    Solved Buggy Code (Part 4) in Py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,20 @@
+# Emmy has written a function that returns a greeting to users. However, she's in love with Mubashir, and would like to greet him slightly differently. She added a special case in her function, but she made a mistake.
+
+# Can you help her?
+# Examples
+
+# greeting("Matt") ➞ "Hello, Matt!"
+
+# greeting("Helen") ➞ "Hello, Helen!"
+
+# greeting("Mubashir") ➞ "Hello, my Love!"
+
+# Notes
+
+#     READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+#     Don't overthink this challenge; it's not supposed to be hard.
+def greeting(name):
+	return "Hello, my Love!" if name == "Mubashir" else "Hello, " + name + "!"
 # Create a function that will handle simple math expressions. The input is an expression in the form of a string.
 # Examples
 
