2600feafe7be2784af0340b23485c49ac01e33a1
JC <jcacct@protonmail.ch>
2021-09-23 09:14:11 -0700

    Solved FizzBuzz Interview Question in Ruby

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,37 @@
+# Create a function that takes a number as an argument and returns "Fizz", "Buzz" or "FizzBuzz".
+
+# If the number is a multiple of 3 the output should be "Fizz".
+# If the number given is a multiple of 5, the output should be "Buzz".
+# If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
+# If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
+# The output should always be a string even if it is not a multiple of 3 or 5.
+
+# Examples
+
+# fizz_buzz(3) ➞ "Fizz"
+
+# fizz_buzz(5) ➞ "Buzz"
+
+# fizz_buzz(15) ➞ "FizzBuzz"
+
+# fizz_buzz(4) ➞ "4"
+
+# Notes
+
+# Try to be precise with how you spell things and where you put the capital letters.
+# If you get stuck on a challenge, find help in the Resources tab.
+# If you're really stuck, unlock solutions in the Solutions tab.
+def fizz_buzz(num)
+  if num % 3 == 0 and num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num.to_s
+  end
+end
 # Given a pH value, return whether that value is "alkaline" (greater than 7), "acidic" (less than 7), or "neutral" (7). Return "invalid" if the value given is less than 0 or greater than 14.
 
 # Image of a pH chart
