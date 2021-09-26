dfbc99cfae292b6ea71fe9b19993d468b5c6c551
JC <jcacct@protonmail.ch>
2021-09-26 13:38:09 -0700

    Solved Product of All Odd Integers in Ruby

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,4 +1,19 @@
+# Create a function that returns the product of all odd integers in an array.
+# Examples
 
+# odd_product([3, 4, 1, 1, 5]) ➞ 15
+
+# odd_product([5, 5, 8, 2, 4, 32]) ➞ 25
+
+# odd_product([1, 2, 1, 2, 1, 2, 1, 2]) ➞ 1
+
+# Notes
+
+# N/A
+def odd_product(arr)
+  odd_int = arr.select {|num| num.odd?}
+  odd_int.reduce(:*)
+end
 # Create a function that takes an array and returns the difference between the biggest and smallest numbers.
 # Examples
 
