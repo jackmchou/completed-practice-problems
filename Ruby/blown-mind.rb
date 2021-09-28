5b04d429127bc5df86c23a9af00a286770898cd3
JC <jcacct@protonmail.ch>
2021-09-28 07:28:56 -0700

    Solved Lowercase, Uppercase or Mixed? in Ruby

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,26 @@
+# Create a function which returns "upper" if all the letters in a word are uppercase, "lower" if lowercase and "mixed" for any mix of the two.
+# Examples
+
+# get_case("whisper...") ➞ "lower"
+
+# get_case("SHOUT!") ➞ "upper"
+
+# get_case("Indoor Voice") ➞ "mixed"
+
+# Notes
+
+# Ignore punctuation, spaces and numbers.
+
+def get_case(str)
+  if str == str.downcase
+    "lower"
+  elsif str == str.upcase
+    "upper"
+  else
+    "mixed"
+  end
+end
+
 # A leap year happens every four years, so it's a year that is perfectly divisible by four. However, if the year is a multiple of 100 (1800, 1900, etc), the year must be divisible by 400.
 
 # Write a function that determines if the year is a leap year or not.
