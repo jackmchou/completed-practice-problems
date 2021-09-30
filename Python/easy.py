b60ca57dc881f59d39211966c11c18bba05b7773
JC <jcacct@protonmail.ch>
2021-09-30 08:42:53 -0700

    Solved Fix the Error: Check Whether a Given Number Is Odd in Python

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,21 @@
+# Éowyn has written the function is_odd() to check if a given number is odd or not. Unfortunately, the function does not return the correct result for all the inputs. Help her fix the error.
+
+# def is_odd(num):
+#   return num % 1 == 1 or 2
+
+# Examples
+
+# is_odd(-5) ➞ True
+
+# is_odd(25) ➞ True
+
+# is_odd(0) ➞ False
+
+# Notes
+
+# All the inputs will only be integers.
+def is_odd(num):
+  return num % 2 == 1
 # Create a function that takes a list of strings and integers, and filters out the list so that it returns a list of integers only.
 # Examples
 # filter_list([1, 2, 3, "a", "b", 4]) ➞ [1, 2, 3, 4]
