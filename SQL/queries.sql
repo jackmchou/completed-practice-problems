29d165114e885e3c1577b34b68439abd3269f1ed
JC <jcacct@protonmail.ch>
2021-10-07 07:18:48 -0700

    Solved SQLbolt exercise 2.3 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -15,3 +15,6 @@ WHERE id = 6;
 -- Find the movies released in the years between 2000 and 2010 
 SELECT * FROM movies
 WHERE  year > 2000 and year <= 2010
+-- Find the movies not released in the years between 2000 and 2010 
+SELECT * FROM movies
+WHERE year NOT BETWEEN 2000 AND 2010
