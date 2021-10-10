206b39d2f40f9d2da8aa0dc8b43332c474dc87d0
JC <jcacct@protonmail.ch>
2021-10-10 07:46:44 -0700

    Solved SQLbolt exercise 4.2 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -36,3 +36,7 @@ WHERE title LIKE "Wall-_"
 -- List all directors of Pixar movies (alphabetically), without duplicates
 SELECT DISTINCT director FROM movies
 ORDER BY director ASC
+-- List the last four Pixar movies released (ordered from most recent to least) 
+SELECT * FROM movies
+ORDER BY year DESC
+LIMIT 4
