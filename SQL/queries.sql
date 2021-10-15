d5ed47f73259ca08bab0e2722b2a50b8f3f269de
JC <jcacct@protonmail.ch>
2021-10-15 08:26:08 -0700

    Solved SQLbolt exercise 5.5 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -64,3 +64,8 @@ SELECT * FROM north_american_cities
 WHERE country = "Mexico"
 ORDER BY population desc
 LIMIT 2
+-- List the third and fourth largest cities (by population) in the United States and their population 
+SELECT * FROM north_american_cities
+WHERE country = "United States"
+ORDER BY population desc
+LIMIT 2 OFFSET 2
