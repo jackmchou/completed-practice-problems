5ac8694f4f4cbbaa227b0242568a9605b2586a8f
JC <jcacct@protonmail.ch>
2021-11-04 17:10:56 -0700

    Solved SQLbolt exercise 14.3 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -150,3 +150,7 @@ WHERE title = "A Bug's Life";
 UPDATE movies
 SET year = 1999
 WHERE title = "Toy Story 2";
+-- Both the title and director for Toy Story 8 is incorrect! The title should be "Toy Story 3" and it was directed by Lee Unkrich
+UPDATE movies
+SET title = "Toy Story 3", director = "Lee Unkrich"
+WHERE title = "Toy Story 8";
