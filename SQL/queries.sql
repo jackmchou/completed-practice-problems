40ee9ce7074317829a22b97a75726e9456b18584
JC <jcacct@protonmail.ch>
2021-11-06 17:21:17 -0700

    Solved SQLbolt exercise 16.1 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -160,3 +160,14 @@ WHERE YEAR < 2005
 -- Andrew Stanton has also left the studio, so please remove all movies directed by him.
 DELETE FROM movies
 WHERE director = "Andrew Stanton"
+-- Create a new table named Database with the following columns:
+-- – Name A string (text) describing the name of the database
+-- – Version A number (floating point) of the latest version of this database
+-- – Download_count An integer count of the number of times this database was downloaded
+-- This table has no constraints.
+CREATE TABLE Database (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    version INTEGER, 
+    download_count INTEGER
+);
