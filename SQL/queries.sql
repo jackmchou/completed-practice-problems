3592f94957fc08537af469622d037e6620894289
JC <jcacct@protonmail.ch>
2021-11-10 07:01:53 -0800

    Solved SQLbolt exercise 18.2 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -180,3 +180,5 @@ ADD Language TEXT
     DEFAULT "English"
 -- We've sadly reached the end of our lessons, lets clean up by removing the Movies table 
 DROP TABLE IF EXISTS movies;
+-- And drop the BoxOffice table as well
+DROP TABLE IF EXISTS boxoffice;
