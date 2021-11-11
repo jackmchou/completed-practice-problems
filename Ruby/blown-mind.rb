faf332f60684f5a4aa0d135dd66e58dfe9d76eb0
JC <jcacct@protonmail.ch>
2021-11-11 08:30:46 -0800

    Solved After N Months... in Ruby

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,26 @@
+# Create a function that takes in year and month as input, then return what year it would be after n-months has elapsed.
+# Examples
+
+# after_n_months(2020, 24) ➞ 2022
+
+# after_n_months(1832, 2) ➞ 1832
+
+# after_n_months(1444, 60) ➞ 1449
+
+# Notes
+
+#     Assume that adding 12 months will always increment the year by 1.
+#     If no value is given for year or months, return "year missing" or "month missing".
+#     At least one value will be present.
+def after_n_months(year, month)
+	if year.nil?
+		"year missing" 
+	elsif month.nil?
+		"month missing"
+	else
+		year + (month / 12).floor
+	end
+end
 # Write a function that takes the last number of a consecutive list of numbers and returns the total of all numbers up to and including it.
 # Examples
 # add_up_to(3) ➞ 6
