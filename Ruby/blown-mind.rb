b87f8fa56a1b7f55d9dc043abfa9909dad5337c3
JC <jcacct@protonmail.ch>
2021-11-20 17:46:21 -0800

    Solved How Many Decimal Places? in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,13 @@
+# Create a function that returns the number of decimal places a number has. Any zeros after the decimal point count towards the number of decimal places.
+# Examples
+# get_decimal_places("43.20") ➞ 2
+# get_decimal_places("400") ➞ 0
+# get_decimal_places("3.1") ➞ 1
+# Notes
+# Return 0 if the number doesn't have any decimal places (see example #2).
+def get_decimal_places(n)
+	(n.split(".")[1] || []).size
+end
 # Create a function that takes a string as input and capitalizes a letter if its ASCII code is even and returns its lower case version if its ASCII code is odd.
 # Examples
 # ascii_capitalize("to be or not to be!") ➞ "To Be oR NoT To Be!"
