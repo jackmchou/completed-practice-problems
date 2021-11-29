64622fc0730adf97e4d4859b2748a1623265aecc
JC <jcacct@protonmail.ch>
2021-11-29 09:08:23 -0800

    Solved Adding Numbers in a String in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Given a string of numbers separated by a comma and space, return the total sum of all the numbers.
+# Examples
+
+# add_nums("2, 5, 1, 8, 4") ➞ 20
+
+# add_nums("1, 2, 3, 4, 5, 6, 7") ➞ 28
+
+# add_nums("10") ➞ 10
+
+# Notes
+
+#     Numbers will always be separated by a comma and space.
+#     Your function should accept negative numbers.
+def add_nums(nums)
+	nums.split(', ').reduce(0) {|sum, num| sum + num.to_i}
+end
 # Remove enemies from the array of people, even if the enemy shows up twice.
 # Examples
 # remove_enemies(["Fred"], []) ➞ ["Fred"]
