59f02110de2bfbaf72c5ed0e1d19ede0a0498ade
JC <jcacct@protonmail.ch>
2021-12-07 17:05:21 -0800

    Solved Which Function Returns the Larger Number? in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,21 @@
+# Your function will be passed two functions, f and g, that don't take any parameters. Your function has to call them, and return a string which indicates which function returned the larger number.
+
+#     If f returns the larger number, return the string f.
+#     If g returns the larger number, return the string g.
+#     If the functions return the same number, return the string neither.
+
+# Examples
+
+# larger(lambda{5}, lambda{10}) ➞ "g"
+
+# larger(lambda{25}, lambda{25}) ➞ "neither"
+
+# larger(lambda{505050}, lambda{5050}) ➞ "f"
+
+# Notes
+
+# This exercise is designed as an introduction to higher order functions (functions which use other functions to do their work).
+
 # Create two functions: a left-shift function and a right-shift function. Each function will take in an array and a single parameter: the number of shifts.
 
 # [1, 2, 3, 4, 5]
