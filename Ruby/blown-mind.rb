b001d379c9a0d42daa8c6eb6f2e1d050cbd0eb6d
JC <jcacct@protonmail.ch>
2021-12-14 10:30:29 -0800

    Solved Largest Swap in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,26 @@
+# Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+
+# To illustrate:
+
+# largest_swap(27) ➞ false
+
+# largest_swap(43) ➞ true
+
+# If 27 is our input, we should return false because swapping the digits gives us 72, and 72 > 27. On the other hand, swapping 43 gives us 34, and 43 > 34.
+# Examples
+
+# largest_swap(14) ➞ false
+
+# largest_swap(53) ➞ true
+
+# largest_swap(99) ➞ true
+
+# Notes
+
+# Numbers with two identical digits (third example) should yield true (you can't do better).
+def largest_swap(num)
+	num.to_s.chars.reverse.join('').to_i <= num
+end
 # Create a function that takes a string of words and return a string sorted alphabetically by the last character of each word.
 # Examples
 
