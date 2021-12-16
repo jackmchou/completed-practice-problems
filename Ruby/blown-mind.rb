f8fb4ef72f99e8b16de812784245bad9ab13393c
JC <jcacct@protonmail.ch>
2021-12-16 11:13:21 -0800

    Solved Temperature Conversion in Rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,32 @@
+# Write a program that takes a temperature input in celsius and converts it to Fahrenheit and Kelvin. Return the converted temperature values in an array.
+
+# The formula to calculate the temperature in Fahrenheit from Celsius is:
+
+# F = C*9/5 +32
+
+# The formula to calculate the temperature in Kelvin from Celsius is:
+
+# K = C + 273.15
+
+# Examples
+
+# temp_conversion(0) ➞ [32, 273.15]
+# # 0°C is equal to 32°F and 273.15 K.
+
+# temp_conversion(100) ➞ [212, 373.15]
+
+# temp_conversion(-10) ➞ [14, 263.15]
+
+# temp_conversion(300.4) ➞ [572.72, 573.55]
+
+# Notes
+
+#     Return calculated temperatures up to two decimal places.
+#     Return "Invalid" if K is less than 0.
+def temp_conversion(c)
+	return "Invalid" if (c + 273.15).round(2) < 0
+	[(c*9/5 + 32).round(2), (c + 273.15).round(2)]
+end
 # Create a function that ends the first word of a phrase with "ed", essentially verbifying a noun.
 # Examples
 
