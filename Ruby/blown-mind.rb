9edb84851a216ea51c867bbd6e12565dc4adbe6e
JC <jcacct@protonmail.ch>
2021-12-21 10:25:16 -0800

    Solved From A to Z in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,22 @@
+# Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
+# Examples
+
+# gimme_the_letters("a-z") ➞ "abcdefghijklmnopqrstuvwxyz"
+
+# gimme_the_letters("h-o") ➞ "hijklmno"
+
+# gimme_the_letters("Q-Z") ➞ "QRSTUVWXYZ"
+
+# gimme_the_letters("J-J") ➞ J"
+
+# Notes
+
+#     A hyphen will separate the two letters in the string.
+#     You don't need to worry about error handling in this one (i.e. both letters will be the same case and the second letter will always be after the first alphabetically).
+def gimme_the_letters(spectrum)
+  start, finish = spectrum.split('-')
+  (start..finish).to_a.join
+end
 # Write a function that reverses the subarray between the start and end index (inclusive). The rest of the array should be kept the same.
 # Examples
 
