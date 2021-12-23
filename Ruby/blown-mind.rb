9549addb78fece20974366bf20700b3f8b3345b5
JC <jcacct@protonmail.ch>
2021-12-23 08:47:42 -0800

    Solved H4ck3r Sp34k in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# Create a function that takes a string as an argument and returns a coded (h4ck3r 5p34k) version of the string.
+# Examples
+
+# hacker_speak("javascript is cool") ➞ "j4v45cr1pt 15 c00l"
+
+# hacker_speak("programming is fun") ➞ "pr0gr4mm1ng 15 fun"
+
+# hacker_speak("become a coder") ➞ "b3c0m3 4 c0d3r"
+
+# Notes
+
+# In order to work properly, the function should replace all "a"s with 4, "e"s with 3, "i"s with 1, "o"s with 0, and "s"s with 5.
+def hacker_speak(str)
+	str.gsub(/[aeios]/i, 'a' => 4, 'e' => 3, 'i' => 1, 'o' => 0, 's' => 5)
+end
 # Create a function that takes two parameters (start, stop), and returns the sum of all even numbers in the range.
 # Examples
 
