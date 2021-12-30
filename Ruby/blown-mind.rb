9dc42c59f52c396d06e5f97da9de76b914a331e3
JC <jcacct@protonmail.ch>
2021-12-29 21:01:25 -0800

    Solved On/Off Switches in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# Create a function that returns how many possible arrangements can come from a certain number of switches (on / off). In other words, for a given number of switches, how many different patterns of on and off can we have?
+# Examples
+
+# pos_com(1) ➞ 2
+
+# pos_com(3) ➞ 8
+
+# pos_com(10) ➞ 1024
+
+# Notes
+
+# All numbers will be whole and positive.
+def pos_com(n)
+	2 ** n
+end
 # Create a function that takes an array of numbers or letters and returns a string.
 # Examples
 
