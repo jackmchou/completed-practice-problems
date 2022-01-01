38798f5204dbf617a6a8af1b5b80913495615ef2
JC <jcacct@protonmail.ch>
2021-12-31 21:08:03 -0800

    Solved Buggy Code (Part 4) in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,21 @@
+# Emmy has written a function that returns a greeting to users. However, she's in love with Mubashir, and would like to greet him slightly differently. She added a special case in her function, but she made a mistake.
+
+# Can you help her?
+# Examples
+
+# greeting("Matt") ➞ "Hello, Matt!"
+
+# greeting("Helen") ➞ "Hello, Helen!"
+
+# greeting("Mubashir") ➞ "Hello, my Love!"
+
+# Notes
+
+#     READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+#     Don't overthink this challenge; it's not supposed to be hard.
+def greeting(name)
+  name == "Mubashir" ? "Hello, my Love!" : "Hello, " + name + "!"
+end
 # Given an array of numbers, write a function that returns an array that...
 
 #     Has all duplicate elements removed.
