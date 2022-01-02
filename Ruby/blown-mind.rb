025f396e4e07bd19127b91e88191007b65157642
JC <jcacct@protonmail.ch>
2022-01-01 21:14:48 -0800

    Solved I'd Like a New Shade of Blue, Please  in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,24 @@
+# I have a bucket containing an amount of navy blue paint and I'd like to paint as many walls as possible. Create a function that returns the number of complete walls that I can paint, before I need to head to the shops to buy more.
+
+#     n is the number of square meters I can paint.
+#     w and h are the widths and heights of a single wall in meters.
+
+# Examples
+
+# how_many_walls(100, 4, 5) ➞ 5
+
+# how_many_walls(10, 15, 12) ➞ 0
+
+# how_many_walls(41, 3, 6) ➞ 2
+
+# Notes
+
+#     Don't count a wall if I don't manage to finish painting all of it before I run out of paint.
+#     All walls will have the same dimensions.
+#     All numbers will be positive integers.
+def how_many_walls(n, w, h)
+	n / (w * h)
+end
 # Emmy has written a function that returns a greeting to users. However, she's in love with Mubashir, and would like to greet him slightly differently. She added a special case in her function, but she made a mistake.
 
 # Can you help her?
