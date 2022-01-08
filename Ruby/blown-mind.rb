9c0367bbeaf90e87ea1acf880f9fe00bfeb1e327
JC <jcacct@protonmail.ch>
2022-01-07 20:51:49 -0800

    Solved Circle or Square in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,20 @@
+# Given the radius of a circle and the area of a square, return true if the circumference of the circle is greater than the square's perimeter and false if the square's perimeter is greater than the circumference of the circle.
+# Examples
+
+# circle_or_square(16, 625) ➞ true
+
+# circle_or_square(5, 100) ➞ false
+
+# circle_or_square(8, 144) ➞ true
+
+# Notes
+
+#     You can use Pi to 2 decimal places (3.14).
+#     Circumference of a circle equals 2 * Pi * radius.
+#     To find the perimeter of a square using its area, find the square root of area (to get side length) and multiply that by 4.
+def circle_or_square(rad, area)
+	2 * 3.14 * rad > Math.sqrt(area) * 4
+end
 # Given an array of numbers, negate all elements contained within.
 
 #     Negating a positive value -+n will return -n, because all +'s are removed.
