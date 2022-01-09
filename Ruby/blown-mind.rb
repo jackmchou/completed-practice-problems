1580de4570336e4276fe6bca26efdc8e94216e24
JC <jcacct@protonmail.ch>
2022-01-08 21:25:14 -0800

    Solved Factors of a Given Number in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,30 @@
+# Create a function that finds each factor of a given number n. Your solution should return an array of the number(s) that meet this criteria.
+# Examples
+
+# find_factors(9) ➞ [1, 3, 9]
+# # 9 has three factors 1, 3 and 9
+
+# find_factors(12) ➞ [1, 2, 3, 4, 6, 12]
+
+# find_factors(20) ➞ [1, 2, 4, 5, 10, 20]
+
+# find_factors(0) ➞ []
+# # 0 has no factors
+
+# Notes
+
+# Return an empty array if the given number is less than 1.
+def find_factors(n)
+	index = 1
+	result = []
+	while index <= n do
+		if n % index == 0
+			result << index
+		end 
+    index += 1
+  end
+	result
+end
 # Given the radius of a circle and the area of a square, return true if the circumference of the circle is greater than the square's perimeter and false if the square's perimeter is greater than the circumference of the circle.
 # Examples
 
