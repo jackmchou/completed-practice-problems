d03dc7bd278985e24cf1e1b9c3226917cda70841
JC <jcacct@protonmail.ch>
2022-01-09 21:29:21 -0800

    Solved Get Word Count in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that takes a string and returns the word count. The string will be a sentence.
+# Examples
+
+# count_words("Just an example here move along") ➞ 6
+
+# count_words("This is a test") ➞ 4
+
+# count_words("What an easy task, right") ➞ 5
+
+# Notes
+
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def count_words(str)
+	str.split(' ').length
+end
 # Create a function that finds each factor of a given number n. Your solution should return an array of the number(s) that meet this criteria.
 # Examples
 
