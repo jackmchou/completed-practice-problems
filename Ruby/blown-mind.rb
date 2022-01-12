d73bcfde73c2d649610fb2b0a6ea4aa826817808
JC <jcacct@protonmail.ch>
2022-01-12 12:50:08 -0800

    Solved Zip It, If You Can? in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,25 @@
+# Given an array of women and an array of men, either:
+
+#     Return "sizes don't match" if the two arrays have different sizes.
+#     If the sizes match, return an array of pairs, with the first woman paired with the first man, second woman paired with the second man, etc.
+
+# Examples
+
+# zip_it(["Elise", "Mary"], ["John", "Rick"])
+#  ➞ [("Elise", "John"), ("Mary", "Rick")]
+
+# zip_it(["Ana", "Amy", "Lisa"], ["Bob", "Josh"])
+#  ➞ "sizes don't match"
+
+# zip_it(["Ana", "Amy", "Lisa"], ["Bob", "Josh", "Tim"])
+#  ➞ [("Ana", "Bob"), ("Amy", "Josh"), ("Lisa", "Tim")]
+
+# Notes
+
+# Consider using the .zip function.
+def zip_it(women, men)
+	women.length == men.length ? women.zip(men) : "sizes don't match"
+end
 # Mubashir needs your help in a simple task.
 
 # Given an array of numbers arr:
