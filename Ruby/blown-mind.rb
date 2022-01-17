0f190672a93225f779b5b190507bba9528d56f3c
JC <jcacct@protonmail.ch>
2022-01-17 14:47:40 -0800

    Solved Find Array of Consecutive Numbers in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Implement a function that returns an array containing all the consecutive numbers in ascendant order from the given value low up to the given value high (bounds included).
+# Examples
+
+# get_sequence(1, 5) ➞ [1, 2, 3, 4, 5]
+
+# get_sequence(98, 100) ➞ [98, 99, 100]
+
+# get_sequence(1000, 1000) ➞ [1000]
+
+# Notes
+
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def get_sequence(low, high)
+	(low..high).to_a
+end
 # Create a function that takes the height and radius of a cone as arguments and returns the volume of the cone rounded to the nearest hundredth.
 # Volume of a Cone Image
 # Examples
