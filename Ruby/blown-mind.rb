0bb8670f75bfa35e8652d17216ff9e24d0dd7c5e
JC <jcacct@protonmail.ch>
2022-01-18 15:10:07 -0800

    Solved Opposite House in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,28 @@
+# Mubashir was walking through a straight street with exactly n identical houses on both sides. House numbers in the street look like this:
+
+# 1 |   | 6
+
+# 3 |   | 4
+
+# 5 |   | 2
+
+# He noticed that Even numbered houses increases on the right while Odd numbered houses decreases on the left.
+
+# Create a function that takes a house number house and length of the street n and returns the house number on the opposite side.
+# Examples
+
+# opposite_house(1, 3) ➞ 6
+
+# opposite_house(2, 3) ➞ 5
+
+# opposite_house(3, 5) ➞ 8
+
+# Notes
+
+# N/A
+def opposite_house(house, n)
+	n * 2 + 1 - house
+end
 # Implement a function that returns an array containing all the consecutive numbers in ascendant order from the given value low up to the given value high (bounds included).
 # Examples
 
