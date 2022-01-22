b3024424aea6107ca8fc03af1141bfda578dd421
JC <jcacct@protonmail.ch>
2022-01-22 15:50:05 -0800

    Solved Get the File Name in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that returns the selected filename from a path. Include the extension in your answer.
+# Examples
+
+# get_filename("C:/Projects/pil_tests/ascii/edabit.txt") ➞ "edabit.txt"
+
+# get_filename("C:/Users/johnsmith/Music/Beethoven_5.mp3") ➞ "Beethoven_5.mp3"
+
+# get_filename("ffprobe.exe") ➞ "ffprobe.exe"
+
+# Notes
+
+#     Tests will include both absolute and relative paths.
+#     For simplicity, all paths will include forward slashes.
+def get_filename(path)
+	path.rpartition('/')[-1]
+end
 # Create a function that takes a hash of student names and returns an array of student names in alphabetical order.
 # Examples
 
