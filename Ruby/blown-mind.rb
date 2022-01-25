63dbb395052b5f63aedf60e648ab255b6d133b31
JC <jcacct@protonmail.ch>
2022-01-25 09:23:03 -0800

    Solved Hello; Hello World; World in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,29 @@
+# Write a function that takes an integer and:
+
+#     If the number is a multiple of 3, return "Hello".
+#     If the number is a multiple of 5, return "World".
+#     If the number is a multiple of both 3 and 5, return "Hello World".
+
+# Examples
+
+# hello_world(3) ➞ "Hello"
+
+# hello_world(5) ➞ "World"
+
+# hello_world(15) ➞ "Hello World"
+
+# Notes
+
+# Don't forget to return the result.
+def hello_world(num)
+	if num % 3 == 0 and num % 5 == 0
+			"Hello World" 
+	elsif num % 5 == 0
+			"World"
+	else
+		"Hello"
+	end
+end
 # Create a function that checks if the argument is an integer or a string. Return "int" if it's an integer and "str" if it's a string.
 # Examples
 
