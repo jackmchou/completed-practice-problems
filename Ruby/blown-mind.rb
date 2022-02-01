6ffc8037c4aa9f320aa31975d777d8f23373b06e
JC <jcacct@protonmail.ch>
2022-01-31 17:51:16 -0800

    Solved Burrrrrrrp in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,20 @@
+# Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function.
+# Examples
+
+# long_burp(3) ➞ "Burrrp"
+
+# long_burp(5) ➞ "Burrrrrp"
+
+# long_burp(9) ➞ "Burrrrrrrrrp"
+
+# Notes
+
+#     Expect num to always be >= 1.
+#     Remember to use a capital "B".
+#     Don't forget to return the result.
+def long_burp(num)
+	'Bu' + 'r' * num + 'p'
+end
 # Create a function that takes in a word and determines whether or not it is plural. A plural word is one that ends in "s".
 # Examples
 # is_plural("changes") ➞ true
