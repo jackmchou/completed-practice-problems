cd7011ae024e0bddcb94a1255151459146b5c75d
JC <jcacct@protonmail.ch>
2022-02-02 18:01:48 -0800

    Solved Date Format in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that converts a date formatted as MM/DD/YYYY to YYYYDDMM.
+# Examples
+
+# format_date("11/12/2019") ➞ "20191211"
+
+# format_date("12/31/2019") ➞ "20193112"
+
+# format_date("01/15/2019") ➞ "20191501"
+
+# Notes
+
+# Return value should be a string.
+def format_date(date)
+	month, day, year = date.split('/')
+	year + day + month
+end
 # Create a function that takes an array of words and transforms it into an array of each word's length.
 # Examples
 
