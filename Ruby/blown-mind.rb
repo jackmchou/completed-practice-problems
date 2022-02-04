5e2b9bdd645bc3d15788f5812138ea43a683daff
JC <jcacct@protonmail.ch>
2022-02-03 18:15:03 -0800

    Solved Less Than, Greater Than in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that takes the number of daily average recovered cases recovers, daily average new_cases, current active_cases, and returns the number of days it will take to reach zero cases.
+# Examples
+
+# end_corona(4000, 2000, 77000) ➞ 39
+
+# end_corona(3000, 2000, 50699) ➞ 51
+
+# end_corona(30000, 25000, 390205) ➞ 79
+
+# Notes
+
+#     The number of people who recover per day recovers will always be greater than daily new_cases.
+#     Be conservative and round up the number of days needed.
+def arr_between(num1, num2, arr)
+	arr.select {|num| num1 < num < num2 }
+end
 # Create a function that converts a date formatted as MM/DD/YYYY to YYYYDDMM.
 # Examples
 
