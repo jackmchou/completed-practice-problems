d67fad9898ea41b175093e27b9a663c604ac235b
JC <jcacct@protonmail.ch>
2022-02-05 18:25:37 -0800

    Solved Reverse Psychology in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# For this challenge, you will NOT be given a string. Your task isn't to add "Do not" before the given string. If there is no given string, you will not return "Do not do anything." Do not check the examples to know how to do this challenge.
+# 	Examples
+	
+# 	reverse_psychology("wash the dishes") ➞ "Do not wash the dishes."
+	
+# 	reverse_psychology("eat your lunch") ➞ "Do not eat your lunch."
+	
+# 	reverse_psychology("go to school") ➞ "Do not go to school."
+	
+# 	Notes
+	
+# 	Are available.
+def reverse_psychology(s = '')
+	s != '' ? "Do not " + s +'.' : "Do not do anything."
+end
 # Create a function that takes an array of strings and integers, and filters out the array so that it returns an array of integers only.
 # Examples
 
