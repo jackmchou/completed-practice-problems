404f9af29a83a1392583214fb089d662592b3b5b
JC <jcacct@protonmail.ch>
2022-02-06 18:30:32 -0800

    Solved Get the Sum of All Array Elements in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# Create a function that takes an array and returns the sum of all numbers in the array.
+# Examples
+
+# get_sum_of_elements([2, 7, 4]) ➞ 13
+
+# get_sum_of_elements([45, 3, 0]) ➞ 48
+
+# get_sum_of_elements([-2, 84, 23]) ➞ 105
+
+# Notes
+
+# N/A
+def get_sum_of_elements(arr)
+	arr.reduce(:+)
+end
 # For this challenge, you will NOT be given a string. Your task isn't to add "Do not" before the given string. If there is no given string, you will not return "Do not do anything." Do not check the examples to know how to do this challenge.
 # 	Examples
 	
