fc803196461d7f91e306b53475bad364d65d6d13
JC <jcacct@protonmail.ch>
2022-02-07 18:35:20 -0800

    Solved Reverse the Number in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that takes an integer n and reverses it.
+# Examples
+
+# rev(5121) ➞ "1215"
+
+# rev(69) ➞ "96"
+
+# rev(-122157) ➞ "751221"
+
+# Notes
+
+#     This challenge is about using two operators that are related to division.
+#     If the number is negative, treat it like it's positive.
+def rev(n)
+	n.abs.to_s.reverse
+end
 # Create a function that takes an array and returns the sum of all numbers in the array.
 # Examples
 
