12810438f546ea723ea9a6566602259664de906c
JC <jcacct@protonmail.ch>
2022-02-08 18:51:56 -0800

    Solved Taxi Journey in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,23 @@
+# A taxi journey costs $3 for the first kilometer travelled. However, all kilometers travelled after that will cost $2 each.
+
+# Create a function which returns the distance that the taxi must've travelled, given the cost as a parameter.
+# Examples
+
+# journey_distance(3) ➞ 1
+# # The first kilometer costs $3
+
+# journey_distance(9) ➞ 4
+# # The first kilometer costs $3 plus the other three kilometers (costing $2 each)
+
+# journey_distance(5) ➞ 2
+
+# Notes
+
+#     The input tests are integers >= 0.
+#     Remember that you are calculating the distance from the cost, not the other way around!
+def journey_distance(n)
+	((n.to_f - 1) / 2).ceil || 0
+end
 # Create a function that takes an integer n and reverses it.
 # Examples
 
