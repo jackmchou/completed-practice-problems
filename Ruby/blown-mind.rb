4465e90ee969e2a480f89cae7f3c272024fa29b2
JC <jcacct@protonmail.ch>
2022-02-09 18:56:55 -0800

    Solved The 3 Programmers Problem in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,20 @@
+# You hired three programmers and you (hopefully) pay them. Create a function that takes three numbers (the hourly wages of each programmer) and returns the difference between the highest-paid programmer and the lowest-paid.
+# Examples
+
+# programmers(147, 33, 526) ➞ 493
+
+# programmers(33, 72, 74) ➞ 41
+
+# programmers(1, 5, 9) ➞ 8
+
+# Notes
+
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def programmers(one, two, three)
+	[one, two, three].max - [one, two, three].min
+end
 # A taxi journey costs $3 for the first kilometer travelled. However, all kilometers travelled after that will cost $2 each.
 
 # Create a function which returns the distance that the taxi must've travelled, given the cost as a parameter.
