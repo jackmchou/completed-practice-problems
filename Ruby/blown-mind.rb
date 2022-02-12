a645d640ab14e318d074bbaa9dba595af9750b4e
JC <jcacct@protonmail.ch>
2022-02-11 19:06:18 -0800

    Solved Similar Bread in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,28 @@
+# Given two arrays, which represent two sandwiches, return whether both sandwiches use the same type of bread. The bread will always be found at the start and end of the array.
+# Examples
+
+# has_same_bread(
+#   ["white bread", "lettuce", "white bread"],
+#   ["white bread", "tomato", "white bread"]
+# ) ➞ true
+
+# has_same_bread(
+#   ["brown bread", "chicken", "brown bread"],
+#   ["white bread", "chicken", "white bread"]
+# ) ➞ false
+
+# has_same_bread(
+#   ["toast", "cheese", "toast"],
+#   ["brown bread", "cheese", "toast"]
+# ) ➞ false
+
+# Notes
+
+#     The arrays will always be exactly three elements long.
+#     The first piece of bread on one sandwich must be the same as the first piece of bread on the other sandwich. The same goes for the last piece of bread.
+def has_same_bread(arr1, arr2)
+	arr1[0] == arr2[0] && arr1[2] == arr2[2]
+end
 # Write a function that returns true if both numbers are:
 
 #     Smaller than 0, OR ...
