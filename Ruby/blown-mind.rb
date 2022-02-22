70b6ed35449855e51547444d67ea5fb38de0e369
JC <jcacct@protonmail.ch>
2022-02-22 13:33:15 -0800

    Solved Spaces Between Each Character in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,19 @@
+# Create a function that takes a string and returns a string with spaces in between all of the characters.
+# Examples
+
+# space_me_out("space") ➞ "s p a c e"
+
+# space_me_out("far out") ➞ "f a r   o u t"
+
+# space_me_out("elongated musk") ➞ "e l o n g a t e d   m u s k"
+
+# Notes
+
+# Treat a space as its own character (i.e. leave three spaces between words).
+def space_me_out(s)
+	str = s.gsub(/./) {|letter| letter + ' '}
+	str.rstrip
+end
 # Create a function that always returns true for every item in a given array. However, if an element is the word "flick", switch to always returning the opposite boolean value.
 # Examples
 
