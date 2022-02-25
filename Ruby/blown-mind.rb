7fb37dcc9048b20b7b846c513face9788c986afb
JC <jcacct@protonmail.ch>
2022-02-25 13:41:52 -0800

    Solved Simple Letters in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,25 @@
+# Create a function that takes two lowercase strings str1 and str2 of letters from a to z and returns the sorted and longest string containing distinct letters.
+# Examples
+
+# str1 = "mubashir"
+# str2 = "edabit"
+
+# longest_string(str1, str2) ➞ "abdehimrstu"
+# # Contains sorted and distinct letters of the given strings.
+
+# str1 = "abcdefghijklmnopqrstuvwxyz"
+# str2 = "abcdefghijklmnopqrstuvwxyz"
+
+# longest_string(str1, str2) ➞ "abcdefghijklmnopqrstuvwxyz"
+# # Contains sorted and distinct letters of the given strings.
+
+# Notes
+
+# N/A
+# Checking
+def longest_string(str1, str2)
+  (str1 + str2).split('').uniq.sort.join
+end
 # Given a sandwich (as an array), return an array of fillings inside the sandwich. This involves ignoring the first and last elements.
 # Examples
 
