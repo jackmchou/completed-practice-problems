fad5781e829722fcfceca73b0e90bb24b8d60d44
JC <jcacct@protonmail.ch>
2022-02-28 16:27:10 -0800

    Solved Volume of a Box in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,20 @@
+# Create a function that takes a hash argument sizes (contains width, length, height keys) and returns the volume of the box.
+# Examples
+
+# volume_of_box({ "width": 2, "length": 5, "height": 1 }) ➞ 10
+
+# volume_of_box({ "width": 4, "length": 2, "height": 2 }) ➞ 16
+
+# volume_of_box({ "width": 2, "length": 3, "height": 5 }) ➞ 30
+
+# Notes
+
+#     Don't forget to return the result.
+#     Remember that the values are in an object.
+#     Volume is length * width * height.
+def volume_of_box(sizes)
+	sizes[:width] * sizes[:height] * sizes[:length]
+end
 # Kinetic energy can be calculated with the following formula:
 
 # KE = 1/2mv²
