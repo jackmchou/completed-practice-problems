d4f200d8fd639ccbd0763714888cc368a0fb271e
JC <jcacct@protonmail.ch>
2022-03-03 07:46:34 -0800

    Solved Is the Number Even or Odd? in py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,20 @@
+# Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
+# Examples
+
+# isEvenOrOdd(3) ➞ "odd"
+
+# isEvenOrOdd(146) ➞ "even"
+
+# isEvenOrOdd(19) ➞ "odd"
+
+# Notes
+
+#     Dont forget to return the result.
+#     Input will always be a valid integer.
+#     Expect negative integers (whole numbers).
+#     Tests are case sensitive (return "even" or "odd" in lowercase).
+def isEvenOrOdd(num):
+	return 'even' if num % 2 == 0 else 'odd'
 # Create a function that takes in a word and determines whether or not it is plural. A plural word is one that ends in "s".
 # Examples
 
