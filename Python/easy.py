17455c1aef1c6a0b4dbb2ec25ea3485204a09f5e
JC <jcacct@protonmail.ch>
2022-03-04 08:02:10 -0800

    Solved Even or Odd? in py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,24 @@
+# Given a list of integers, determine whether the sum of its elements is even or odd.
+
+# The return value should be a string ("odd" or "even").
+
+# If the input list is empty, consider it as a list with a zero ([0]).
+# Examples
+
+# even_or_odd([0]) ➞ "even"
+
+# even_or_odd([1]) ➞ "odd"
+
+# even_or_odd([]) ➞ "even"
+
+# even_or_odd([0, 1, 5]) ➞ "even"
+
+# Notes
+
+# N/A
+def even_or_odd(lst):
+	if sum(lst) == 0: return 'even'
+	return 'even' if sum(lst) % 2 == 0 else 'odd'
 # Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
 # Examples
 
