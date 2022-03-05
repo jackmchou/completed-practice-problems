0eb8f6d632e685090b42fe645873ca9097920313
JC <jcacct@protonmail.ch>
2022-03-05 08:12:08 -0800

    Solved List From a Range of Numbers in py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,19 @@
+# Create a function that returns a list of all the integers between two given numbers start and end.
+# Examples
+
+# range_of_num(2, 4) ➞ [3]
+
+# range_of_num(5, 9) ➞ [6, 7, 8]
+
+# range_of_num(2, 11) ➞ [3, 4, 5, 6, 7, 8, 9, 10]
+
+# Notes
+
+#     start will always be <= end.
+#     start and end are NOT included in the final list.
+#     If start == end, return an empty list.
+def range_of_num(start, end):
+	return list(range(start + 1, end))
 # Given a list of integers, determine whether the sum of its elements is even or odd.
 
 # The return value should be a string ("odd" or "even").
