1558eb68d0752eeeaa58ef948ed12a93362ca30f
JC <jcacct@protonmail.ch>
2022-03-07 08:31:50 -0800

    Solved Is the Last Character an N? in py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,19 @@
+# Create a function that takes a string (a random name). If the last character of the name is an "n", return True, otherwise return False.
+# Examples
+
+# is_last_character_n("Aiden") ➞ True
+
+# is_last_character_n("Piet") ➞ False
+
+# is_last_character_n("Bert") ➞ False
+
+# is_last_character_n("Dean") ➞ True
+
+# Notes
+
+# The function must return a boolean value ( i.e. True or False).
+def is_last_character_n(word):
+	return word[-1] == 'n'
 # Create a function that returns how many possible arrangements can come from a certain number of switches (on / off). In other words, for a given number of switches, how many different patterns of on and off can we have?
 # Examples
 
