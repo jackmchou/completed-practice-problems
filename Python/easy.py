02c7770d0ee90bb8bbeba004e7520bc9458723fd
JC <jcacct@protonmail.ch>
2022-03-08 08:35:13 -0800

    Solved Testing K^K == N? in py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,20 @@
+# Write a function that returns True if k^k == n for input (n, k) and return False otherwise.
+# Examples
+
+# k_to_k(4, 2) ➞ True
+
+# k_to_k(387420489, 9) ➞ True
+# # 9^9 == 387420489
+
+# k_to_k(3124, 5) ➞ False
+
+# k_to_k(17, 3) ➞ False
+
+# Notes
+
+# The ^ operator refers to exponentiation operation **, not the bitwise XOR operation.
+def k_to_k(n, k):
+	return k ** k == n
 # Create a function that takes a string (a random name). If the last character of the name is an "n", return True, otherwise return False.
 # Examples
 
