2833867f1ab9f0c80164e992a779cbaeb2266e3e
JC <jcacct@protonmail.ch>
2022-03-09 08:37:59 -0800

    Solved Is the String Empty? in py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,20 @@
+# Create a function that returns True if a string is empty and False otherwise.
+# Examples
+
+# is_empty("") ➞ True
+
+# is_empty(" ") ➞ False
+
+# is_empty("a") ➞ False
+
+# Notes
+
+#     A string containing only whitespaces " " does not count as empty.
+#     Don't forget to return the result.
+#     If you get stuck on a challenge, find help in the Resources tab.
+#     If you're really stuck, unlock solutions in the Solutions tab.
+def is_empty(s):
+	return s == ''
 # Write a function that returns True if k^k == n for input (n, k) and return False otherwise.
 # Examples
 
