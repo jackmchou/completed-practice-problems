6e56b00d0bd1ed535809ae7aa3c634ea133e355a
JC <jcacct@protonmail.ch>
2022-03-10 08:43:27 -0800

    Solved Let's Fuel Up! in py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,20 @@
+# A vehicle needs 10 times the amount of fuel than the distance it travels. However, it must always carry a minimum of 100 fuel before setting off.
+
+# Create a function which calculates the amount of fuel it needs, given the distance.
+# Examples
+
+# calculate_fuel(15) ➞ 150
+
+# calculate_fuel(23.5) ➞ 235
+
+# calculate_fuel(3) ➞ 100
+
+# Notes
+
+#     Distance will be a number greater than zero.
+#     Return 100 if the calculated fuel turns out to be less than 100.
+def calculate_fuel(n):
+	return n * 10
 # Create a function that returns True if a string is empty and False otherwise.
 # Examples
 
