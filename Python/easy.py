42a515fc6ac238c5ddfa68f26e7b568887806751
JC <jcacct@protonmail.ch>
2022-03-12 08:51:34 -0800

    Solved Evaluate an Equation in py

--- Python/easy.py
+++ Python/easy.py
@@ -1,3 +1,19 @@
+# Create a function that evaluates an equation.
+# Examples
+
+# eq("1+2") ➞ 3
+
+# eq("6/(9-7)") ➞ 3
+
+# eq("3+2-4") ➞ 1
+
+# Notes
+
+#     Don't print, return a value.
+#     Return the value, not the equation.
+#     The method used to solve this challenge should not be used in practice. However, it's important to be aware of how this functionality works and why it should not be used. Check the Resources for more information.
+def eq(evaluate):
+	return eval(evaluate)
 # Mubashir wants to swap two given numbers!
 
 # It is not returning the right values. Can you help him fix it?
