86be2dd5318d59a9607dd869d14a7d69127dc82c
JC <jcacct@protonmail.ch>
2022-03-16 08:16:01 -0700

    Solved Add the Index in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,22 @@
+# Given an array of numbers, create a function which returns the array but with each element's index in the array added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+# Examples
+
+# add_indexes([0, 0, 0, 0, 0]) ➞ [0, 1, 2, 3, 4]
+
+# add_indexes([1, 2, 3, 4, 5]) ➞ [1, 3, 5, 7, 9]
+
+# add_indexes([5, 4, 3, 2, 1]) ➞ [5, 5, 5, 5, 5]
+
+# Notes
+
+# You'll only get numbers in the array.
+def add_indexes(arr)
+  result = []
+	arr.each_with_index do |item, index| 
+		result << item + index
+	end
+  result
+end
 # Given a fraction as a string, return whether or not it is greater than 1 when evaluated.
 # Examples
 
