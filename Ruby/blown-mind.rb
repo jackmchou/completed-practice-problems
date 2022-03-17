971c28d2fa9857f6810eba2396ec09e031eec029
JC <jcacct@protonmail.ch>
2022-03-17 08:21:53 -0700

    Solved Smash Factor in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,22 @@
+
+# Smash factor is a term in golf that relates to the amount of energy transferred from the club head to the golf ball. The formula for calculating smash factor is ball speed divided by club speed.
+
+# Create a function that takes ball speed bs and club speed cs as arguments and returns the smash factor to the nearest hundredth.
+# Examples
+
+# smash_factor(139.4, 93.8) ➞ 1.49
+
+# smash_factor(181.2, 124.5) ➞ 1.46
+
+# smash_factor(154.7, 104.3) ➞ 1.48
+
+# Notes
+
+#     Remember to round to the nearest hundredth.
+#     All values will be valid (so no dividing by zero).
+def smash_factor(bs, cs)
+	(bs / cs).round(2)
+end
 # Given an array of numbers, create a function which returns the array but with each element's index in the array added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
 # Examples
 
