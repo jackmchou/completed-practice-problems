2f89b5ddada49023f7ad119a6e038ecbc083b5e1
JC <jcacct@protonmail.ch>
2022-03-19 07:39:43 -0700

    Solved Count the Capital Letters in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# Given a string of letters, how many capital letters are there?
+# Examples
+
+# capital_letters("fvLzpxmgXSDrobbgMVrc") ➞ 6
+
+# capital_letters("JMZWCneOTFLWYwBWxyFw") ➞ 14
+
+# capital_letters("mqeytbbjwqemcdrdsyvq") ➞ 0
+
+# Notes
+
+# N/A
+def capital_letters(str)
+	str.scan(/[A-Z]/).length
+end
 # Create a function that takes three numbers as arguments and returns true if it's a triangle and false if not.
 # Examples
 # is_triangle(2, 3, 4) ➞ true
