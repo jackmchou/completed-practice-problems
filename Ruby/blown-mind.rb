ebcd80919dc7e9b2a4c69a874a1759794ff8099e
JC <jcacct@protonmail.ch>
2022-03-20 07:55:56 -0700

    Solved Sort by String Length in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,21 @@
+# Create a function that returns an array of strings sorted by length in ascending order.
+# Examples
+
+# sort_by_length(["a", "ccc", "dddd", "bb"]) ➞ ["a", "bb", "ccc", "dddd"]
+
+# sort_by_length(["apple", "pie", "shortcake"]) ➞ ["pie", "apple", "shortcake"]
+
+# sort_by_length(["may", "april", "september", "august"]) ➞ ["may", "april", "august", "september"]
+
+# sort_by_length([]) ➞ []
+
+# Notes
+
+#     Strings will have unique lengths, so don't worry about comparing two strings with identical length.
+#     Return an empty array if the input array is empty (see example #4).
+def sort_by_length(arr)
+	arr.sort_by {|word| word.length}
+end
 # Given a string of letters, how many capital letters are there?
 # Examples
 
