8a86fbc38e82f6af6bff462c81035fbf2731d9da
JC <jcacct@protonmail.ch>
2022-03-21 22:30:06 -0700

    Solved Among Us Imposter Formula in rb

--- Ruby/blown-mind.rb
+++ Ruby/blown-mind.rb
@@ -1,3 +1,18 @@
+# Create a function that calculates the chance of being an imposter. The formula for the chances of being an imposter is 100 × (i / p) where i is the imposter count and p is the player count. Make sure to round the value to the nearest integer and return the value as a percentage.
+# Examples
+
+# imposter_formula(1, 10) ➞ "10%"
+
+# imposter_formula(2, 5) ➞ "40%"
+
+# imposter_formula(1, 8) ➞ "13%"
+
+# Notes
+
+# The player limit is 10 and the imposter count can only go up to 3.
+def imposter_formula(i, p)
+	"#{(100 * (i.to_f / p)).round}%"
+end
 # Write a function that takes two numbers and returns if they should be added, subtracted, multiplied or divided to get 24. If none of the operations can give 24, return nil.
 # Examples
 
