792eb9a4d93d2713b1b4f3df2c16f14c50221779
JC <jcacct@protonmail.ch>
2022-03-24 23:40:23 -0700

    w3school operators in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -16,3 +16,17 @@ alert(z);
 // lastName = "Doe"
 // age = 35
 var firstName = "John", lastName = "Doe", age = 35
+// Multiply 10 with 5, and alert the result:
+alert(10 * 5);
+// Divide 10 by 2, and alert the result:
+alert(10 / 2);
+// Alert the remainder when 15 is divided by 9.
+alert(15 % 9);
+// Use the correct assignment operator that will result in x being 15 (same as x = x + y).
+x = 10;
+y = 5;
+x += y;
+// Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+x = 10;
+y = 5;
+x *= y;
