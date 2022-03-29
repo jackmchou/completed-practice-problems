7034f3d8fd0299eb1c092479018d54933b8d6cd8
JC <jcacct@protonmail.ch>
2022-03-29 08:58:31 -0700

    w3school functions 4 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -51,3 +51,7 @@ function myFunction() {
 	return "Hello";
 }
 document.getElementById("demo").innerHTML = myFunction();
+// Make the function display "Hello" in the inner HTML of an element with the ID "demo".
+function myFunction() {
+	document.getElementById("demo").innerHTML = "Hello";
+}
