2504cc97ffeafe4b3d996a49dfb2d1845e8f0164
JC <jcacct@protonmail.ch>
2022-04-04 09:48:00 -0700

    w3school events 3 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -77,3 +77,5 @@ alert(person.name + " is " + person.age)
 `<button onclick="alert('Hello')">Click me.</button>`
 // When the button is clicked, the function "myFunction" should be executed.
 `<button onclick="myFunction()">Click me.</button>`
+// The <div> element should turn red when someone moves the mouse over it.
+`<div onmouseover="this.style.backgroundColor='red'">myDIV.</div>`
