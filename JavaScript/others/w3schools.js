49ec02d0201ccef9d5e2ae5497efcba02596136f
JC <jcacct@protonmail.ch>
2022-04-08 10:04:51 -0700

    w3school strings methods 2 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -93,3 +93,6 @@ alert(str1 + str2);
 // Convert the text into an UPPERCASE text:
 let txt = "Hello World!";
 txt = txt.toUpperCase();
+// Use the slice method to return the word "bananas".
+let txt = "I can eat bananas all day";
+let x = txt.slice(10, 17);
