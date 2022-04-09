460d32e943502b1870326de70bc8edecbf3e795c
JC <jcacct@protonmail.ch>
2022-04-09 10:07:15 -0700

    w3school strings methods 3 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -96,3 +96,6 @@ txt = txt.toUpperCase();
 // Use the slice method to return the word "bananas".
 let txt = "I can eat bananas all day";
 let x = txt.slice(10, 17);
+// Use the correct String method to replace the word "Hello" with the word "Welcome".
+let txt = "Hello World";
+txt = txt.replace("Hello", "Welcome");
