4088c2e3d14f99881df53a8e5c37768fb48d2cbf
JC <jcacct@protonmail.ch>
2022-04-14 06:18:27 -0700

    w3school arrays 3 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -111,3 +111,6 @@ let x = cars[1]
 // Change the first item of cars to "Ford".
 const cars = ["Volvo", "Jeep", "Mercedes"];
 cars[0] = "Ford";
+// Alert the number of items in an array, using the correct Array property.
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
