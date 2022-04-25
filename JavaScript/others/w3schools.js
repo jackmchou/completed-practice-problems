cca349114edb07df391fe14d90fe05c81064c7fe
JC <jcacct@protonmail.ch>
2022-04-25 11:54:18 -0700

    w3school math 2 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -140,3 +140,5 @@ const d = new Date();
 d.setFullYear(2020);
 // Use the correct Math method to create a random number.
 let r = Math.random();
+// Use the correct Math method to return the largest number of 10 and 20.
+let x = Math.max(10, 20);
