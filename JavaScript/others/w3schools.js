91c4c40f6d98491db5af66d2bbc3e59c50b8e405
JC <jcacct@protonmail.ch>
2022-04-28 12:05:30 -0700

    w3school comparisons 1 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -146,3 +146,7 @@ let x = Math.max(10, 20);
 let x = Math.round(5.3);
 // Use the correct Math method to get the square root of 9.
 let x = Math.sqrt(9)
+//Choose the correct comparison operator to alert true, when x is greater than y.
+x = 10;
+y = 5;
+alert(x > y);
