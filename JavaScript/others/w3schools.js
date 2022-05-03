ee51451c8e57ba3d8c94345bc59f3735419ef438
JC <jcacct@protonmail.ch>
2022-05-03 12:18:43 -0700

    w3school conditions 2 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -166,3 +166,9 @@ alert(voteable);
 if (x > y) {
  alert("Hello World");
 };
+//Fix the if statement to alert "Hello World" if x is greater than y, otherwise alert "Goodbye".
+if (x > y) {
+	alert("Hello World");
+} else {
+	alert("Goodbye");
+}
