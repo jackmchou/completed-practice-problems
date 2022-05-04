cb29cb3d55e69dc3aa8f64bf58261c192e5fb2e4
JC <jcacct@protonmail.ch>
2022-05-04 12:33:55 -0700

    w3school switch 1 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -172,3 +172,12 @@ if (x > y) {
 } else {
 	alert("Goodbye");
 }
+// Create a switch statement that will alert "Hello" if fruits is "banana", and "Welcome" if fruits is "apple".
+switch (fruits) {
+	case "Banana":
+    alert("Hello")
+    break;
+	case "Apple":
+    alert("Welcome")
+    break;
+}
