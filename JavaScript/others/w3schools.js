3aa7cdcaa7ba03ecae00e6422f6cd0deaba83087
JC <jcacct@protonmail.ch>
2022-05-05 12:34:37 -0700

    w3school switch 2 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -181,3 +181,14 @@ switch (fruits) {
     alert("Welcome")
     break;
 }
+//Add a section that will alert("Neither") if fruits is neither "banana" nor "apple".
+switch(fruits) {
+  case "Banana":
+    alert("Hello")
+    break;
+  case "Apple":
+    alert("Welcome")
+    break;
+	default:
+		alert("Neither");
+}
