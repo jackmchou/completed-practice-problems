5fa017d18f7729c231a305b7ad95e87bb68c8359
JC <jcacct@protonmail.ch>
2022-05-11 13:14:12 -0700

    w3school break loops 2 in JS

--- JavaScript/others/w3schools.js
+++ JavaScript/others/w3schools.js
@@ -221,3 +221,10 @@ for (i = 0; i < 10; i++) {
 		break;
 	}
 }
+//Make the loop jump to the next iteration when i is 5.
+for (i = 0; i < 10; i++) {
+  if (i == 5) {
+		break;
+	}
+	console.log(i);
+}
