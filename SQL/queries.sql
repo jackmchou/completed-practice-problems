29e7711ced0b253d1af7819c8ffbad91977f15d5
JC <jcacct@protonmail.ch>
2022-05-22 15:26:44 -0700

    w3school SQL Select 3 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -189,3 +189,5 @@ DROP TABLE IF EXISTS boxoffice;
 SELECT * FROM Customers;
 -- Write a statement that will select the City column from the Customers table.
 SELECT City FROM Customers;
+-- Select all the different values from the Country column in the Customers table.
+SELECT DISTINCT Country FROM Customers;
