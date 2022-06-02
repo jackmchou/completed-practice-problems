f01b309ea24e9d3fa9054a01f606004bb2cff894
JC <jcacct@protonmail.ch>
2022-06-02 16:39:00 -0700

    w3school SQL Null 2 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -233,3 +233,6 @@ VALUES (
 -- Select all records from the Customers where the PostalCode column is empty.
 SELECT * FROM Customers
 WHERE PostalCode IS NULL
+-- Select all records from the Customers where the PostalCode column is NOT empty.
+SELECT * FROM Customers
+WHERE PostalCode IS NOT NULL;
