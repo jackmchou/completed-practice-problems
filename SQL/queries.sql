10d912717f4d53b1d39d3ac50cbb444a6ad2d25e
JC <jcacct@protonmail.ch>
2022-06-05 16:46:50 -0700

    w3school SQL Update 3 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -243,3 +243,7 @@ SET City = 'Oslo';
 UPDATE Customers
 SET City = 'Oslo'
 WHERE Country = 'Norway';
+-- Update the City value and the Country value.
+UPDATE Customers
+SET City = 'Oslo', Country = 'Norway'
+WHERE CustomerID = 32;
