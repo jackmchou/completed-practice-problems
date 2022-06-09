61db5656ec1f0347b54d0b1ea020e2cfbffd2843
JC <jcacct@protonmail.ch>
2022-06-09 05:57:14 -0700

    w3school SQL Functions 2 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -255,4 +255,6 @@ DELETE FROM Customers
 -- Use the MIN function to select the record with the smallest value of the Price column.
 SELECT MIN(Price)
 FROM Products;
-
+-- Use an SQL function to select the record with the highest value of the Price column.
+SELECT MAX(Price)
+FROM Product;
