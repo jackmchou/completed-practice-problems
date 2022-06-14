053de66aac318c48da0ed157c8971025443c092e
JC <jcacct@protonmail.ch>
2022-06-14 06:16:21 -0700

    w3school SQL Like 2 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -271,5 +271,7 @@ FROM Products;
 -- Select all records where the value of the City column starts with the letter "a".
 SELECT * FROM Customers
 WHERE City LIKE 'a%'
-
+-- Select all records where the value of the City column ends with the letter "a".
+SELECT * FROM Customers
+WHERE City LIKE '%a'
 
