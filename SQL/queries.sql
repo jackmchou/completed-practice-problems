fdfdbe784cc6cb354a7a245c80eaacb20109e20f
JC <jcacct@protonmail.ch>
2022-06-15 06:21:18 -0700

    w3school SQL Like 3 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -274,4 +274,6 @@ WHERE City LIKE 'a%'
 -- Select all records where the value of the City column ends with the letter "a".
 SELECT * FROM Customers
 WHERE City LIKE '%a'
-
+-- Select all records where the value of the City column contains the letter "a".
+SELECT * FROM Customers
+WHERE City LIKE '%a%';
