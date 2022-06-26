e277627db011c93de70536c483f2298bec82385a
JC <jcacct@protonmail.ch>
2022-06-26 12:58:37 -0700

    w3school SQL Between 3 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -307,3 +307,6 @@ WHERE Price BETWEEN 10 AND 20;
 -- Use the BETWEEN operator to select all the records where the value of the Price column is NOT between 10 and 20.
 SELECT * FROM Products
 WHERE Price NOT Between 10 AND 20; 
+-- Use the BETWEEN operator to select all the records where the value of the ProductName column is alphabetically between 'Geitost' and 'Pavlova'.
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'Geitost' AND 'Pavlova';
