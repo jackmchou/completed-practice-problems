841c05cb915cc012f966617a99a3686a23f4db50
JC <jcacct@protonmail.ch>
2022-06-28 13:02:56 -0700

    w3school SQL Alias 2 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -313,3 +313,6 @@ WHERE ProductName BETWEEN 'Geitost' AND 'Pavlova';
 -- When displaying the Customers table, make an ALIAS of the PostalCode column, the column should be called Pno instead.
 SELECT CustomerName,
 Address, PostalCode AS Pno, FROM Customers;
+-- When displaying the Customers table, refer to the table as Consumers instead of Customers.
+SELECT *
+FROM Customers As Consumers;
