10670154709ef7a31c212916e613a2bbeb6e870d
JC <jcacct@protonmail.ch>
2022-07-01 13:10:14 -0700

    w3school SQL Join 3 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -326,4 +326,9 @@ SELECT *
 FROM Orders
 INNER JOIN Customers
 ON Orders.CustomerID=Customers.CustomerID;
+-- Choose the correct JOIN clause to select all the records from the Customers table plus all the matches in the Orders table.
+SELECT *
+FROM Orders
+RIGHT JOIN Customers
+ON Orders.CustomerID=Customers.CustomerID;
 
