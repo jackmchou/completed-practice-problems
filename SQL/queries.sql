db8a836372c8c02ecc14940c46f0664840d3cdf2
JC <jcacct@protonmail.ch>
2022-07-03 13:20:11 -0700

    w3school SQL Group By 2 in SQL

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -335,3 +335,8 @@ ON Orders.CustomerID=Customers.CustomerID;
 SELECT count(CustomerID), Country
 FROM Customers
 GROUP BY Country;
+-- List the number of customers in each country, ordered by the country with the most customers first.
+SELECT count(CustomerID), Country
+FROM Customers
+GROUP BY Country
+ORDER BY count(CustomerID) DESC;
